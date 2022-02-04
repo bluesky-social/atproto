2dd3ab52f7bf402a207722ea6898e38665d2d988
Daniel Holmgren <dtholmgren@gmail.com>
2022-02-04 14:15:41 -0700

    UserStore interface

--- common/src/types.ts
+++ common/src/types.ts
@@ -20,8 +20,9 @@ export type Follow = {
 }
 
 export type Post = {
-  user: string
+  author: string
   text: string
+  time: string // ISO 8601
 }
 
 export type SignedRoot = {
@@ -32,4 +33,26 @@ export type SignedRoot = {
 export interface BlockstoreI {
   get(cid: CID): Promise<Uint8Array>
   put(cid: CID, bytes: Uint8Array): Promise<void>
+}
+
+export interface UserStoreI {
+  getUser(): Promise<User>
+
+  addPost(text: string): Promise<string>
+  editPost(id: string, text: string): Promise<void>
+  deletePost(id: string): Promise<void>
+  listPosts(count: number, from?: number): Promise<Post[]>
+
+  reply(id: string, text: string): Promise<void>
+
+  like(id: string): Promise<void>
+  unlike(id: string): Promise<void>
+  listLikes(): Promise<void>
+
+  followUser(username: string, did: string): Promise<void>
+  unfollowUser(did:string): Promise<void>
+  listFollows(): Promise<Follow[]>
+
+  getCarStream(): AsyncIterable<Uint8Array>
+  getCarFile(): Promise<Uint8Array>
 }
