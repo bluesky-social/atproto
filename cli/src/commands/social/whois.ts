0242631844f033675037004f8d325ff84c44cd56
Paul Frazee <pfrazee@gmail.com>
2022-02-04 12:03:58 -0600

    Implement cli 'whois' command

--- cli/src/commands/social/whois.ts
+++ cli/src/commands/social/whois.ts
@@ -1,4 +1,7 @@
+import chalk from 'chalk'
 import cmd from '../../lib/command.js'
+import { Repo } from '../../lib/repo.js'
+import { REPO_PATH } from '../../lib/env.js'
 
 export default cmd({
   name: 'whois',
@@ -9,6 +12,10 @@ export default cmd({
   ],
   opts: [],
   async command (args) {
-    throw new Error('TODO')
+    const repo = await Repo.load(REPO_PATH)
+    const store = await repo.getUserStore(args._[0])
+    const user = await store.getUser()
+    console.log(`${user.name} ${chalk.gray(user.did)}`)
+    console.log(`${store.posts.length} posts | ${store.follows.length} followed`) // @TODO follower count
   }
 })
