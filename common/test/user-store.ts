57653a6f32e20777c822544a2a7c26595ba0e188
dholms <dtholmgren@gmail.com>
2022-03-08 13:11:09 -0600

    uncomment test

--- common/test/user-store.ts
+++ common/test/user-store.ts
@@ -26,71 +26,71 @@ test.beforeEach(async (t) => {
   t.pass('Context setup')
 })
 
-// test('adds a valid signature to commit', async (t) => {
-//   const { store } = t.context as Context
+test('adds a valid signature to commit', async (t) => {
+  const { store } = t.context as Context
 
-//   const commit = await store.getCommit()
-//   const verified = await ucan.verifySignature(
-//     commit.root.bytes,
-//     commit.sig,
-//     store.did,
-//   )
-//   t.true(verified, 'signature matches DID of root')
-// })
+  const commit = await store.getCommit()
+  const verified = await ucan.verifySignature(
+    commit.root.bytes,
+    commit.sig,
+    store.did,
+  )
+  t.true(verified, 'signature matches DID of root')
+})
 
-// test('sets correct DID', async (t) => {
-//   const { store, keypair } = t.context as Context
-//   t.is(store.did, keypair.did(), 'DIDs match')
-// })
+test('sets correct DID', async (t) => {
+  const { store, keypair } = t.context as Context
+  t.is(store.did, keypair.did(), 'DIDs match')
+})
 
-// test('runs operations on the related program', async (t) => {
-//   const { store, programName } = t.context as Context
+test('runs operations on the related program', async (t) => {
+  const { store, programName } = t.context as Context
 
-//   const tid = TID.next()
-//   const cid = await util.randomCid()
-//   await store.runOnProgram(programName, async (program) => {
-//     await program.posts.addEntry(tid, cid)
-//   })
+  const tid = TID.next()
+  const cid = await util.randomCid()
+  await store.runOnProgram(programName, async (program) => {
+    await program.posts.addEntry(tid, cid)
+  })
 
-//   const got = await store.runOnProgram(programName, async (program) => {
-//     return program.posts.getEntry(tid)
-//   })
-//   t.deepEqual(got, cid, `Matching content for post tid: ${tid}`)
-// })
+  const got = await store.runOnProgram(programName, async (program) => {
+    return program.posts.getEntry(tid)
+  })
+  t.deepEqual(got, cid, `Matching content for post tid: ${tid}`)
+})
 
-// test('name spaces programs', async (t) => {
-//   const { store, programName, otherProgram } = t.context as Context
+test('name spaces programs', async (t) => {
+  const { store, programName, otherProgram } = t.context as Context
 
-//   const tid = TID.next()
-//   const cid = await util.randomCid()
-//   await store.runOnProgram(programName, async (program) => {
-//     await program.posts.addEntry(tid, cid)
-//   })
+  const tid = TID.next()
+  const cid = await util.randomCid()
+  await store.runOnProgram(programName, async (program) => {
+    await program.posts.addEntry(tid, cid)
+  })
 
-//   const tidOther = TID.next()
-//   const cidOther = await util.randomCid()
-//   await store.runOnProgram(otherProgram, async (program) => {
-//     await program.posts.addEntry(tidOther, cidOther)
-//   })
+  const tidOther = TID.next()
+  const cidOther = await util.randomCid()
+  await store.runOnProgram(otherProgram, async (program) => {
+    await program.posts.addEntry(tidOther, cidOther)
+  })
 
-//   const got = await store.runOnProgram(programName, async (program) => {
-//     return Promise.all([
-//       program.posts.getEntry(tid),
-//       program.posts.getEntry(tidOther),
-//     ])
-//   })
-//   t.deepEqual(got[0], cid, 'correctly retrieves tid from program')
-//   t.deepEqual(got[1], null, 'cannot find tid from other program')
+  const got = await store.runOnProgram(programName, async (program) => {
+    return Promise.all([
+      program.posts.getEntry(tid),
+      program.posts.getEntry(tidOther),
+    ])
+  })
+  t.deepEqual(got[0], cid, 'correctly retrieves tid from program')
+  t.deepEqual(got[1], null, 'cannot find tid from other program')
 
-//   const gotOther = await store.runOnProgram(otherProgram, async (program) => {
-//     return Promise.all([
-//       program.posts.getEntry(tid),
-//       program.posts.getEntry(tidOther),
-//     ])
-//   })
-//   t.deepEqual(gotOther[0], null, 'cannot find tid from other program')
-//   t.deepEqual(gotOther[1], cidOther, 'correctly retrieves tid from program')
-// })
+  const gotOther = await store.runOnProgram(otherProgram, async (program) => {
+    return Promise.all([
+      program.posts.getEntry(tid),
+      program.posts.getEntry(tidOther),
+    ])
+  })
+  t.deepEqual(gotOther[0], null, 'cannot find tid from other program')
+  t.deepEqual(gotOther[1], cidOther, 'correctly retrieves tid from program')
+})
 
 test('loads from blockstore', async (t) => {
   const { ipld, store, programName } = t.context as Context
