445052454da422fd64c8558ea509f59d55e75a7f
dholms <dtholmgren@gmail.com>
2022-04-09 19:24:48 -0500

    remove old user routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,13 +3,11 @@ import WellKnown from './well-known.js'
 import ID from './id.js'
 import Data from './data/index.js'
 import Indexer from './indexer/index.js'
-import User from './user.js'
 
 const router = express.Router()
 
 router.use('/.well-known', WellKnown)
 router.use('/id', ID)
-router.use('/user', User)
 router.use('/data', Data)
 router.use('/indexer', Indexer)
 
