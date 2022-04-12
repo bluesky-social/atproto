aab5133a230b8e8b43e234dfabe14c38e956aaec
dholms <dtholmgren@gmail.com>
2022-04-12 13:42:43 -0500

    Push repo on initial subscribe

--- server/src/routes/data/subscribe.ts
+++ server/src/routes/data/subscribe.ts
@@ -14,6 +14,9 @@ router.post('/', async (req, res) => {
   const { did, host } = util.checkReqBody(req.body, subscribeReq)
   const db = util.getDB(res)
   await db.createSubscription(host, did)
+  // do an initial push
+  const repo = await util.loadRepo(res, did)
+  await repo.push(host)
   res.status(200).send()
 })
 
