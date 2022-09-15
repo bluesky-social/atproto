4fb4df1633aae3ef55f8be9e82d46f1716aadae4
dholms <dtholmgren@gmail.com>
2022-09-15 16:26:49 -0500

    remove lobby page & static

--- packages/aic/src/server/routes.ts
+++ packages/aic/src/server/routes.ts
@@ -6,7 +6,6 @@ import * as locals from './locals'
 import { ServerError } from './error'
 
 const router = express.Router()
-router.use(express.static('static'))
 
 // Get a DID doc
 router.get(`/:did`, async function (req, res) {
@@ -41,29 +40,6 @@ router.post(`/:did`, async function (req, res) {
   const { db } = locals.get(res)
   await db.validateAndAddOp(did, op)
   res.sendStatus(200)
-})
-
-router.get('/lobby', async (req, res) => {
-  res.send(`
-  <html>
-    <head>
-      <title>AIC Lobby</title>
-    </head>
-    <body>
-      hello lobby
-      <form action="#" id="">
-        Did Doc: <input type="text" name="init" id="doc">
-        <input type="button" value="look up" id="btn">
-      </form>
-      <script type="text/javascript">
-        function run() {
-          alert(document.getElementById('doc').value)
-        }
-        document.getElementById('btn').onclick = run;
-      </script>
-    </body>
-  </html>
-  `)
 })
 
 export default router
