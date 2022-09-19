ca05aaa5fe94fb2fedaf3abf1486858183aab780
Paul Frazee <pfrazee@gmail.com>
2022-09-19 17:54:27 -0500

    Update codegen from xrpc-cli to run prettier and include a banner comment

--- packages/xrpc-cli/src/codegen/common.ts
+++ packages/xrpc-cli/src/codegen/common.ts
@@ -1,6 +1,14 @@
 import { Project, SourceFile, VariableDeclarationKind } from 'ts-morph'
 import { MethodSchema } from '@adxp/xrpc'
+import prettier from 'prettier'
 import { GeneratedFile } from '../types'
+
+const PRETTIER_OPTS = {
+  parser: 'babel',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
 
 export const schemasTs = (project, schemas: MethodSchema[]) =>
   gen(project, '/schemas.ts', async (file) => {
@@ -34,9 +42,18 @@ export async function gen(
   const file = project.createSourceFile(path)
   await gen(file)
   file.saveSync()
+  const src = project.getFileSystem().readFileSync(path)
   // TODO run prettier on the output
   return {
     path: path,
-    content: project.getFileSystem().readFileSync(path),
+    content: `${banner()}${prettier.format(src, PRETTIER_OPTS)}`,
   }
+}
+
+function banner() {
+  return `/**
+* GENERATED CODE - DO NOT MODIFY
+* Created ${new Date().toDateString()}
+*/
+`
 }
