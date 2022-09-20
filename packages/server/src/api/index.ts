bf5b1d1d7754328ee9f8f2f86d704f70e7f39bc1
Paul Frazee <pfrazee@gmail.com>
2022-09-19 20:24:13 -0500

    Add /schemas folder with todo.adx

--- packages/server/src/api/index.ts
+++ packages/server/src/api/index.ts
@@ -2,11 +2,15 @@ import { createServer } from '../xrpc'
 import names from './names'
 import session from './session'
 import account from './account'
+import repo from './repo'
+import sync from './sync'
 
 export default function () {
   const server = createServer()
   names(server)
   session(server)
   account(server)
+  repo(server)
+  sync(server)
   return server
 }
