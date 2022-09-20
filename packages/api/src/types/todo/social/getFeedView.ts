04111a656cf2884a4e240628c2597d73d30a9ac2
Paul Frazee <pfrazee@gmail.com>
2022-09-19 23:45:34 -0500

    Fix some schema definitions

--- packages/api/src/types/todo/social/getFeedView.ts
+++ packages/api/src/types/todo/social/getFeedView.ts
@@ -14,9 +14,48 @@ export interface CallOptions {
   headers?: Headers;
 }
 
+export interface OutputSchema {
+  feed: FeedItem[];
+}
+export interface FeedItem {
+  uri: string;
+  author: User;
+  repostedBy?: User;
+  record: {};
+  embed?: RecordEmbed | ExternalEmbed | UnknownEmbed;
+  replyCount: number;
+  repostCount: number;
+  likeCount: number;
+  indexedAt: string;
+  myState?: {
+    repost?: string,
+    like?: string,
+  };
+}
+export interface User {
+  did: string;
+  name: string;
+  displayName?: string;
+}
+export interface RecordEmbed {
+  type: 'record';
+  author: User;
+  record: {};
+}
+export interface ExternalEmbed {
+  type: 'external';
+  uri: string;
+  title: string;
+  description: string;
+  imageUri: string;
+}
+export interface UnknownEmbed {
+  type: string;
+}
+
 export interface Response {
   success: boolean;
   error: boolean;
   headers: Headers;
-  data: Uint8Array;
+  data: OutputSchema;
 }
