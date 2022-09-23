0dfe856bfac6c4cbdfaa6303458708552bbb3505
dholms <dtholmgren@gmail.com>
2022-09-23 18:54:52 -0500

    parsing dids to something we understand

---
+++ packages/did-sdk/src/atp-did.ts
@@ -0,0 +1,105 @@
+type VerificationMethod = {
+  id: string
+  type: string
+  controller: string
+  publicKeyMultibase: string
+}
+
+type Service = {
+  id: string
+  type: string
+  serviceEndpoint: string
+}
+
+type Document = {
+  id: string
+  alsoKnownAs: string[] | string
+  verificationMethod: VerificationMethod[] | VerificationMethod
+  service: Service[] | Service
+}
+
+type DocumentData = {
+  id: string
+  signingKey: string
+  recoveryKey: string
+  username: string
+  atpPds: string
+}
+
+export const getId = (doc: Document): string => {
+  const id = doc.id
+  if (typeof id !== 'string') {
+    throw new Error('No `id` on document')
+  }
+  return id
+}
+
+export const getKey = (doc: Document, id: string): string | undefined => {
+  let keys = doc.verificationMethod
+  if (!keys) return undefined
+  if (typeof keys !== 'object') return undefined
+  if (!Array.isArray(keys)) {
+    keys = [keys]
+  }
+  const found = keys.find((key) => key.id === id)
+  if (!found) return undefined
+
+  // @TODO convert this to did:key
+  return found.publicKeyMultibase
+}
+
+export const getUsername = (doc: Document): string | undefined => {
+  const aka = doc.alsoKnownAs
+  if (!aka) return undefined
+  if (typeof aka === 'string') return aka
+  if (Array.isArray(aka) && typeof aka[0] === 'string') {
+    return aka[0]
+  }
+  return undefined
+}
+
+export const getAtpPds = (doc: Document): string | undefined => {
+  let services = doc.service
+  if (!services) return undefined
+  if (typeof services !== 'object') return undefined
+  if (!Array.isArray(services)) {
+    services = [services]
+  }
+  const found = services.find(
+    (service) => service.type === 'AtpPersonalDataServer',
+  )
+  if (!found) return undefined
+  return found.serviceEndpoint
+}
+
+export const parseToAtpDocument = (doc: Document): Partial<DocumentData> => {
+  const id = getId(doc)
+  return {
+    id,
+    signingKey: getKey(doc, `${id}#signingKey`),
+    recoveryKey: getKey(doc, `${id}#recoveryKey`),
+    username: getUsername(doc),
+    atpPds: getAtpPds(doc),
+  }
+}
+
+export const ensureAtpDocument = (doc: Document): DocumentData => {
+  const { id, signingKey, recoveryKey, username, atpPds } =
+    parseToAtpDocument(doc)
+  if (!id) {
+    throw new Error(`Could not parse id from doc: ${doc}`)
+  }
+  if (!signingKey) {
+    throw new Error(`Could not parse signingKey from doc: ${doc}`)
+  }
+  if (!recoveryKey) {
+    throw new Error(`Could not parse recoveryKey from doc: ${doc}`)
+  }
+  if (!username) {
+    throw new Error(`Could not parse username from doc: ${doc}`)
+  }
+  if (!atpPds) {
+    throw new Error(`Could not parse atpPds from doc: ${doc}`)
+  }
+  return { id, signingKey, recoveryKey, username, atpPds }
+}
