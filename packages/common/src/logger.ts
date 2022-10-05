202616cd3f20deffae3a7cced96e4e628caab856
dholms <dtholmgren@gmail.com>
2022-10-05 16:28:19 -0500

    fix subsystem logging

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -1,15 +1,19 @@
 import pino from 'pino'
 
 const allSystemsEnabled = !process.env.LOG_SYSTEMS
-const enabledSystems = (process.env.LOG_SYSTEMS || '').split(' ')
+const enabledSystems = (process.env.LOG_SYSTEMS || '')
+  .replace(',', ' ')
+  .split(' ')
 
 const enabledEnv = process.env.LOG_ENABLED
 const enabled =
   enabledEnv === 'true' || enabledEnv === 't' || enabledEnv === '1'
 
+const level = process.env.LOG_LEVEL || 'info'
+
 const config = {
   enabled,
-  level: process.env.LOG_LEVEL || 'info',
+  level,
 }
 
 const rootLogger = process.env.LOG_DESTINATION
@@ -22,6 +26,11 @@ export const subsystemLogger = (name: string): pino.Logger => {
   if (subsystems[name]) return subsystems[name]
   const subsystemEnabled =
     enabled && (allSystemsEnabled || enabledSystems.indexOf(name) > -1)
-  subsystems[name] = rootLogger.child({ name, enabled: subsystemEnabled })
+
+  // can't disable child loggers, so we just set their level to fatal to effectively turn them off
+  subsystems[name] = rootLogger.child(
+    { name },
+    { level: subsystemEnabled ? level : 'fatal' },
+  )
   return subsystems[name]
 }
