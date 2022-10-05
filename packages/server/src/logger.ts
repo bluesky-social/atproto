7eac068bffd42dc51ec6290cb7be7dfe9086b690
dholms <dtholmgren@gmail.com>
2022-10-05 14:43:27 -0500

    remove auth tokens from logs

--- packages/server/src/logger.ts
+++ packages/server/src/logger.ts
@@ -1,8 +1,25 @@
+import pino from 'pino'
 import pinoHttp from 'pino-http'
 import { subsystemLogger } from '@adxp/common'
 
 export const logger = subsystemLogger('pds')
-// @TODO config to strip out auth tokens
+
 export const loggerMiddleware = pinoHttp({
   logger,
+  serializers: {
+    req: (req) => {
+      const serialized = pino.stdSerializers.req(req)
+      const authHeader = serialized.headers.authorization || ''
+      let auth = 'NONE'
+      if (authHeader.startsWith('Bearer ')) auth = 'BEARER'
+      if (authHeader.startsWith('Basic ')) auth = 'BASIC'
+      return {
+        ...serialized,
+        headers: {
+          ...serialized.headers,
+          authorization: auth,
+        },
+      }
+    },
+  },
 })
