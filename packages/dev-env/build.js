98b14e7b5503a40b15bed275773be3fbd0cca9d0
Devin Ivy <devinivy@gmail.com>
2022-10-06 16:00:12 -0400

    Fix dev-env build, account for server's hbs templates

--- packages/dev-env/build.js
+++ packages/dev-env/build.js
@@ -1,3 +1,5 @@
+const { copy } = require('esbuild-plugin-copy')
+
 require('esbuild')
   .build({
     logLevel: 'info',
@@ -11,6 +13,17 @@ require('esbuild')
       '../plc/node_modules/@mapbox/node-pre-gyp/*',
       '../server/node_modules/sqlite3/*',
       '../../node_modules/classic-level/*',
+    ],
+    plugins: [
+      copy({
+        // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+        // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+        assets: {
+          from: ['../server/src/mailer/templates/**/*'],
+          to: ['./templates'],
+          keepStructure: true,
+        },
+      }),
     ],
   })
   .catch(() => process.exit(1))
