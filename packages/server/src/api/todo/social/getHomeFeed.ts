be30e27ef1b968654d35c2a055d809b2080f3abe
Devin Ivy <devinivy@gmail.com>
2022-10-10 17:05:02 -0400

    Convert home feed to kysely

--- packages/server/src/api/todo/social/getHomeFeed.ts
+++ packages/server/src/api/todo/social/getHomeFeed.ts
@@ -1,18 +1,13 @@
-import { Server } from '../../../lexicon'
+import { sql } from 'kysely'
 import { AuthRequiredError, InvalidRequestError } from '@adxp/xrpc-server'
+import { Server } from '../../../lexicon'
 import * as GetHomeFeed from '../../../lexicon/types/todo/social/getHomeFeed'
-import { FollowIndex } from '../../../db/records/follow'
-import { PostIndex } from '../../../db/records/post'
 import * as locals from '../../../locals'
-import { SelectQueryBuilder } from 'typeorm'
+import { FeedAlgorithm, isEnum, queryResultToFeedItem } from './util'
 import {
-  FeedAlgorithm,
-  isEnum,
-  queryPostsAndRepostsAsFeedItems,
-  queryPostsWithReposts,
-  queryResultToFeedItem,
-} from './util'
-import { postOrRepostIndexedAtClause } from '../../../db/util'
+  isNotRepostClause,
+  postOrRepostIndexedAtClause,
+} from '../../../db/util'
 
 export default function (server: Server) {
   server.todo.social.getHomeFeed(
@@ -34,61 +29,126 @@ export default function (server: Server) {
         throw new InvalidRequestError(`Unsupported algorithm: ${algorithm}`)
       }
 
-      const builder = db.db.createQueryBuilder().from(PostIndex, 'post')
-
-      // Determine result set of posts and reposts
-      if (feedAlgorithm === FeedAlgorithm.Firehose) {
-        // All posts, except requester's reposts
-        queryPostsWithReposts(builder).where(
-          // The null check handles ANSI nulls
-          '(repost.creator != :requester or repost.creator is null)',
-          { requester },
+      // @TODO break this query up, share parts with author feed
+      const builder = db.db
+        .selectFrom('todo_social_post as post')
+        // Determine result set of posts and reposts
+        .leftJoin('todo_social_repost as repost', 'repost.subject', 'post.uri')
+        .leftJoin('user as originator', (join) =>
+          join
+            .onRef('originator.did', '=', 'post.creator')
+            .orOnRef('originator.did', '=', 'repost.creator'),
         )
-      } else if (feedAlgorithm === FeedAlgorithm.ReverseChronological) {
-        // Followee's posts and reposts, and requester's posts
-        const followingIdsSubquery = (qb: SelectQueryBuilder<PostIndex>) => {
-          return qb
-            .subQuery()
-            .select('follow.subject')
-            .from(FollowIndex, 'follow')
-            .where('follow.creator = :did', { did: requester })
-            .getQuery()
-        }
-        queryPostsWithReposts(builder)
-          .where(`(repost.creator != :requester or repost.creator is null)`, {
-            requester,
-          })
-          .andWhere(
-            (qb) => {
-              return (
-                `(originator.did IN ${followingIdsSubquery(qb)} or ` +
-                `originator.did = :requester)`
-              )
-            },
-            { requester },
+        .if(feedAlgorithm === FeedAlgorithm.Firehose, (qb) => {
+          return qb.where(
+            // The null check handles ANSI nulls
+            sql`(repost.creator != :requester or repost.creator is null)`,
           )
-      } else {
-        const exhaustiveCheck: never = feedAlgorithm
-        throw new Error(`Unhandled case: ${exhaustiveCheck}`)
-      }
-
-      // Select data for presentation into FeedItem
-      queryPostsAndRepostsAsFeedItems(builder, { requester })
+        })
+        .if(feedAlgorithm === FeedAlgorithm.ReverseChronological, (qb) => {
+          const followingIdsSubquery = db.db
+            .selectFrom('todo_social_follow as follow')
+            .select('follow.subject')
+            .where('follow.creator', '=', requester)
+          return qb
+            .where(
+              sql`(repost.creator != :requester or repost.creator is null)`,
+            )
+            .where((qb) =>
+              qb
+                .where('originator.did', '=', requester)
+                .orWhere(`originator.did`, 'in', followingIdsSubquery),
+            )
+        })
+        // Select data for presentation into FeedItem
+        .leftJoin('user as author', 'author.did', 'post.creator')
+        .leftJoin(
+          'todo_social_profile as author_profile',
+          'author_profile.creator',
+          'author.did',
+        )
+        .leftJoin('user as reposted_by', 'reposted_by.did', 'repost.subject')
+        .leftJoin(
+          'todo_social_profile as reposted_by_profile',
+          'reposted_by_profile.creator',
+          'reposted_by.did',
+        )
+        .leftJoin('record', 'record.uri', 'post.uri')
+        .leftJoin(
+          db.db
+            .selectFrom('todo_social_like')
+            .select([
+              'todo_social_like.subject as subject',
+              db.db.fn.count('todo_social_like.uri').as('count'),
+            ])
+            .groupBy('subject')
+            .as('like_count'),
+          'like_count.subject',
+          'post.uri',
+        )
+        .leftJoin(
+          db.db
+            .selectFrom('todo_social_repost')
+            .select([
+              'todo_social_repost.subject as subject',
+              db.db.fn.count('todo_social_repost.uri').as('count'),
+            ])
+            .groupBy('subject')
+            .as('repost_count'),
+          'repost_count.subject',
+          'post.uri',
+        )
+        .leftJoin(
+          db.db
+            .selectFrom('todo_social_post')
+            .select([
+              'todo_social_post.replyParent as subject',
+              db.db.fn.count('todo_social_post.uri').as('count'),
+            ])
+            .groupBy('subject')
+            .as('reply_count'),
+          'reply_count.subject',
+          'post.uri',
+        )
+        .leftJoin('todo_social_repost as requester_repost', (join) =>
+          join
+            .on('requester_repost.creator', '=', requester)
+            .onRef('requester_repost.subject', '=', 'post.uri'),
+        )
+        .leftJoin('todo_social_like as requester_like', (join) =>
+          join
+            .on('requester_like.creator', '=', requester)
+            .onRef('requester_like.subject', '=', 'post.uri'),
+        )
+        .select([
+          'post.uri as uri',
+          'author.did as authorDid',
+          'author.username as authorName',
+          'author_profile.displayName as authorDisplayName',
+          'reposted_by.did as repostedByDid',
+          'reposted_by.username as repostedByName',
+          'reposted_by_profile.displayName as repostedByDisplayName',
+          sql`${isNotRepostClause}`.as('isNotRepost'),
+          'record.raw as rawRecord',
+          'like_count.count as likeCount',
+          'repost_count.count as repostCount',
+          'reply_count.count as replyCount',
+          'requester_repost.uri as requesterRepost',
+          'requester_like.uri as requesterLike',
+          'record.indexedAt as indexedAt',
+          sql`${postOrRepostIndexedAtClause}`.as('cursor'),
+        ])
         // Grouping by post then originator preserves one row for each
         // post or repost. Reposts of a given post only vary by originator.
-        .groupBy('post.uri')
-        .addGroupBy('originator.did')
+        .groupBy(['post.uri', 'originator.did'])
+        // Apply pagination
+        .orderBy(postOrRepostIndexedAtClause, 'desc')
+        .if(before !== undefined, (qb) =>
+          qb.where(postOrRepostIndexedAtClause, '<', before as string),
+        )
+        .if(limit !== undefined, (qb) => qb.limit(limit as number))
 
-      // Apply pagination
-      builder.orderBy(postOrRepostIndexedAtClause, 'DESC')
-      if (before !== undefined) {
-        builder.andWhere(`${postOrRepostIndexedAtClause} < :before`, { before })
-      }
-      if (limit !== undefined) {
-        builder.limit(limit)
-      }
-
-      const queryRes = await builder.getRawMany()
+      const queryRes = await builder.execute()
       const feed: GetHomeFeed.FeedItem[] = queryRes.map(queryResultToFeedItem)
 
       return { encoding: 'application/json', body: { feed } }
