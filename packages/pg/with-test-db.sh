4fe4b5d13eaa66bbac7cb708d5d2d306278643b4
Devin Ivy <devinivy@gmail.com>
2022-10-11 17:54:53 -0400

    Setup dockerization of postgres db

---
+++ packages/pg/with-test-db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Example usage:
+# ./with-test-db.sh psql postgresql://pg:password@localhost:5433/postgres -c 'select 1;'
+
+dir=$(dirname $0)
+compose_file="$dir/docker-compose.yaml"
+
+docker compose -f $compose_file up --wait db_test
+echo # newline
+
+# Based on creds in compose.yaml
+export DB_POSTGRES_URL="postgresql://pg:password@localhost:5433/postgres"
+"$@"
+code=$?
+
+echo # newline
+docker compose -f $compose_file rm -f --stop db_test
+
+exit $code
+q
