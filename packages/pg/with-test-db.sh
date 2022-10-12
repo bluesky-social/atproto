640874a82c0ddff50e8b6bcffb123ddd7dbe4a30
Devin Ivy <devinivy@gmail.com>
2022-10-12 14:30:59 -0400

    Ensure a more complete reset of containers/volumes in with-test-db.sh, set libpq env vars

--- packages/pg/with-test-db.sh
+++ packages/pg/with-test-db.sh
@@ -7,15 +7,20 @@
 dir=$(dirname $0)
 compose_file="$dir/docker-compose.yaml"
 
-docker compose -f $compose_file up --wait db_test
+docker compose -f $compose_file up --wait --force-recreate db_test
 echo # newline
 
 # Based on creds in compose.yaml
+export PGPORT=5433
+export PGHOST=localhost
+export PGUSER=pg
+export PGPASSWORD=password
+export PGDATABASE=postgres
 export DB_POSTGRES_URL="postgresql://pg:password@localhost:5433/postgres"
 "$@"
 code=$?
 
 echo # newline
-docker compose -f $compose_file rm -f --stop db_test
+docker compose -f $compose_file rm -f --stop --volumes db_test
 
 exit $code
