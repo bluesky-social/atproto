70dd7b1f01f61914e007b0529bf652b26d8e3a8a
dholms <dtholmgren@gmail.com>
2022-10-25 18:32:53 -0500

    comments

--- packages/aws-kms/src/index.ts
+++ packages/aws-kms/src/index.ts
@@ -33,6 +33,7 @@ export class KmsKeypair implements crypto.DidableKey {
     if (!res.PublicKey) {
       throw new Error('Could not find public key')
     }
+    // public key comes back DER-encoded, so we translate it to raw 65 byte encoding
     const rawPublicKeyHex = keyEncoder.encodePublic(
       Buffer.from(res.PublicKey),
       'der',
@@ -55,6 +56,11 @@ export class KmsKeypair implements crypto.DidableKey {
     if (!res.Signature) {
       throw new Error('Could not get signature')
     }
+    // signature comes back DER encoded & not-normalized
+    // we translate to raw 64 byte encoding
+    // we also normalize s as no more than 1/2 prime order to pass strict verification
+    // (prevents duplicating a signature)
+    // more: https://github.com/bitcoin-core/secp256k1/blob/a1102b12196ea27f44d6201de4d25926a2ae9640/include/secp256k1.h#L530-L534
     const sig = secp.Signature.fromDER(res.Signature)
     const normalized = sig.normalizeS()
     return normalized.toCompactRawBytes()
