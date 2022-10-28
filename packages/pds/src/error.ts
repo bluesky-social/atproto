b3966734debdb039f098a794fb37b955e01976de
devin ivy <devinivy@gmail.com>
2022-10-28 17:40:36 -0400

    Misc HTTP error fixes (#280)
    * Avoid double decode for plc params
    
    * Unify http error handling across plc and pds, avoid reflected xss, details of unexpected errors
    
    * Check headersSent in plc error handler
    
    * Test pds error handler in isolation

--- packages/pds/src/error.ts
+++ packages/pds/src/error.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from 'express'
+import { InternalServerError } from '@atproto/xrpc-server'
+import { ErrorRequestHandler } from 'express'
 import { httpLogger as log } from './logger'
 
-export const handler = (err: Error, _req: Request, res: Response) => {
-  log.error({ err }, 'unexpected internal server error')
-  res.status(500).send(err.message)
+export const handler: ErrorRequestHandler = (err, _req, res, next) => {
+  log.error(err, 'unexpected internal server error')
+  if (res.headersSent) {
+    return next(err)
+  }
+  const internalErr = new InternalServerError()
+  res.status(internalErr.type).json(internalErr.payload)
 }
