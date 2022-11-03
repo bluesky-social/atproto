b329263394162b6c46caad388f5c27154b868772
dholms <dtholmgren@gmail.com>
2022-11-03 17:11:23 -0500

    handle package

---
+++ packages/handle/src/index.ts
@@ -0,0 +1,54 @@
+import * as address from '@sideway/address'
+import { reservedSubdomains } from './reserved'
+
+export const ensureValid = (
+  handle: string,
+  availableUserDomains: string[],
+): void => {
+  if (handle.startsWith('did:')) {
+    throw new InvalidHandleError(
+      'Cannot register a handle that starts with `did:`',
+    )
+  }
+  const supportedDomain = availableUserDomains.find((domain) =>
+    handle.endsWith(domain),
+  )
+  if (!supportedDomain) {
+    throw new InvalidHandleError('Not a supported handle domain')
+  }
+  const front = handle.slice(0, handle.length - supportedDomain.length)
+  if (front.length < 2) {
+    throw new InvalidHandleError('Handle too short')
+  } else if (front.length > 20) {
+    throw new InvalidHandleError('Handle too long')
+  }
+
+  if (reservedSubdomains[front]) {
+    throw new InvalidHandleError('Reserved handle')
+  }
+
+  if (front.indexOf('.') > -1) {
+    throw new InvalidHandleError('Invalid characters in handle')
+  }
+
+  const isValid = address.isDomainValid(handle)
+  if (!isValid) {
+    throw new InvalidHandleError('Invalid characters in handle')
+  }
+}
+export const isValid = (
+  handle: string,
+  availableUserDomains: string[],
+): boolean => {
+  try {
+    ensureValid(handle, availableUserDomains)
+  } catch (err) {
+    if (err instanceof InvalidHandleError) {
+      return false
+    }
+    throw err
+  }
+  return true
+}
+
+export class InvalidHandleError extends Error {}
