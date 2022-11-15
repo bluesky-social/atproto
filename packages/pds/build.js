802471763246d6f23add7e4d27fa17d603b8e48f
Devin Ivy <devinivy@gmail.com>
2022-11-15 10:47:05 -0500

    Hoist update-main-to-dist to separate call, fix lex-cli and plc shallow builds

--- packages/pds/build.js
+++ packages/pds/build.js
@@ -2,31 +2,36 @@ const pkgJson = require('@npmcli/package-json')
 const { copy } = require('esbuild-plugin-copy')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
-const buildShallow = process.env.ATP_BUILD_SHALLOW === 'true'
+const buildShallow =
+  process.argv.includes('--shallow') || process.env.ATP_BUILD_SHALLOW === 'true'
 
-require('esbuild')
-  .build({
-    logLevel: 'info',
-    entryPoints: ['src/index.ts', 'src/bin.ts', 'src/db/index.ts'],
-    bundle: true,
-    outdir: 'dist',
-    platform: 'node',
-    external: [
-      'better-sqlite3',
-      'level',
-      'classic-level',
-      // Referenced in pg driver, but optional and we don't use it
-      'pg-native',
-    ],
-    plugins: [].concat(buildShallow ? [nodeExternalsPlugin()] : []).concat([
-      copy({
-        assets: {
-          from: ['./src/mailer/templates/**/*'],
-          to: ['./templates'],
-          keepStructure: true,
-        },
-      }),
-    ]),
-  })
-  .then(() => (buildShallow ? pkgJson.load(__dirname) : null))
-  .then((pkg) => (pkg?.update({ main: 'dist/index.js' }), pkg?.save()))
+if (process.argv.includes('--update-main-to-dist')) {
+  return pkgJson
+    .load(__dirname)
+    .then((pkg) => pkg.update({ main: 'dist/index.js' }))
+    .then((pkg) => pkg.save())
+}
+
+require('esbuild').build({
+  logLevel: 'info',
+  entryPoints: ['src/index.ts', 'src/bin.ts', 'src/db/index.ts'],
+  bundle: true,
+  outdir: 'dist',
+  platform: 'node',
+  external: [
+    'better-sqlite3',
+    'level',
+    'classic-level',
+    // Referenced in pg driver, but optional and we don't use it
+    'pg-native',
+  ],
+  plugins: [].concat(buildShallow ? [nodeExternalsPlugin()] : []).concat([
+    copy({
+      assets: {
+        from: ['./src/mailer/templates/**/*'],
+        to: ['./templates'],
+        keepStructure: true,
+      },
+    }),
+  ]),
+})
