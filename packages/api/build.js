582ae9ebaa3a05f1b51ca89a0b1b9e3dea52921a
Paul Frazee <pfrazee@gmail.com>
2022-12-22 10:08:20 -0600

    Separate the update-pkg script from build

--- packages/api/build.js
+++ packages/api/build.js
@@ -1,21 +1,7 @@
-const pkgJson = require('@npmcli/package-json')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
 const buildShallow =
   process.argv.includes('--shallow') || process.env.ATP_BUILD_SHALLOW === 'true'
-
-if (process.argv.includes('--update-main-to-dist')) {
-  return pkgJson
-    .load(__dirname)
-    .then((pkg) => pkg.update({ main: 'dist/index.js' }))
-    .then((pkg) => pkg.save())
-}
-if (process.argv.includes('--update-main-to-src')) {
-  return pkgJson
-    .load(__dirname)
-    .then((pkg) => pkg.update({ main: 'src/index.ts' }))
-    .then((pkg) => pkg.save())
-}
 
 require('esbuild').build({
   logLevel: 'info',
