bce8406e970f3e14e09fb2a3527b9f93da90e5a6
dholms <dtholmgren@gmail.com>
2023-02-01 14:22:04 -0600

    errors in storage instead of routes

--- packages/pds/src/api/com/atproto/sync.ts
+++ packages/pds/src/api/com/atproto/sync.ts
@@ -20,76 +20,72 @@ export default function (server: Server, ctx: AppContext) {
   })
 
   server.com.atproto.sync.getCommitPath(async ({ params }) => {
-    throw new Error('Method termporarily disabled')
-    // const { did } = params
-    // const storage = new SqlRepoStorage(ctx.db, did)
-    // const earliest = params.earliest ? CID.parse(params.earliest) : null
-    // const latest = params.latest
-    //   ? CID.parse(params.latest)
-    //   : await storage.getHead()
-    // if (latest === null) {
-    //   throw new InvalidRequestError(`Could not find root for DID: ${did}`)
-    // }
-    // const commitPath = await storage.getCommitPath(latest, earliest)
-    // if (commitPath === null) {
-    //   throw new InvalidRequestError(
-    //     `Could not find a valid commit path from ${latest.toString()} to ${earliest?.toString()}`,
-    //   )
-    // }
-    // const commits = commitPath.map((c) => c.toString())
-    // return {
-    //   encoding: 'application/json',
-    //   body: { commits },
-    // }
+    const { did } = params
+    const storage = new SqlRepoStorage(ctx.db, did)
+    const earliest = params.earliest ? CID.parse(params.earliest) : null
+    const latest = params.latest
+      ? CID.parse(params.latest)
+      : await storage.getHead()
+    if (latest === null) {
+      throw new InvalidRequestError(`Could not find root for DID: ${did}`)
+    }
+    const commitPath = await storage.getCommitPath(latest, earliest)
+    if (commitPath === null) {
+      throw new InvalidRequestError(
+        `Could not find a valid commit path from ${latest.toString()} to ${earliest?.toString()}`,
+      )
+    }
+    const commits = commitPath.map((c) => c.toString())
+    return {
+      encoding: 'application/json',
+      body: { commits },
+    }
   })
 
   server.com.atproto.sync.getRepo(async ({ params }) => {
-    throw new Error('Method termporarily disabled')
-    // const { did, from = null } = params
-    // const storage = new SqlRepoStorage(ctx.db, did)
-    // const head = await storage.getHead()
-    // if (head === null) {
-    //   throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
-    // }
-    // const fromCid = from ? CID.parse(from) : null
-    // const diff = await repo.getDiff(storage, head, fromCid)
-    // return {
-    //   encoding: 'application/vnd.ipld.car',
-    //   body: Buffer.from(diff),
-    // }
+    const { did, from = null } = params
+    const storage = new SqlRepoStorage(ctx.db, did)
+    const head = await storage.getHead()
+    if (head === null) {
+      throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
+    }
+    const fromCid = from ? CID.parse(from) : null
+    const diff = await repo.getDiff(storage, head, fromCid)
+    return {
+      encoding: 'application/vnd.ipld.car',
+      body: Buffer.from(diff),
+    }
   })
 
   server.com.atproto.sync.getCheckout(async ({ params }) => {
-    throw new Error('Method termporarily disabled')
-    // const { did } = params
-    // const storage = new SqlRepoStorage(ctx.db, did)
-    // const commit = params.commit
-    //   ? CID.parse(params.commit)
-    //   : await storage.getHead()
-    // if (!commit) {
-    //   throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
-    // }
-    // const checkout = await repo.getCheckout(storage, commit)
-    // return {
-    //   encoding: 'application/vnd.ipld.car',
-    //   body: Buffer.from(checkout),
-    // }
+    const { did } = params
+    const storage = new SqlRepoStorage(ctx.db, did)
+    const commit = params.commit
+      ? CID.parse(params.commit)
+      : await storage.getHead()
+    if (!commit) {
+      throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
+    }
+    const checkout = await repo.getCheckout(storage, commit)
+    return {
+      encoding: 'application/vnd.ipld.car',
+      body: Buffer.from(checkout),
+    }
   })
 
   server.com.atproto.sync.getRecord(async ({ params }) => {
-    throw new Error('Method termporarily disabled')
-    // const { did, collection, rkey } = params
-    // const storage = new SqlRepoStorage(ctx.db, did)
-    // const commit = params.commit
-    //   ? CID.parse(params.commit)
-    //   : await storage.getHead()
-    // if (!commit) {
-    //   throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
-    // }
-    // const proof = await repo.getRecords(storage, commit, [{ collection, rkey }])
-    // return {
-    //   encoding: 'application/vnd.ipld.car',
-    //   body: Buffer.from(proof),
-    // }
+    const { did, collection, rkey } = params
+    const storage = new SqlRepoStorage(ctx.db, did)
+    const commit = params.commit
+      ? CID.parse(params.commit)
+      : await storage.getHead()
+    if (!commit) {
+      throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
+    }
+    const proof = await repo.getRecords(storage, commit, [{ collection, rkey }])
+    return {
+      encoding: 'application/vnd.ipld.car',
+      body: Buffer.from(proof),
+    }
   })
 }
