f5ad5fb2540e59cc2b99962d48b74cd0c6678d99
bryan newbold <bnewbold@robocracy.org>
2023-02-07 21:52:43 -0800

    handle: overall and segment lengths; enforce ASCII; test dashes
    The 'address' package we are using to check domains doesn't seem to do
    any overall length checks. The values and examples i'm testing here are
    based on general domain limits. Technically a web domain could be 255,
    it is email domains limited to 253 chars, but that seems like a
    reasonable value to cut off at.

--- packages/handle/tests/handle.test.ts
+++ packages/handle/tests/handle.test.ts
@@ -10,7 +10,20 @@ describe('handle validation', () => {
     check('john.test')
     check('jan.test')
     check('a234567890123456789.test')
+    check('john2.test')
+    check('john-john.test')
+    check('john-.test')
     check('john.bsky.app')
+    check('0john.test')
+    check('12345.test')
+    check(
+      'short.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.test',
+    )
+  })
+
+  it('allows punycode handles', () => {
+    check('xn--ls8h.test') // 💩.test
+    check('xn--bcher-kva.tld') // bücher.tld
   })
 
   it('throws on invalid handles', () => {
@@ -22,16 +35,32 @@ describe('handle validation', () => {
     expect(check('j.test')).toThrow('Handle too short')
     expect(check('uk.test')).toThrow('Handle too short')
     expect(check('jaymome-johnber123456.test')).toThrow('Handle too long')
+    expect(check('jay.mome-johnber123456.test')).toThrow('Handle too long')
+    expect(
+      check(
+        'short.loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.test',
+      ),
+    ).toThrow('Handle too long')
+    expect(
+      check(
+        'short.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.test',
+      ),
+    ).toThrow('Handle too long')
     expect(check('john.test.bsky.app')).toThrow('Invalid characters in handle')
     expect(check('john..test')).toThrow('Invalid characters in handle')
     expect(check('jo_hn.test')).toThrow('Invalid characters in handle')
+    expect(check('-john.test')).toThrow('Invalid characters in handle')
+    expect(check('.john.test')).toThrow('Invalid characters in handle')
     expect(check('jo!hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo%hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo&hn.test')).toThrow('Invalid characters in handle')
+    expect(check('jo@hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo*hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo|hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo:hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo/hn.test')).toThrow('Invalid characters in handle')
+    expect(check('john💩.test')).toThrow('Invalid characters in handle')
+    expect(check('bücher.test')).toThrow('Invalid characters in handle')
     expect(check('about.test')).toThrow('Reserved handle')
     expect(check('atp.test')).toThrow('Reserved handle')
     expect(check('barackobama.test')).toThrow('Reserved handle')
