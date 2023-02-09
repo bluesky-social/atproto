6278577cec7279db8a2a6709bbd8a3ef443d3921
dholms <dtholmgren@gmail.com>
2023-02-08 18:12:14 -0600

    migration

--- packages/pds/src/db/migrations/20230208T222001557Z-user-table-did-pk.ts
+++ packages/pds/src/db/migrations/20230208T222001557Z-user-table-did-pk.ts
@@ -39,10 +39,63 @@ export async function up(db: Kysely<any>): Promise<void> {
     )
     .execute()
 
+  await db.schema
+    .createIndex('user_account_password_reset_token_idx')
+    .unique()
+    .on('user_account')
+    .column('passwordResetToken')
+    .execute()
+
   await db.schema.dropIndex('user_password_reset_token_idx').execute()
   await db.schema.dropTable('user').execute()
 }
 
-export async function down(db: Kysely<unknown>): Promise<void> {
-  // Migration code
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('user')
+    .addColumn('handle', 'varchar', (col) => col.primaryKey())
+    .addColumn('email', 'varchar', (col) => col.notNull().unique())
+    .addColumn('password', 'varchar', (col) => col.notNull())
+    .addColumn('lastSeenNotifs', 'varchar', (col) => col.notNull())
+    .addColumn('createdAt', 'varchar', (col) => col.notNull())
+    .addColumn('passwordResetToken', 'varchar')
+    .addColumn('passwordResetGrantedAt', 'varchar')
+    .execute()
+
+  await db
+    .insertInto('user')
+    .columns([
+      'handle',
+      'email',
+      'password',
+      'lastSeenNotifs',
+      'createdAt',
+      'passwordResetToken',
+      'passwordResetGrantedAt',
+    ])
+    .expression((exp) =>
+      exp
+        .selectFrom('user_account')
+        .innerJoin('did_handle', 'did_handle.did', 'user_account.did')
+        .select([
+          'did_handle.handle',
+          'user_account.email',
+          'user_account.password',
+          'user_account.lastSeenNotifs',
+          'user_account.createdAt',
+          'user_account.passwordResetToken',
+          'user_account.passwordResetGrantedAt',
+        ]),
+    )
+    .execute()
+
+  await db.schema
+    .createIndex('user_password_reset_token_idx')
+    .unique()
+    .on('user')
+    .column('passwordResetToken')
+    .execute()
+
+  await db.schema.dropIndex('user_account_password_reset_token_idx').execute()
+  await db.schema.dropTable('user_account').execute()
 }
