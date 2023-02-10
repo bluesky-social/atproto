a3c1105acaae1bd6c07af2f8be1cb16cff05442c
bnewbold <bnewbold@robocracy.org>
2023-02-09 17:58:42 -0800

    handles: reserve some top-100 handles; prevent new 2-char handle registrations (#531)
    * handle: disallow new 2-char handle registration
    
    This should not impact existing handles, only new registrations.
    
    We discussed blocking these to prevent possible future URL path
    collisions with, eg, ISO country or language codes.
    
    * handle: reserve some more famous handles
    
    This isn't the full list of thousands of twitter handles that have more
    than 5 million or more than 1 million followers. Instead, it is only the
    top 100 or so, plus a handful of additional top-50 instagram accounts (when
    different), and a couple recent famous figures. Pretty subjective.
    
    * handle: overall and segment lengths; enforce ASCII; test dashes
    
    The 'address' package we are using to check domains doesn't seem to do
    any overall length checks. The values and examples i'm testing here are
    based on general domain limits. Technically a web domain could be 255,
    it is email domains limited to 253 chars, but that seems like a
    reasonable value to cut off at.

--- packages/handle/tests/handle.test.ts
+++ packages/handle/tests/handle.test.ts
@@ -8,7 +8,22 @@ describe('handle validation', () => {
 
   it('allows valid handles', () => {
     check('john.test')
+    check('jan.test')
+    check('a234567890123456789.test')
+    check('john2.test')
+    check('john-john.test')
+    check('john-.test')
     check('john.bsky.app')
+    check('0john.test')
+    check('12345.test')
+    check(
+      'short.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.test',
+    )
+  })
+
+  it('allows punycode handles', () => {
+    check('xn--ls8h.test') // 💩.test
+    check('xn--bcher-kva.tld') // bücher.tld
   })
 
   it('throws on invalid handles', () => {
@@ -18,19 +33,37 @@ describe('handle validation', () => {
     expect(check('john.bsky.io')).toThrow('Not a supported handle domain')
     expect(check('john.com')).toThrow('Not a supported handle domain')
     expect(check('j.test')).toThrow('Handle too short')
+    expect(check('uk.test')).toThrow('Handle too short')
     expect(check('jaymome-johnber123456.test')).toThrow('Handle too long')
+    expect(check('jay.mome-johnber123456.test')).toThrow('Handle too long')
+    expect(
+      check(
+        'short.loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.test',
+      ),
+    ).toThrow('Handle too long')
+    expect(
+      check(
+        'short.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.loooooooooooooooooooooooong.test',
+      ),
+    ).toThrow('Handle too long')
     expect(check('john.test.bsky.app')).toThrow('Invalid characters in handle')
     expect(check('john..test')).toThrow('Invalid characters in handle')
     expect(check('jo_hn.test')).toThrow('Invalid characters in handle')
+    expect(check('-john.test')).toThrow('Invalid characters in handle')
+    expect(check('.john.test')).toThrow('Invalid characters in handle')
     expect(check('jo!hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo%hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo&hn.test')).toThrow('Invalid characters in handle')
+    expect(check('jo@hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo*hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo|hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo:hn.test')).toThrow('Invalid characters in handle')
     expect(check('jo/hn.test')).toThrow('Invalid characters in handle')
+    expect(check('john💩.test')).toThrow('Invalid characters in handle')
+    expect(check('bücher.test')).toThrow('Invalid characters in handle')
     expect(check('about.test')).toThrow('Reserved handle')
     expect(check('atp.test')).toThrow('Reserved handle')
+    expect(check('barackobama.test')).toThrow('Reserved handle')
   })
 
   it('normalizes handles', () => {
