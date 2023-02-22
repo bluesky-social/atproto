ed2eea46c61c020337b2610e92904c119caeddc2
bryan newbold <bnewbold@robocracy.org>
2023-02-21 23:27:30 -0800

    identifiers: add DID validation

--- packages/lexicon/tests/identifiers.test.ts
+++ packages/lexicon/tests/identifiers.test.ts
@@ -3,6 +3,8 @@ import {
   lexVerifyHandleRegex,
   lexVerifyNsid,
   lexVerifyNsidRegex,
+  lexVerifyDid,
+  lexVerifyDidRegex,
 } from '../src/identifiers'
 
 describe('handle permissive validation', () => {
@@ -193,5 +195,66 @@ describe('NSID permissive validation', () => {
 
   it('handles corner-cases which nsid package does not', () => {
     expectInvalid('com.example-.foo')
+  })
+})
+
+describe('DID permissive validation', () => {
+  const expectValid = (h: string) => {
+    lexVerifyDid(h)
+    lexVerifyDidRegex(h)
+  }
+  const expectInvalid = (h: string) => {
+    expect(() => lexVerifyDid(h)).toThrow()
+    expect(() => lexVerifyDidRegex(h)).toThrow()
+  }
+
+  it('enforces spec details', () => {
+    expectValid('did:method:val')
+    expectValid('did:method:VAL')
+    expectValid('did:method:val123')
+    expectValid('did:method:123')
+    expectValid('did:method:val-two')
+    expectValid('did:method:val_two')
+    expectValid('did:method:val.two')
+    expectValid('did:method:val:two')
+    expectValid('did:method:val%BB')
+
+    expectInvalid('did')
+    expectInvalid('didmethodval')
+    expectInvalid('method:did:val')
+    expectInvalid('did:method:')
+    expectInvalid('didmethod:val')
+    expectInvalid('did:methodval')
+    expectInvalid(':did:method:val')
+    expectInvalid('did.method.val')
+    expectInvalid('did:method:val:')
+    expectInvalid('did:method:val%')
+    expectInvalid('DID:method:val')
+    expectInvalid('did:METHOD:val')
+    expectInvalid('did:m123:val')
+
+    expectValid('did:method:' + 'v'.repeat(240))
+    expectInvalid('did:method:' + 'v'.repeat(8500))
+
+    expectValid('did:m:v')
+    expectValid('did:method::::val')
+    expectValid('did:method:-')
+    expectValid('did:method:-:_:.:%ab')
+    expectValid('did:method:.')
+    expectValid('did:method:_')
+    expectValid('did:method::.')
+
+    expectInvalid('did:method:val/two')
+    expectInvalid('did:method:val?two')
+    expectInvalid('did:method:val#two')
+    expectInvalid('did:method:val%')
+  })
+
+  it('allows some real DID values', () => {
+    expectValid('did:example:123456789abcdefghi')
+    expectValid('did:plc:7iza6de2dwap2sbkpav7c6c6')
+    expectValid('did:web:example.com')
+    expectValid('did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N')
+    expectValid('did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a')
   })
 })
