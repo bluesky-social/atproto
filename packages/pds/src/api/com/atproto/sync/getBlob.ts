3ce2d0ef226be39bcad084270f6dcaef4cc8fd11
dholms <dtholmgren@gmail.com>
2023-02-28 17:35:44 -0800

    allow mimetype on getBlob

--- packages/pds/src/api/com/atproto/sync/getBlob.ts
+++ packages/pds/src/api/com/atproto/sync/getBlob.ts
@@ -6,8 +6,9 @@ export default function (server: Server, ctx: AppContext) {
   server.com.atproto.sync.getBlob(async ({ params }) => {
     const cid = CID.parse(params.cid)
     const blobStream = await ctx.blobstore.getStream(cid)
+    // @TODO better codegen for */* mimetype
     return {
-      encoding: 'application/octet-stream',
+      encoding: 'application/octet-stream' as any,
       body: blobStream,
     }
   })
