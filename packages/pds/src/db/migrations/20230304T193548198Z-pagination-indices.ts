639800d983c49e49d1fbd6e16f6e8b88541a23bc
dholms <dtholmgren@gmail.com>
2023-03-04 12:20:16 -0800

    remove creator indices

--- packages/pds/src/db/migrations/20230304T193548198Z-pagination-indices.ts
+++ packages/pds/src/db/migrations/20230304T193548198Z-pagination-indices.ts
@@ -2,24 +2,6 @@ import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
-    .createIndex('follow_creator_idx')
-    .on('follow')
-    .column('creator')
-    .execute()
-
-  await db.schema
-    .createIndex('vote_creator_idx')
-    .on('vote')
-    .column('creator')
-    .execute()
-
-  await db.schema
-    .createIndex('repost_creator_idx')
-    .on('repost')
-    .column('creator')
-    .execute()
-
-  await db.schema
     .createIndex('post_order_by_idx')
     .on('post')
     .columns(['indexedAt', 'cid'])
@@ -35,7 +17,4 @@ export async function up(db: Kysely<unknown>): Promise<void> {
 export async function down(db: Kysely<unknown>): Promise<void> {
   await db.schema.dropIndex('repost_order_by_idx').execute()
   await db.schema.dropIndex('post_order_by_idx').execute()
-  await db.schema.dropIndex('repost_creator_idx').execute()
-  await db.schema.dropIndex('vote_creator_idx').execute()
-  await db.schema.dropIndex('follow_creator_idx').execute()
 }
