8b68577a255083307c7f11197b246fc875be5903
dholms <dtholmgren@gmail.com>
2023-03-04 11:53:02 -0800

    add orderby indices

--- packages/pds/src/db/migrations/20230304T193548198Z-creator-indices.ts
+++ packages/pds/src/db/migrations/20230304T193548198Z-creator-indices.ts
@@ -18,9 +18,23 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     .on('repost')
     .column('creator')
     .execute()
+
+  await db.schema
+    .createIndex('post_order_by_idx')
+    .on('post')
+    .columns(['indexedAt', 'cid'])
+    .execute()
+
+  await db.schema
+    .createIndex('repost_order_by_idx')
+    .on('repost')
+    .columns(['indexedAt', 'cid'])
+    .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
+  await db.schema.dropIndex('repost_order_by_idx').execute()
+  await db.schema.dropIndex('post_order_by_idx').execute()
   await db.schema.dropIndex('repost_creator_idx').execute()
   await db.schema.dropIndex('vote_creator_idx').execute()
   await db.schema.dropIndex('follow_creator_idx').execute()
