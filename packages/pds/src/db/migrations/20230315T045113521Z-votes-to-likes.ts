bd9ea6e4e15240276e02b2b10cba8171577b808c
Devin Ivy <devinivy@gmail.com>
2023-03-15 09:29:29 -0400

    Fix-up votes-to-likes db migration

--- packages/pds/src/db/migrations/20230315T045113521Z-votes-to-likes.ts
+++ packages/pds/src/db/migrations/20230315T045113521Z-votes-to-likes.ts
@@ -1,4 +1,4 @@
-import { Kysely } from 'kysely'
+import { Kysely, sql } from 'kysely'
 
 export async function up(db: Kysely<Schema>): Promise<void> {
   // Nix downvotes from index
@@ -7,32 +7,104 @@ export async function up(db: Kysely<Schema>): Promise<void> {
     .deleteFrom('duplicate_record')
     .where('duplicateOf', 'in', downvotesQb.select('vote.uri'))
     .execute()
-  await db.deleteFrom('vote').where('direction', '=', 'down').execute()
-  // Drop vote indexes, direction column, and rename table
-  await db.schema.dropIndex('vote_subject_direction_idx').execute()
-  await db.schema.dropIndex('vote_unique_subject').execute()
-  await db.schema.alterTable('vote').dropColumn('direction').execute()
-  await db.schema.alterTable('vote').renameTo('like').execute()
-  // Recreate like indexes
   await db.schema
-    .createIndex('like_unique_subject')
-    .on('like')
-    .columns(['creator', 'subject'])
+    .createTable('like')
+    .addColumn('uri', 'varchar', (col) => col.primaryKey())
+    .addColumn('cid', 'varchar', (col) => col.notNull())
+    .addColumn('creator', 'varchar', (col) => col.notNull())
+    .addColumn('subject', 'varchar', (col) => col.notNull())
+    .addColumn('subjectCid', 'varchar', (col) => col.notNull())
+    .addColumn('createdAt', 'varchar', (col) => col.notNull())
+    .addColumn('indexedAt', 'varchar', (col) => col.notNull())
+    // Aids in index uniqueness plus post like counts
+    .addUniqueConstraint('like_unique_subject', ['subject', 'creator'])
     .execute()
+  await db
+    .insertInto('like')
+    .columns([
+      'uri',
+      'cid',
+      'creator',
+      'subject',
+      'subjectCid',
+      'createdAt',
+      'indexedAt',
+    ])
+    .expression((exp) =>
+      exp
+        .selectFrom('vote')
+        .where('direction', '=', 'up')
+        .select([
+          'uri',
+          'cid',
+          'creator',
+          'subject',
+          'subjectCid',
+          'createdAt',
+          'indexedAt',
+        ]),
+    )
+    .execute()
+  const missing = await db
+    .selectFrom('vote')
+    .select(sql<number>`count(*)`.as('count'))
+    .where('direction', '=', 'up')
+    .whereNotExists(
+      db
+        .selectFrom('like')
+        .selectAll()
+        .whereRef('uri', '=', db.dynamic.ref('vote.uri')),
+    )
+    .executeTakeFirstOrThrow()
+  if (missing.count !== 0) {
+    throw new Error(
+      `Likes were not migrated properly from votes: ${missing.count} likes missing.`,
+    )
+  }
+  await db.schema.dropTable('vote').execute()
 }
 
-export async function down(db: Kysely<unknown>): Promise<void> {
-  await db.schema.dropIndex('like_unique_subject').execute()
-  await db.schema.alterTable('like').renameTo('vote').execute()
+export async function down(db: Kysely<Schema>): Promise<void> {
   await db.schema
-    .alterTable('vote')
-    .addColumn('direction', 'varchar', (col) => col.notNull().defaultTo('up'))
+    .createTable('vote')
+    .addColumn('uri', 'varchar', (col) => col.primaryKey())
+    .addColumn('cid', 'varchar', (col) => col.notNull())
+    .addColumn('creator', 'varchar', (col) => col.notNull())
+    .addColumn('direction', 'varchar', (col) => col.notNull())
+    .addColumn('subject', 'varchar', (col) => col.notNull())
+    .addColumn('subjectCid', 'varchar', (col) => col.notNull())
+    .addColumn('createdAt', 'varchar', (col) => col.notNull())
+    .addColumn('indexedAt', 'varchar', (col) => col.notNull())
+    .addUniqueConstraint('vote_unique_subject', ['creator', 'subject'])
     .execute()
-  await db.schema
-    .createIndex('vote_unique_subject')
-    .on('vote')
-    .columns(['creator', 'subject'])
+  await db
+    .insertInto('vote')
+    .columns([
+      'uri',
+      'cid',
+      'creator',
+      'direction',
+      'subject',
+      'subjectCid',
+      'createdAt',
+      'indexedAt',
+    ])
+    .expression((exp) =>
+      exp
+        .selectFrom('like')
+        .select([
+          'uri',
+          'cid',
+          'creator',
+          sql`${'up'}`.as('direction'),
+          'subject',
+          'subjectCid',
+          'createdAt',
+          'indexedAt',
+        ]),
+    )
     .execute()
+  await db.schema.dropTable('like').execute()
   await db.schema
     .createIndex('vote_subject_direction_idx')
     .on('vote')
@@ -42,12 +114,19 @@ export async function down(db: Kysely<unknown>): Promise<void> {
 
 type Schema = {
   vote: Vote
+  like: Like
   duplicate_record: DuplicateRecord
 }
 
 type Vote = {
   uri: string
   direction: 'up' | 'down'
+  [k: string]: unknown
+}
+
+type Like = {
+  uri: string
+  [k: string]: unknown
 }
 
 type DuplicateRecord = {
