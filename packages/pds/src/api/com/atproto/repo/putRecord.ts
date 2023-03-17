25685389a988b653a627e3d0cbc5d5edeac34b57
Devin Ivy <devinivy@gmail.com>
2023-03-17 01:04:52 -0400

    Implementation and tests for putRecord

--- packages/pds/src/api/com/atproto/repo/putRecord.ts
+++ packages/pds/src/api/com/atproto/repo/putRecord.ts
@@ -1,9 +1,67 @@
-import { InvalidRequestError } from '@atproto/xrpc-server'
+import { AtUri } from '@atproto/uri'
+import { ForbiddenError, InvalidRequestError } from '@atproto/xrpc-server'
 import { Server } from '../../../../lexicon'
+import { ids } from '../../../../lexicon/lexicons'
+import * as repo from '../../../../repo'
 import AppContext from '../../../../context'
+import {
+  InvalidRecordError,
+  PreparedCreate,
+  PreparedUpdate,
+} from '../../../../repo'
 
-export default function (server: Server, _ctx: AppContext) {
-  server.com.atproto.repo.putRecord(async () => {
-    throw new InvalidRequestError(`Updates are not yet supported.`)
+export default function (server: Server, ctx: AppContext) {
+  server.com.atproto.repo.putRecord({
+    auth: ctx.accessVerifierCheckTakedown,
+    handler: async ({ auth, input }) => {
+      const { did, collection, rkey, record, validate } = input.body
+
+      if (did !== auth.credentials.did) {
+        throw new ForbiddenError()
+      }
+      if (collection !== ids.AppBskyActorProfile || rkey !== 'self') {
+        // @TODO temporary
+        throw new InvalidRequestError(
+          `Temporarily only accepting puts for ${ids.AppBskyActorProfile}/self.`,
+        )
+      }
+      if (validate === false) {
+        throw new InvalidRequestError(
+          'Unvalidated writes are not yet supported.',
+        )
+      }
+
+      const write = await ctx.db.transaction(async (dbTxn) => {
+        const now = new Date().toISOString()
+        const uri = AtUri.make(did, collection, rkey)
+        const recordTxn = ctx.services.record(dbTxn)
+        const repoTxn = ctx.services.repo(dbTxn)
+        const writeInfo = { did, collection, rkey, record, validate }
+        const current = await recordTxn.getRecord(uri, null, true)
+
+        let write: PreparedCreate | PreparedUpdate
+        try {
+          write = current
+            ? await repo.prepareUpdate(writeInfo)
+            : await repo.prepareCreate(writeInfo)
+        } catch (err) {
+          if (err instanceof InvalidRecordError) {
+            throw new InvalidRequestError(err.message)
+          }
+          throw err
+        }
+
+        await repoTxn.processWrites(did, [write], now)
+        return write
+      })
+
+      return {
+        encoding: 'application/json',
+        body: {
+          uri: write.uri.toString(),
+          cid: write.cid.toString(),
+        },
+      }
+    },
   })
 }
