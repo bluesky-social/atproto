829563f3023b88fd4c98508773a2dab3703ba38f
dholms <dtholmgren@gmail.com>
2023-03-29 14:48:29 -0600

    another test

--- packages/common/tests/ipld.test.ts
+++ packages/common/tests/ipld.test.ts
@@ -1,6 +1,12 @@
 import * as ui8 from 'uint8arrays'
 import vectors from './ipld-vectors'
-import { cborEncode, cidForCbor, ipldToJson, jsonToIpld } from '../src'
+import {
+  cborDecode,
+  cborEncode,
+  cidForCbor,
+  ipldToJson,
+  jsonToIpld,
+} from '../src'
 
 describe('ipld', () => {
   for (const vector of vectors) {
@@ -8,8 +14,11 @@ describe('ipld', () => {
       const ipld = jsonToIpld(vector.json)
       const json = ipldToJson(ipld)
       const cbor = cborEncode(ipld)
+      const ipldAgain = cborDecode(cbor)
+      const jsonAgain = ipldToJson(ipldAgain)
       const cid = await cidForCbor(ipld)
       expect(json).toEqual(vector.json)
+      expect(jsonAgain).toEqual(vector.json)
       expect(ui8.equals(cbor, vector.cbor)).toBeTruthy()
       expect(cid.toString()).toEqual(vector.cid)
     })
