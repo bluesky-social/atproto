d795d70028253f7a779e63717ea4a969f1156da6
Devin Ivy <devinivy@gmail.com>
2023-03-30 14:49:00 -0400

    Update bsky feed service for lex refactor

--- packages/bsky/src/api/app/bsky/util/index.ts
+++ packages/bsky/src/api/app/bsky/util/index.ts
@@ -1,23 +1,6 @@
 import express from 'express'
 import { AuthRequiredError } from '@atproto/xrpc-server'
 
-export const isEnum = <T extends { [s: string]: unknown }>(
-  object: T,
-  possibleValue: unknown,
-): possibleValue is T[keyof T] => {
-  return Object.values(object).includes(possibleValue)
-}
-
-// @TODO(lex) remove declaration, stubbed for now
-export const getDeclarationSimple = (): Declaration => {
-  return {
-    actorType: '',
-    cid: '',
-  }
-}
-
-export type Declaration = { cid: string; actorType: string }
-
 // @TODO(bsky) treating did as a bearer, just a placeholder for now.
 export const authVerifier = (ctx: {
   req: express.Request
