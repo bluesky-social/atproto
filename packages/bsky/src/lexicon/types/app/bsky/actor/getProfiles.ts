d05b9053fc63aed46e8c9b49b32bc260c71e1506
Devin Ivy <devinivy@gmail.com>
2023-03-30 12:37:32 -0400

    Update bsky lexicons for lex refactor

--- packages/bsky/src/lexicon/types/app/bsky/actor/getProfiles.ts
+++ packages/bsky/src/lexicon/types/app/bsky/actor/getProfiles.ts
@@ -2,11 +2,12 @@
  * GENERATED CODE - DO NOT MODIFY
  */
 import express from 'express'
-import { ValidationResult } from '@atproto/lexicon'
+import { ValidationResult, BlobRef } from '@atproto/lexicon'
 import { lexicons } from '../../../../lexicons'
 import { isObj, hasProp } from '../../../../util'
+import { CID } from 'multiformats/cid'
 import { HandlerAuth } from '@atproto/xrpc-server'
-import * as AppBskyActorProfile from './profile'
+import * as AppBskyActorDefs from './defs'
 
 export interface QueryParams {
   actors: string[]
@@ -15,7 +16,7 @@ export interface QueryParams {
 export type InputSchema = undefined
 
 export interface OutputSchema {
-  profiles: AppBskyActorProfile.View[]
+  profiles: AppBskyActorDefs.ProfileViewDetailed[]
   [k: string]: unknown
 }
 
