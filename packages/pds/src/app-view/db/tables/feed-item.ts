f6e9cbf9efe924a72ca0ef014fe4c53b20d5d6ee
Devin Ivy <devinivy@gmail.com>
2023-04-06 16:58:25 -0400

    Add table for feed_item to pds

---
+++ packages/pds/src/app-view/db/tables/feed-item.ts
@@ -0,0 +1,13 @@
+export const tableName = 'feed_item'
+
+export interface FeedItem {
+  uri: string
+  cid: string
+  type: 'post' | 'repost'
+  postUri: string
+  postAuthorDid: string
+  originatorDid: string
+  sortAt: string
+}
+
+export type PartialDB = { [tableName]: FeedItem }
