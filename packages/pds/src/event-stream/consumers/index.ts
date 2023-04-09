c018f1b44c5db822cffd05da7bd4936b527edd49
dholms <dtholmgren@gmail.com>
2023-04-09 12:46:46 -0700

    setting up user

--- packages/pds/src/event-stream/consumers/index.ts
+++ packages/pds/src/event-stream/consumers/index.ts
@@ -1,5 +1,28 @@
 import AppContext from '../../context'
+import Database from '../../db'
+import { LabelPost } from '../messages'
+import { Consumer } from '../types'
 
-export const listen = (_ctx: AppContext) => {
-  // does nothing now but will be used for moderation
+export const listen = (ctx: AppContext) => {
+  ctx.messageQueue.listen('label_post', new LabelPostConsumer())
+}
+
+class LabelPostConsumer extends Consumer<LabelPost> {
+  async dispatch(ctx: { db: Database; message: LabelPost }) {
+    // @TODO generate labels
+    // const { db, message } = ctx
+    // await db.db
+    //   .insertInto('user_notification')
+    //   .values({
+    //     userDid: message.userDid,
+    //     recordUri: message.recordUri,
+    //     recordCid: message.recordCid,
+    //     author: message.author,
+    //     reason: message.reason,
+    //     reasonSubject: message.reasonSubject,
+    //     indexedAt: new Date().toISOString(),
+    //   })
+    //   .returningAll()
+    //   .execute()
+  }
 }
