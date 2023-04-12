0762e8b7f62faba510d4e4cf912a4eefeaa5a962
Daniel Holmgren <dtholmgren@gmail.com>
2023-04-12 17:23:32 -0500

    Label bugfixes (#802)
    * some bugfixing in labels
    
    * hardcode profile nsid

--- packages/pds/src/app-view/services/label/index.ts
+++ packages/pds/src/app-view/services/label/index.ts
@@ -1,6 +1,7 @@
 import { AtUri } from '@atproto/uri'
 import Database from '../../../db'
 import { Label } from '../../../lexicon/types/com/atproto/label/defs'
+import { ids } from '../../../lexicon/lexicons'
 
 export type Labels = Record<string, Label[]>
 
@@ -11,7 +12,7 @@ export class LabelService {
     return (db: Database) => new LabelService(db)
   }
 
-  async getLabels(subjects: string[]): Promise<Labels> {
+  async getLabelsForSubjects(subjects: string[]): Promise<Labels> {
     if (subjects.length < 1) return {}
     const res = await this.db.db
       .selectFrom('label')
@@ -23,7 +24,7 @@ export class LabelService {
       acc[cur.subjectUri].push({
         src: cur.sourceDid,
         uri: cur.subjectUri,
-        cid: cur.subjectCid,
+        cid: cur.subjectCid ?? undefined,
         val: cur.value,
         neg: cur.negated === 1, // @TODO update in appview
         cts: cur.createdAt,
@@ -35,9 +36,26 @@ export class LabelService {
   async getLabelsForProfiles(dids: string[]): Promise<Labels> {
     if (dids.length < 1) return {}
     const profileUris = dids.map((did) =>
-      AtUri.make(did, 'app.bsky.feed.profile', 'self').toString(),
+      AtUri.make(did, ids.AppBskyActorProfile, 'self').toString(),
     )
     const subjects = [...dids, ...profileUris]
-    return this.getLabels(subjects)
+    const labels = await this.getLabelsForSubjects(subjects)
+    // combine labels for profile + did
+    return Object.keys(labels).reduce((acc, cur) => {
+      const did = cur.startsWith('at://') ? new AtUri(cur).hostname : cur
+      acc[did] ??= []
+      acc[did] = [...acc[did], ...labels[cur]]
+      return acc
+    }, {} as Labels)
+  }
+
+  async getLabels(subject: string): Promise<Label[]> {
+    const labels = await this.getLabelsForSubjects([subject])
+    return labels[subject] ?? []
+  }
+
+  async getLabelsForProfile(did: string): Promise<Label[]> {
+    const labels = await this.getLabelsForProfiles([did])
+    return labels[did] ?? []
   }
 }
