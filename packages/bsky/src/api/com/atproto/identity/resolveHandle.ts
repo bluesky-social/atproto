77dac469142da1306dc62d2a28556f25a6c8de70
Devin Ivy <devinivy@gmail.com>
2023-04-18 17:29:01 -0400

    Implement resolve handle on appview

---
+++ packages/bsky/src/api/com/atproto/identity/resolveHandle.ts
@@ -0,0 +1,28 @@
+import { InvalidRequestError } from '@atproto/xrpc-server'
+import * as ident from '@atproto/identifier'
+import { Server } from '../../../../lexicon'
+import AppContext from '../../../../context'
+import { resolveExternalHandle } from '../../../../util/identity'
+
+export default function (server: Server, ctx: AppContext) {
+  server.com.atproto.identity.resolveHandle(async ({ req, params }) => {
+    const handle = ident.normalizeHandle(params.handle || req.hostname)
+
+    let did: string | undefined
+    const user = await ctx.services.actor(ctx.db).getActor(handle, true)
+    if (user) {
+      did = user.did
+    } else {
+      // this is not someone on our server, but we help with resolving anyway
+      did = await resolveExternalHandle(handle)
+    }
+    if (!did) {
+      throw new InvalidRequestError('Unable to resolve handle')
+    }
+
+    return {
+      encoding: 'application/json',
+      body: { did },
+    }
+  })
+}
