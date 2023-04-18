9e1bb49957443e92505d572f1684d0f8799d6e2d
Daniel Holmgren <dtholmgren@gmail.com>
2023-04-18 10:44:34 -0500

    Create invite codes for many accounts (#825)
    * create invite codes for many accounts
    
    * test

--- packages/pds/src/api/com/atproto/server/createInviteCodes.ts
+++ packages/pds/src/api/com/atproto/server/createInviteCodes.ts
@@ -1,34 +1,48 @@
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
 import { genInvCodes } from './util'
+import { InviteCode } from '../../../../db/tables/invite-code'
+import { AccountCodes } from '../../../../lexicon/types/com/atproto/server/createInviteCodes'
+import { chunkArray } from '@atproto/common'
 
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.server.createInviteCodes({
     auth: ctx.adminVerifier,
     handler: async ({ input, req }) => {
-      const { codeCount, useCount, forAccount = 'admin' } = input.body
+      const { codeCount, useCount } = input.body
 
-      const codes = genInvCodes(ctx.cfg, codeCount)
+      const forAccounts = input.body.forAccounts ?? ['admin']
 
-      const vals = codes.map((code) => ({
-        code: code,
-        availableUses: useCount,
-        disabled: 0 as const,
-        forUser: forAccount,
-        createdBy: 'admin',
-        createdAt: new Date().toISOString(),
-      }))
-
-      await ctx.db.db.insertInto('invite_code').values(vals).execute()
+      const vals: InviteCode[] = []
+      const accountCodes: AccountCodes[] = []
+      for (const account of forAccounts) {
+        const codes = genInvCodes(ctx.cfg, codeCount)
+        for (const code of codes) {
+          vals.push({
+            code: code,
+            availableUses: useCount,
+            disabled: 0 as const,
+            forUser: account,
+            createdBy: 'admin',
+            createdAt: new Date().toISOString(),
+          })
+        }
+        accountCodes.push({ account, codes })
+      }
+      await Promise.all(
+        chunkArray(vals, 500).map((chunk) =>
+          ctx.db.db.insertInto('invite_code').values(chunk).execute(),
+        ),
+      )
 
       req.log.info(
-        { useCount, codes, forAccount, codeCount },
+        { useCount, codes: accountCodes, forAccounts, codeCount },
         'created invite codes',
       )
 
       return {
         encoding: 'application/json',
-        body: { codes },
+        body: { codes: accountCodes },
       }
     },
   })
