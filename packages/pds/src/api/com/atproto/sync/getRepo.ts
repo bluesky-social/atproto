a360cc6004075f1199e8ebebdb429a8e966b6576
Daniel Holmgren <dtholmgren@gmail.com>
2023-04-24 10:09:19 -0500

    Chunk getRepo query (#877)
    chunk get repo query

--- packages/pds/src/api/com/atproto/sync/getRepo.ts
+++ packages/pds/src/api/com/atproto/sync/getRepo.ts
@@ -4,7 +4,7 @@ import { InvalidRequestError } from '@atproto/xrpc-server'
 import { Server } from '../../../../lexicon'
 import SqlRepoStorage from '../../../../sql-repo-storage'
 import AppContext from '../../../../context'
-import { byteIterableToStream } from '@atproto/common'
+import { byteIterableToStream, chunkArray } from '@atproto/common'
 
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.sync.getRepo(async ({ params }) => {
@@ -17,10 +17,24 @@ export default function (server: Server, ctx: AppContext) {
     if (latest === null) {
       throw new InvalidRequestError(`Could not find repo for DID: ${did}`)
     }
-    const commits = repo.getCommits(storage, latest, earliest)
+    const commitPath = await storage.getCommitPath(latest, earliest)
+    if (commitPath === null) {
+      throw new InvalidRequestError(`Could not find shared history`)
+    }
+
+    const commitChunks = chunkArray(commitPath, 25)
+    const carStream = repo.writeCar(latest, async (car) => {
+      for (const chunk of commitChunks) {
+        const blocks = await storage.getAllBlocksForCommits(chunk)
+        for (const block of blocks) {
+          await car.put({ cid: block.cid, bytes: block.bytes })
+        }
+      }
+    })
+
     return {
       encoding: 'application/vnd.ipld.car',
-      body: byteIterableToStream(commits),
+      body: byteIterableToStream(carStream),
     }
   })
 }
