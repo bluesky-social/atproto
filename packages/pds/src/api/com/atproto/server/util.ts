db3fe07b13f3c5ecf688db3fd5c9567a7f6808c9
Devin Ivy <devinivy@gmail.com>
2023-04-27 11:24:43 -0400

    Tidy

--- packages/pds/src/api/com/atproto/server/util.ts
+++ packages/pds/src/api/com/atproto/server/util.ts
@@ -1,10 +1,10 @@
 import * as crypto from '@atproto/crypto'
 import { ServerConfig } from '../../../../config'
 
-// generate a 7 char b32 invite code - preceded by the hostname
+// generate a 9-char b32 invite code - preceded by the hostname
 // with '.'s replaced by '-'s so it is not mistakable for a link
-// ex: bsky-app-abc2345
-// regex: bsky-app-[a-z2-7]{7}
+// ex: bsky-app-abc234567
+// regex: bsky-app-[a-z2-7]{9}
 export const genInvCode = (cfg: ServerConfig): string => {
   const code = crypto.randomStr(8, 'base32').slice(0, 9)
   return cfg.publicHostname.replaceAll('.', '-') + '-' + code
