2f2129639f6ebc9370fd0c83ff612ae41763ce5b
dholms <dtholmgren@gmail.com>
2023-04-28 17:44:07 -0500

    update lex

--- packages/pds/src/lexicon/lexicons.ts
+++ packages/pds/src/lexicon/lexicons.ts
@@ -3398,7 +3398,8 @@ export const schemaDict = {
             type: 'boolean',
           },
           blocking: {
-            type: 'boolean',
+            type: 'string',
+            format: 'at-uri',
           },
           following: {
             type: 'string',
