015fe1226c4c1612567be3830f8a546db7b78bcf
Daniel Holmgren <dtholmgren@gmail.com>
2023-05-03 18:34:55 -0500

    Delete dereferenced blobs (#959)
    * delete dereferenced blobs
    
    * invalidate cache for delete blobs
    
    * start testing
    
    * test & cleanup
    
    * rm blob cleanup logic on rebases
    
    * make disk blob deletes idempotent
    
    * move blob deletion to background queue

--- packages/pds/tests/rebase.test.ts
+++ packages/pds/tests/rebase.test.ts
@@ -9,7 +9,6 @@ describe('repo rebases', () => {
   let agent: AtpAgent
   let sc: SeedClient
   let alice: string
-  let bob: string
 
   let ctx: AppContext
 
@@ -28,13 +27,7 @@ describe('repo rebases', () => {
       handle: 'alice.test',
       password: 'alice-pass',
     })
-    await sc.createAccount('bob', {
-      email: 'bob@test.com',
-      handle: 'bob.test',
-      password: 'bob-pass',
-    })
     alice = sc.dids.alice
-    bob = sc.dids.bob
   })
 
   afterAll(async () => {
@@ -72,61 +65,14 @@ describe('repo rebases', () => {
     const contentsAfter = await after.repo.getContents()
     expect(contentsAfter).toEqual(contentsBefore)
     expect(after.repo.commit.prev).toBe(null)
-  })
-
-  it('handles blobs that have been rebased away', async () => {
-    const file1 = await sc.uploadFile(
-      alice,
-      'tests/image/fixtures/key-landscape-small.jpg',
-      'image/jpeg',
-    )
-    const file2 = await sc.uploadFile(
-      alice,
-      'tests/image/fixtures/key-portrait-small.jpg',
-      'image/jpeg',
-    )
-    const file3Alice = await sc.uploadFile(
-      alice,
-      'tests/image/fixtures/key-alt.jpg',
-      'image/jpeg',
-    )
-    const file3Bob = await sc.uploadFile(
-      bob,
-      'tests/image/fixtures/key-alt.jpg',
-      'image/jpeg',
-    )
-
-    const post1 = await sc.post(alice, 'first post', undefined, [file1])
-    await sc.post(alice, 'second post', undefined, [file2])
-    const post3 = await sc.post(alice, 'third post', undefined, [file3Alice])
-    await sc.post(bob, 'bob post', undefined, [file3Bob])
-    await sc.deletePost(alice, post1.ref.uri)
-    await sc.deletePost(alice, post3.ref.uri)
-
-    await ctx.db.transaction((dbTxn) =>
-      ctx.services.repo(dbTxn).rebaseRepo(alice, new Date().toISOString()),
-    )
 
     const repoBlobs = await ctx.db.db
       .selectFrom('repo_blob')
       .where('did', '=', alice)
       .selectAll()
       .execute()
-    const repoBlobCids = repoBlobs.map((row) => row.cid)
-    expect(repoBlobCids).toEqual([file2.image.ref.toString()])
-    const blobs = await ctx.db.db
-      .selectFrom('blob')
-      .where('creator', '=', alice)
-      .selectAll()
-      .execute()
-    const blobCids = blobs.map((row) => row.cid)
-
-    expect(blobCids).toEqual([file2.image.ref.toString()])
-    const has1 = await ctx.blobstore.hasStored(file1.image.ref)
-    expect(has1).toBe(false)
-    const has2 = await ctx.blobstore.hasStored(file2.image.ref)
-    expect(has2).toBe(true)
-    const has3 = await ctx.blobstore.hasStored(file3Bob.image.ref)
-    expect(has3).toBe(true)
+    expect(
+      repoBlobs.every((row) => row.commit === commitPath[0].tostring()),
+    ).toBeTruthy()
   })
 })
