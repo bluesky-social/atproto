6e06e4fecbeb9bd2f35d660e6c301bef36312687
Sarah Eaglesfield <speedbunny@gmail.com>
2023-05-10 18:16:13 +0100

    Update sanitization.ts
    Improved sanitization function

--- packages/api/src/rich-text/sanitization.ts
+++ packages/api/src/rich-text/sanitization.ts
@@ -1,39 +1,38 @@
 import { RichText } from './rich-text'
 import { UnicodeString } from './unicode'
 
-const EXCESS_SPACE_RE = /[\r\n]([\u00AD\u2060\u200D\u200C\u200B\s]*[\r\n]){2,}/
-const REPLACEMENT_STR = '\n\n'
+const EXCESS_SPACE_RE = /[\r\n]([\u00AD\u2060\u200D\u200C\u200B\s]*[\r\n]){2,}/gs
 
 export function sanitizeRichText(
   richText: RichText,
-  opts: { cleanNewlines?: boolean },
-) {
-  if (opts.cleanNewlines) {
-    richText = clean(richText, EXCESS_SPACE_RE, REPLACEMENT_STR)
+  opts: { cleanNewlines?: boolean } = {},
+): RichText {
+  if (opts.cleanNewlines ?? false) {
+    richText = sanitize(richText, EXCESS_SPACE_RE, '\n\n')
   }
   return richText
 }
 
-function clean(
+function sanitize(
   richText: RichText,
   targetRegexp: RegExp,
   replacementString: string,
 ): RichText {
   richText = richText.clone()
 
-  let match = richText.unicodeText.utf16.match(targetRegexp)
-  while (match && typeof match.index !== 'undefined') {
+  let match: RegExpExecArray | null
+  while ((match = targetRegexp.exec(richText.unicodeText.utf16))) {
     const oldText = richText.unicodeText
     const removeStartIndex = richText.unicodeText.utf16IndexToUtf8Index(
       match.index,
     )
-    const removeEndIndex = removeStartIndex + new UnicodeString(match[0]).length
+    const removeEndIndex =
+      removeStartIndex + new UnicodeString(match[0]).length
     richText.delete(removeStartIndex, removeEndIndex)
     if (richText.unicodeText.utf16 === oldText.utf16) {
       break // sanity check
     }
     richText.insert(removeStartIndex, replacementString)
-    match = richText.unicodeText.utf16.match(targetRegexp)
   }
 
   return richText
