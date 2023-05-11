842b281afbb7c30637a428fcd1190d2ba4edb8f4
Daniel Holmgren <dtholmgren@gmail.com>
2023-05-10 20:21:50 -0500

    Update packages/pds/src/services/record/index.ts

--- packages/pds/src/services/record/index.ts
+++ packages/pds/src/services/record/index.ts
@@ -278,7 +278,7 @@ export class RecordService {
 }
 
 // @NOTE in the future this can be replaced with a more generic routine that pulls backlinks based on lex docs.
-// For now we just want to ensure we're tracking links from follows, likes, and reposts.
+// For now we just want to ensure we're tracking links from follows, blocks, likes, and reposts.
 
 function getBacklinks(uri: AtUri, record: unknown): Backlink[] {
   if (
