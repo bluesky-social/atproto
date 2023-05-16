4f305d0ae61f61243d1cbef38a2c762d411f61a7
dholms <dtholmgren@gmail.com>
2023-05-15 19:28:02 -0500

    add team dids

--- packages/pds/src/app-view/api/app/bsky/feed/algos/bsky-team.ts
+++ packages/pds/src/app-view/api/app/bsky/feed/algos/bsky-team.ts
@@ -5,7 +5,20 @@ import { FeedKeyset } from '../../util/feed'
 import { paginate } from '../../../../../../db/pagination'
 import { AlgoHandler, AlgoResponse } from './types'
 
-const BSKY_TEAM: NotEmptyArray<string> = ['did:example:test']
+const BSKY_TEAM: NotEmptyArray<string> = [
+  'did:plc:oky5czdrnfjpqslsw2a5iclo', // jay
+  'did:plc:yk4dd2qkboz2yv6tpubpc6co', // daniel
+  'did:plc:ragtjsm2j2vknwkz3zp4oxrd', // paul
+  'did:plc:l3rouwludahu3ui3bt66mfvj', // devin
+  'did:plc:tpg43qhh4lw4ksiffs4nbda3', // jake
+  'did:plc:44ybard66vv44zksje25o7dz', // bryan
+  'did:plc:qjeavhlw222ppsre4rscd3n2', // rose
+  'did:plc:vjug55kidv6sye7ykr5faxxn', // emily
+  'did:plc:fgsn4gf2dlgnybo4nbej5b2s', // ansh
+  'did:plc:vpkhqolt662uhesyj6nxm7ys', // why
+  'did:plc:z72i7hdynmk6r22z27h6tvur', // @bsky.app
+  'did:plc:ewvi7nxzyoun6zhxrhs64oiz', // @atproto.com
+]
 
 const handler: AlgoHandler = async (
   ctx: AppContext,
