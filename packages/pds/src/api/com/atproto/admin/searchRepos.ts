c22a956759d3db3d58b059e4ea0f3376538d9091
Foysal Ahamed <cfaion341@gmail.com>
2023-05-23 23:40:19 +0200

    :sparkles: Allow searching repo by DID

--- packages/pds/src/api/com/atproto/admin/searchRepos.ts
+++ packages/pds/src/api/com/atproto/admin/searchRepos.ts
@@ -3,6 +3,7 @@ import AppContext from '../../../../context'
 import { SearchKeyset } from '../../../../services/util/search'
 import { sql } from 'kysely'
 import { ListKeyset } from '../../../../services/account'
+import { ensureValidDid } from '@atproto/identifier'
 
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.admin.searchRepos({
@@ -27,15 +28,25 @@ export default function (server: Server, ctx: AppContext) {
         }
       }
 
+      let searchField: 'did' | 'handle'
+      try {
+        ensureValidDid(term)
+        searchField = 'did'
+      } catch (err) {
+        searchField = 'handle'
+      }
+
       const results = await services
         .account(db)
-        .search({ term, limit, cursor, includeSoftDeleted: true })
+        .search({ searchField, term, limit, cursor, includeSoftDeleted: true })
       const keyset = new SearchKeyset(sql``, sql``)
 
       return {
         encoding: 'application/json',
         body: {
-          cursor: keyset.packFromResult(results),
+          // For did search, we can only find 1 or no match, cursors can be ignored entirely
+          cursor:
+            searchField === 'did' ? undefined : keyset.packFromResult(results),
           repos: await moderationService.views.repo(results),
         },
       }
