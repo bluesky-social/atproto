194505e4ec93df91cd304a962a07485c4d45b37f
dholms <dtholmgren@gmail.com>
2023-05-24 13:35:35 -0500

    add feed gen did to cfg

--- packages/pds/src/app-view/api/app/bsky/feed/describeFeedGenerator.ts
+++ packages/pds/src/app-view/api/app/bsky/feed/describeFeedGenerator.ts
@@ -1,14 +1,19 @@
 import { Server } from '../../../../../lexicon'
 import AppContext from '../../../../../context'
+import { MethodNotImplementedError } from '@atproto/xrpc-server'
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.describeFeedGenerator(async () => {
+    if (!ctx.cfg.feedGenDid) {
+      throw new MethodNotImplementedError()
+    }
+
     const feeds = Object.keys(ctx.algos).map((uri) => ({ uri }))
 
     return {
       encoding: 'application/json',
       body: {
-        did: ctx.cfg.serverDid,
+        did: ctx.cfg.feedGenDid,
         feeds,
       },
     }
