89ee61e2ebdc52adf4f7b00b4a0a759fe52169e5
bryan newbold <bnewbold@robocracy.org>
2023-05-23 19:03:09 -0700

    identifier: reduce max length of DID from 8 KB to 2 KB

--- packages/identifier/src/did.ts
+++ packages/identifier/src/did.ts
@@ -38,7 +38,7 @@ export const ensureValidDid = (did: string): void => {
     throw new InvalidDidError('DID can not end with ":" or "%"')
   }
 
-  if (did.length > 8 * 1024) {
+  if (did.length > 2 * 1024) {
     throw new InvalidDidError('DID is far too long')
   }
 }
@@ -50,7 +50,7 @@ export const ensureValidDidRegex = (did: string): void => {
     throw new InvalidDidError("DID didn't validate via regex")
   }
 
-  if (did.length > 8 * 1024) {
+  if (did.length > 2 * 1024) {
     throw new InvalidDidError('DID is far too long')
   }
 }
