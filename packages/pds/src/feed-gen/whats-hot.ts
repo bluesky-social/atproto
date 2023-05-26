6775f2ab29c80b8f479de940020813d457aef2f2
Devin Ivy <devinivy@gmail.com>
2023-05-26 11:39:50 -0400

    Implement quicker mute checks on pds

--- packages/pds/src/feed-gen/whats-hot.ts
+++ packages/pds/src/feed-gen/whats-hot.ts
@@ -77,7 +77,9 @@ const handler: AlgoHandler = async (
             .orWhereRef('label.uri', '=', ref('post_embed_record.embedUri')),
         ),
     )
-    .whereNotExists(accountService.mutedQb(requester, [ref('post.creator')]))
+    .where((qb) =>
+      accountService.whereNotMuted(qb, requester, [ref('post.creator')]),
+    )
     .whereNotExists(graphService.blockQb(requester, [ref('post.creator')]))
     .orderBy('candidate.score', 'desc')
     .limit(limit)
