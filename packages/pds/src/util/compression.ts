8f24b0dc433fa60e8ca4ce1fb2a1bd0a4de7aa79
dholms <dtholmgren@gmail.com>
2023-06-02 10:21:34 -0500

    remove compressible dep

--- packages/pds/src/util/compression.ts
+++ packages/pds/src/util/compression.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import compression from 'compression'
-import compressible from 'compressible'
 
 export default function () {
   return compression({
@@ -13,8 +12,5 @@ function filter(_req: express.Request, res: express.Response) {
   if (contentType === 'application/vnd.ipld.car') {
     return true
   }
-  if (contentType === undefined || !compressible(contentType)) {
-    return false
-  }
-  return true
+  return compression.filter(_req, res)
 }
