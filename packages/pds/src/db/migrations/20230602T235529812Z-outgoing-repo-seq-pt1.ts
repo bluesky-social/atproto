75e8d23b0502635f4dace28629bf43b40267277c
dholms <dtholmgren@gmail.com>
2023-06-02 19:02:24 -0500

    add prep migration for stream fixing

---
+++ packages/pds/src/db/migrations/20230602T235529812Z-outgoing-repo-seq-pt1.ts
@@ -0,0 +1,37 @@
+import { Kysely } from 'kysely'
+import { Dialect } from '..'
+
+export async function up(db: Kysely<any>, dialect: Dialect): Promise<void> {
+  if (dialect === 'sqlite') {
+    return
+  }
+  await db.schema.alterTable('repo_seq').renameColumn('seq', 'id').execute()
+  await db.schema.alterTable('repo_seq').renameTo('repo_event').execute()
+  await db.schema
+    .createView('repo_seq')
+    .as(
+      db
+        .selectFrom('repo_event')
+        .select([
+          'seq as id',
+          'did',
+          'eventType',
+          'event',
+          'invalidated',
+          'sequencedAt',
+        ]),
+    )
+    .execute()
+}
+
+export async function down(
+  db: Kysely<unknown>,
+  dialect: Dialect,
+): Promise<void> {
+  if (dialect === 'sqlite') {
+    return
+  }
+  await db.schema.dropView('repo_seq').execute()
+  await db.schema.alterTable('repo_event').renameColumn('id', 'seq').execute()
+  await db.schema.alterTable('repo_event').renameTo('repo_seq').execute()
+}
