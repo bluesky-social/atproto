d318f3e483863d4b73ced32a0fad5c6e88ec5cf2
Devin Ivy <devinivy@gmail.com>
2023-06-12 19:21:19 -0400

    remove unused image helper from pds

--- packages/pds/src/image/sharp.ts
+++ packages/pds/src/image/sharp.ts
@@ -1,53 +1,10 @@
 import { Readable } from 'stream'
 import { pipeline } from 'stream/promises'
 import sharp from 'sharp'
-import { errHasMsg, forwardStreamErrors } from '@atproto/common'
+import { errHasMsg } from '@atproto/common'
 import { formatsToMimes, ImageInfo, Options } from './util'
 
 export type { Options }
-
-export async function resize(
-  stream: Readable,
-  options: Options,
-): Promise<Readable> {
-  const { height, width, min = false, fit = 'cover', format, quality } = options
-
-  let processor = sharp()
-
-  // Scale up to hit any specified minimum size
-  if (typeof min !== 'boolean') {
-    const upsizeProcessor = sharp().resize({
-      fit: 'outside',
-      width: min.width,
-      height: min.height,
-      withoutReduction: true,
-      withoutEnlargement: false,
-    })
-    forwardStreamErrors(stream, upsizeProcessor)
-    stream = stream.pipe(upsizeProcessor)
-  }
-
-  // Scale down (or possibly up if min is true) to desired size
-  processor = processor.resize({
-    fit,
-    width,
-    height,
-    withoutEnlargement: min !== true,
-  })
-
-  // Output to specified format
-  if (format === 'jpeg') {
-    processor = processor.jpeg({ quality: quality ?? 100 })
-  } else if (format === 'png') {
-    processor = processor.png({ quality: quality ?? 100 })
-  } else {
-    const exhaustiveCheck: never = format
-    throw new Error(`Unhandled case: ${exhaustiveCheck}`)
-  }
-
-  forwardStreamErrors(stream, processor)
-  return stream.pipe(processor)
-}
 
 export async function maybeGetInfo(
   stream: Readable,
