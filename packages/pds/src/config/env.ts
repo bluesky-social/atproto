68a2dafc54b9f4376aceb2a59cb46833bdcbd87d
dholms <dtholmgren@gmail.com>
2023-06-13 12:41:29 -0500

    comments

--- packages/pds/src/config/env.ts
+++ packages/pds/src/config/env.ts
@@ -2,13 +2,18 @@ import { envInt, envStr, envBool, envList } from './util'
 
 export const readEnv = (): ServerEnvironment => {
   return {
+    // service
     port: envInt(process.env.PORT),
     hostname: envStr(process.env.HOSTNAME),
     serviceDid: envStr(process.env.SERVICE_DID),
     version: envStr(process.env.PDS_VERSION),
     privacyPolicyUrl: envStr(process.env.PRIVACY_POLICY_URL),
     termsOfServiceUrl: envStr(process.env.TERMS_OF_SERVICE_URL),
+
+    // db: one required
+    // sqlite
     dbSqliteLocation: envStr(process.env.DB_SQLITE_LOCATION),
+    // postgres
     dbPostgresUrl: envStr(process.env.DB_POSTGRES_URL),
     dbPostgresMigrationUrl: envStr(process.env.DB_POSTGRES_MIGRATION_URL),
     dbPostgresSchema: envStr(process.env.DB_POSTGRES_SCHEMA),
@@ -17,33 +22,57 @@ export const readEnv = (): ServerEnvironment => {
     dbPostgresPoolIdleTimeoutMs: envInt(
       process.env.DB_POSTGRES_POOL_IDLE_TIMEOUT_MS,
     ),
+
+    // blobstore: one required
+    // s3
     blobstoreS3Bucket: envStr(process.env.BLOBSTORE_S3_BUCKET),
+    // disk
     blobstoreDiskLocation: envStr(process.env.BLOBSTORE_DISK_LOCATION),
     blobstoreDiskTmpLocation: envStr(process.env.BLOBSTORE_DISK_TMP_LOCATION),
+
+    // identity
     didPlcUrl: envStr(process.env.DID_PLC_URL),
     didCacheStaleTTL: envInt(process.env.DID_CACHE_STALE_TTL),
     didCacheMaxTTL: envInt(process.env.DID_CACHE_MAX_TTL),
     resolverTimeout: envInt(process.env.ID_RESOLVER_TIMEOUT),
     recoveryDidKey: envStr(process.env.RECOVERY_DID_KEY),
     handleDomains: envList(process.env.HANDLE_DOMAINS),
+
+    // invites
     inviteRequired: envBool(process.env.INVITE_REQUIRED),
     inviteInterval: envInt(process.env.INVITE_INTERVAL),
+
+    // email
     emailSmtpUrl: envStr(process.env.EMAIL_SMTP_URL),
     emailFromAddress: envStr(process.env.EMAIL_FROM_ADDRESS),
+
+    // subscription
     maxSubscriptionBuffer: envInt(process.env.MAX_SUBSCRIPTION_BUFFER),
     repoBackfillLimitMs: envInt(process.env.REPO_BACKFILL_LIMIT_MS),
     sequencerLeaderLockId: envInt(process.env.SEQUENCER_LEADER_LOCK_ID),
+
+    // appview
     bskyAppViewEndpoint: envStr(process.env.BSKY_APP_VIEW_ENDPOINT),
     bskyAppViewDid: envStr(process.env.BSKY_APP_VIEW_DID),
+
+    // crawlers
     crawlers: envList(process.env.CRAWLERS),
+
+    // secrets
     jwtSecret: envStr(process.env.JWT_SECRET),
     adminPassword: envStr(process.env.ADMIN_PASSWORD),
     moderatorPassword: envStr(process.env.MODERATOR_PASSWORD),
+
+    // keys: only one of each required
+    // kms
     repoSigningKeyKmsKeyId: envStr(process.env.REPO_SIGNING_KEY_KMS_KEY_ID),
+    // memory
     repoSigningKeyK256PrivateKeyHex: envStr(
       process.env.REPO_SIGNING_KEY_K256_PRIVATE_KEY_HEX,
     ),
+    // kms
     plcRotationKeyKmsKeyId: envStr(process.env.PLC_ROTATION_KEY_KMS_KEY_ID),
+    // memory
     plcRotationKeyK256PrivateKeyHex: envStr(
       process.env.PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX,
     ),
@@ -51,7 +80,7 @@ export const readEnv = (): ServerEnvironment => {
 }
 
 export type ServerEnvironment = {
-  // infra
+  // service
   port?: number
   hostname?: string
   serviceDid?: string
@@ -81,7 +110,7 @@ export type ServerEnvironment = {
   recoveryDidKey?: string
   handleDomains?: string[] // public hostname by default
 
-  // accounts
+  // invites
   inviteRequired?: boolean
   inviteInterval?: number
 
