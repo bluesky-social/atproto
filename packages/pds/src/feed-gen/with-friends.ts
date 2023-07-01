a96ad5eeffa1b51226937ded34ffd81ce9a23e59
dholms <dtholmgren@gmail.com>
2023-07-01 12:45:54 -0500

    disable with friends

--- packages/pds/src/feed-gen/with-friends.ts
+++ packages/pds/src/feed-gen/with-friends.ts
@@ -12,39 +12,42 @@ const handler: AlgoHandler = async (
   params: SkeletonParams,
   requester: string,
 ): Promise<AlgoResponse> => {
-  const { cursor, limit = 50 } = params
-  const accountService = ctx.services.account(ctx.db)
-  const feedService = ctx.services.appView.feed(ctx.db)
-  const graphService = ctx.services.appView.graph(ctx.db)
-
-  const { ref } = ctx.db.db.dynamic
-
-  const keyset = new FeedKeyset(ref('post.indexedAt'), ref('post.cid'))
-  const sortFrom = keyset.unpack(cursor)?.primary
-
-  let postsQb = feedService
-    .selectPostQb()
-    // .innerJoin('post_agg', 'post_agg.uri', 'post.uri')
-    // .where('post_agg.likeCount', '>=', 6)
-    .whereExists((qb) =>
-      qb
-        .selectFrom('follow')
-        .where('follow.creator', '=', requester)
-        .whereRef('follow.subjectDid', '=', 'post.creator'),
-    )
-    .where((qb) =>
-      accountService.whereNotMuted(qb, requester, [ref('post.creator')]),
-    )
-    .whereNotExists(graphService.blockQb(requester, [ref('post.creator')]))
-    .where('post.indexedAt', '>', getFeedDateThreshold(sortFrom))
-
-  postsQb = paginate(postsQb, { limit, cursor, keyset, tryIndex: true })
-
-  const feedItems = await postsQb.execute()
   return {
-    feedItems,
-    cursor: keyset.packFromResult(feedItems),
+    feedItems: [],
   }
+  // const { cursor, limit = 50 } = params
+  // const accountService = ctx.services.account(ctx.db)
+  // const feedService = ctx.services.appView.feed(ctx.db)
+  // const graphService = ctx.services.appView.graph(ctx.db)
+
+  // const { ref } = ctx.db.db.dynamic
+
+  // const keyset = new FeedKeyset(ref('post.indexedAt'), ref('post.cid'))
+  // const sortFrom = keyset.unpack(cursor)?.primary
+
+  // let postsQb = feedService
+  //   .selectPostQb()
+  //   // .innerJoin('post_agg', 'post_agg.uri', 'post.uri')
+  //   // .where('post_agg.likeCount', '>=', 6)
+  //   .whereExists((qb) =>
+  //     qb
+  //       .selectFrom('follow')
+  //       .where('follow.creator', '=', requester)
+  //       .whereRef('follow.subjectDid', '=', 'post.creator'),
+  //   )
+  //   .where((qb) =>
+  //     accountService.whereNotMuted(qb, requester, [ref('post.creator')]),
+  //   )
+  //   .whereNotExists(graphService.blockQb(requester, [ref('post.creator')]))
+  //   .where('post.indexedAt', '>', getFeedDateThreshold(sortFrom))
+
+  // postsQb = paginate(postsQb, { limit, cursor, keyset, tryIndex: true })
+
+  // const feedItems = await postsQb.execute()
+  // return {
+  //   feedItems,
+  //   cursor: keyset.packFromResult(feedItems),
+  // }
 }
 
 export default handler
