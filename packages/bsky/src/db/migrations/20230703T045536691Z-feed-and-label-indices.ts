b39aa3d1a5ff6047421a826596423078e35f7268
Devin Ivy <devinivy@gmail.com>
2023-07-03 10:21:39 -0400

    sync-up bsky index migration, remove duplicate index

--- packages/bsky/src/db/migrations/20230703T045536691Z-feed-and-label-indices.ts
+++ packages/bsky/src/db/migrations/20230703T045536691Z-feed-and-label-indices.ts
@@ -1,16 +1,17 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  await db.schema
-    .createIndex('label_cts_idx')
-    .on('label')
-    .column('cts')
-    .execute()
-  await db.schema
-    .createIndex('feed_item_originator_cursor_idx')
-    .on('feed_item')
-    .columns(['originatorDid', 'sortAt', 'cid'])
-    .execute()
+  // await db.schema
+  //   .createIndex('label_cts_idx')
+  //   .on('label')
+  //   .column('cts')
+  //   .execute()
+  // await db.schema.dropIndex('feed_item_originator_idx').execute()
+  // await db.schema
+  //   .createIndex('feed_item_originator_cursor_idx')
+  //   .on('feed_item')
+  //   .columns(['originatorDid', 'sortAt', 'cid'])
+  //   .execute()
   // await db.schema
   //   .createIndex('record_did_idx')
   //   .on('record')
@@ -21,5 +22,10 @@ export async function up(db: Kysely<unknown>): Promise<void> {
 export async function down(db: Kysely<unknown>): Promise<void> {
   await db.schema.dropIndex('label_cts_idx').execute()
   await db.schema.dropIndex('feed_item_originator_cursor_idx').execute()
+  await db.schema
+    .createIndex('feed_item_originator_idx')
+    .on('feed_item')
+    .column('originatorDid')
+    .execute()
   await db.schema.dropIndex('record_did_idx').execute()
 }
