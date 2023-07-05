a4919eedb82d852095738db98a0956c992d587d9
dholms <dtholmgren@gmail.com>
2023-07-05 17:03:01 -0500

    update indices

--- packages/bsky/src/db/migrations/20230703T045536691Z-feed-and-label-indices.ts
+++ packages/bsky/src/db/migrations/20230703T045536691Z-feed-and-label-indices.ts
@@ -1,22 +1,22 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  // await db.schema
-  //   .createIndex('label_cts_idx')
-  //   .on('label')
-  //   .column('cts')
-  //   .execute()
-  // await db.schema.dropIndex('feed_item_originator_idx').execute()
-  // await db.schema
-  //   .createIndex('feed_item_originator_cursor_idx')
-  //   .on('feed_item')
-  //   .columns(['originatorDid', 'sortAt', 'cid'])
-  //   .execute()
-  // await db.schema
-  //   .createIndex('record_did_idx')
-  //   .on('record')
-  //   .column('did')
-  //   .execute()
+  await db.schema
+    .createIndex('label_cts_idx')
+    .on('label')
+    .column('cts')
+    .execute()
+  await db.schema.dropIndex('feed_item_originator_idx').execute()
+  await db.schema
+    .createIndex('feed_item_originator_cursor_idx')
+    .on('feed_item')
+    .columns(['originatorDid', 'sortAt', 'cid'])
+    .execute()
+  await db.schema
+    .createIndex('record_did_idx')
+    .on('record')
+    .column('did')
+    .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
--- packages/pds/src/db/migrations/20230703T044601833Z-feed-and-label-indices.ts
+++ packages/pds/src/db/migrations/20230703T044601833Z-feed-and-label-indices.ts
@@ -1,16 +1,16 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  // await db.schema
-  //   .createIndex('label_cts_idx')
-  //   .on('label')
-  //   .column('cts')
-  //   .execute()
-  // await db.schema
-  //   .createIndex('feed_item_originator_cursor_idx')
-  //   .on('feed_item')
-  //   .columns(['originatorDid', 'sortAt', 'cid'])
-  //   .execute()
+  await db.schema
+    .createIndex('label_cts_idx')
+    .on('label')
+    .column('cts')
+    .execute()
+  await db.schema
+    .createIndex('feed_item_originator_cursor_idx')
+    .on('feed_item')
+    .columns(['originatorDid', 'sortAt', 'cid'])
+    .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
