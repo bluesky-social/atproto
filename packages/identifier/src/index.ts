50639f3d48b1dbfdda9243ee65d7be6926e3b37f
dholms <dtholmgren@gmail.com>
2023-07-13 13:05:34 -0700

    missed merge

--- packages/identifier/src/index.ts
+++ packages/identifier/src/index.ts
@@ -1,8 +1,2 @@
 export * from './handle'
 export * from './did'
-<<<<<<< HEAD
-=======
-export * from './reserved'
-export * from './validator'
-export * from './unacceptable'
->>>>>>> handle_validator_sketch
