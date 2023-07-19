e5bf345f5dadf0f0b8a363f79fa0399c3dd19588
NekoShinshi <87232551+neko-shinshi@users.noreply.github.com>
2023-07-19 14:48:32 +0800

    Fix missing re-attempt at calling getSession
    Sorry, missed this out

--- packages/api/src/agent.ts
+++ packages/api/src/agent.ts
@@ -149,9 +149,10 @@ export class AtpAgent {
   ): Promise<ComAtprotoServerGetSession.Response> {
     try {
       this.session = session
-      const res = await this.api.com.atproto.server.getSession()
+      let res = await this.api.com.atproto.server.getSession()
       if (!res.success && this.session.refreshJwt) {
         await this._refreshSession()
+        res = await this.api.com.atproto.server.getSession()
       }
       if (!res.success || res.data.did !== this.session.did) {
         throw new Error('Invalid session')
