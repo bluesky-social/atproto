0b33d30140a6bfc2cad027689beec8c5c31d54d9
Eric Bailey <git@esb.lol>
2023-07-19 17:47:32 -0500

    fix lint

--- packages/bsky/src/api/app/bsky/unspecced/getPopularFeedGenerators.ts
+++ packages/bsky/src/api/app/bsky/unspecced/getPopularFeedGenerators.ts
@@ -29,9 +29,10 @@ export default function (server: Server, ctx: AppContext) {
         ])
 
       if (query) {
-        inner = inner.where(qb => qb
-          .where('feed_generator.displayName', 'ilike', `%${query}%`)
-          .orWhere('feed_generator.description', 'ilike', `%${query}%`)
+        inner = inner.where((qb) =>
+          qb
+            .where('feed_generator.displayName', 'ilike', `%${query}%`)
+            .orWhere('feed_generator.description', 'ilike', `%${query}%`),
         )
       }
 
