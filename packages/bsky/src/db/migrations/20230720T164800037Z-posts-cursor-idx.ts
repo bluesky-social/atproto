71565c6d2ddae112d587a3b928544c23918b0402
Daniel Holmgren <dtholmgren@gmail.com>
2023-07-20 16:41:47 -0500

    Tweak timeline migraiton (#1369)
    * v0.4.3
    
    * tweak timeline migraiton
    
    * misspelling

--- packages/bsky/src/db/migrations/20230720T164800037Z-posts-cursor-idx.ts
+++ packages/bsky/src/db/migrations/20230720T164800037Z-posts-cursor-idx.ts
@@ -1,21 +1,19 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  // @TODO uncomment
-  // await db.schema
-  //   .createIndex('post_creator_cursor_idx')
-  //   .on('post')
-  //   .columns(['creator', 'sortAt', 'cid'])
-  //   .execute()
-  // await db.schema.dropIndex('post_creator_idx').execute()
+  await db.schema
+    .createIndex('post_creator_cursor_idx')
+    .on('post')
+    .columns(['creator', 'sortAt', 'cid'])
+    .execute()
+  await db.schema.dropIndex('post_creator_idx').execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  // @TODO uncomment
-  // await db.schema
-  //   .createIndex('post_creator_idx')
-  //   .on('post')
-  //   .column('creator')
-  //   .execute()
-  // await db.schema.dropIndex('post_creator_cursor_idx').execute()
+  await db.schema
+    .createIndex('post_creator_idx')
+    .on('post')
+    .column('creator')
+    .execute()
+  await db.schema.dropIndex('post_creator_cursor_idx').execute()
 }
