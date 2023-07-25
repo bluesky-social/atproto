8a0f90c00633e4d832d4465d74c0f8474c4267fb
Devin Ivy <devinivy@gmail.com>
2023-07-25 13:29:09 -0400

    fix pds crud test

--- packages/pds/tests/crud.test.ts
+++ packages/pds/tests/crud.test.ts
@@ -5,7 +5,7 @@ import * as createRecord from '@atproto/api/src/client/types/com/atproto/repo/cr
 import * as putRecord from '@atproto/api/src/client/types/com/atproto/repo/putRecord'
 import * as deleteRecord from '@atproto/api/src/client/types/com/atproto/repo/deleteRecord'
 import * as applyWrites from '@atproto/api/src/client/types/com/atproto/repo/applyWrites'
-import { cidForCbor, TID, ui8ToBuffer } from '@atproto/common'
+import { cidForCbor, TID, ui8ToArrayBuffer } from '@atproto/common'
 import { BlobNotFoundError } from '@atproto/repo'
 import { defaultFetchHandler } from '@atproto/xrpc'
 import * as Post from '../src/lexicon/types/app/bsky/feed/post'
@@ -833,7 +833,7 @@ describe('crud operations', () => {
 
     it("writes fail on values that can't reliably transform between cbor to lex", async () => {
       const passthroughBody = (data: unknown) =>
-        ui8ToBuffer(new TextEncoder().encode(JSON.stringify(data)))
+        ui8ToArrayBuffer(new TextEncoder().encode(JSON.stringify(data)))
       const result = await defaultFetchHandler(
         aliceAgent.service.origin + `/xrpc/com.atproto.repo.createRecord`,
         'post',
