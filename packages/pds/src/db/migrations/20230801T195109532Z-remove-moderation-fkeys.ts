394767eba8eae7ca6dfd324713b94f6c253c8b1f
Devin Ivy <devinivy@gmail.com>
2023-08-04 15:12:49 -0400

    reenable migration, comment contents temporarily

--- packages/pds/src/db/migrations/20230801T195109532Z-remove-moderation-fkeys.ts
+++ packages/pds/src/db/migrations/20230801T195109532Z-remove-moderation-fkeys.ts
@@ -5,18 +5,18 @@ export async function up(db: Kysely<unknown>, dialect: Dialect): Promise<void> {
   if (dialect === 'sqlite') {
     return
   }
-  await db.schema
-    .alterTable('repo_root')
-    .dropConstraint('repo_root_takedown_id_fkey')
-    .execute()
-  await db.schema
-    .alterTable('record')
-    .dropConstraint('record_takedown_id_fkey')
-    .execute()
-  await db.schema
-    .alterTable('repo_blob')
-    .dropConstraint('repo_blob_takedown_id_fkey')
-    .execute()
+  // await db.schema
+  //   .alterTable('repo_root')
+  //   .dropConstraint('repo_root_takedown_id_fkey')
+  //   .execute()
+  // await db.schema
+  //   .alterTable('record')
+  //   .dropConstraint('record_takedown_id_fkey')
+  //   .execute()
+  // await db.schema
+  //   .alterTable('repo_blob')
+  //   .dropConstraint('repo_blob_takedown_id_fkey')
+  //   .execute()
 }
 
 export async function down(
