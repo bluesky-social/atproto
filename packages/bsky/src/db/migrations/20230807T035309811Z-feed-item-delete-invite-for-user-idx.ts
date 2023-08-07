225d2c00cce1885f2d2cffc4724843eda4b01cee
Devin Ivy <devinivy@gmail.com>
2023-08-07 15:32:37 -0400

    temp comment migrations and build

--- packages/bsky/src/db/migrations/20230807T035309811Z-feed-item-delete-invite-for-user-idx.ts
+++ packages/bsky/src/db/migrations/20230807T035309811Z-feed-item-delete-invite-for-user-idx.ts
@@ -1,12 +1,12 @@
 import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  // supports post deletion
-  await db.schema
-    .createIndex('feed_item_post_uri_idx')
-    .on('feed_item')
-    .column('postUri')
-    .execute()
+  // // supports post deletion
+  // await db.schema
+  //   .createIndex('feed_item_post_uri_idx')
+  //   .on('feed_item')
+  //   .column('postUri')
+  //   .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
