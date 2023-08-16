4241ee16ecef96ddaddfa5489636847a06089c51
Daniel Holmgren <dtholmgren@gmail.com>
2023-08-16 18:34:19 -0500

    Handle revalidation (#1474)
    * easier hanlde revalidation
    
    * remove duplicate line
    
    * backup handle nameservers on appview
    
    * fix tests & add a couple

--- packages/pds/src/api/com/atproto/identity/updateHandle.ts
+++ packages/pds/src/api/com/atproto/identity/updateHandle.ts
@@ -20,28 +20,35 @@ export default function (server: Server, ctx: AppContext) {
       })
 
       // Pessimistic check to handle spam: also enforced by updateHandle() and the db.
-      const available = await ctx.services
-        .account(ctx.db)
-        .isHandleAvailable(handle)
-      if (!available) {
-        throw new InvalidRequestError(`Handle already taken: ${handle}`)
-      }
+      const handleDid = await ctx.services.account(ctx.db).getHandleDid(handle)
 
-      const seqHandleTok = await ctx.db.transaction(async (dbTxn) => {
-        let tok: HandleSequenceToken
-        try {
-          tok = await ctx.services
-            .account(dbTxn)
-            .updateHandle(requester, handle)
-        } catch (err) {
-          if (err instanceof UserAlreadyExistsError) {
-            throw new InvalidRequestError(`Handle already taken: ${handle}`)
-          }
-          throw err
+      let seqHandleTok: HandleSequenceToken
+      if (handleDid) {
+        if (handleDid !== requester) {
+          throw new InvalidRequestError(`Handle already taken: ${handle}`)
         }
-        await ctx.plcClient.updateHandle(requester, ctx.plcRotationKey, handle)
-        return tok
-      })
+        seqHandleTok = { did: requester, handle: handle }
+      } else {
+        seqHandleTok = await ctx.db.transaction(async (dbTxn) => {
+          let tok: HandleSequenceToken
+          try {
+            tok = await ctx.services
+              .account(dbTxn)
+              .updateHandle(requester, handle)
+          } catch (err) {
+            if (err instanceof UserAlreadyExistsError) {
+              throw new InvalidRequestError(`Handle already taken: ${handle}`)
+            }
+            throw err
+          }
+          await ctx.plcClient.updateHandle(
+            requester,
+            ctx.plcRotationKey,
+            handle,
+          )
+          return tok
+        })
+      }
 
       try {
         await ctx.db.transaction(async (dbTxn) => {
