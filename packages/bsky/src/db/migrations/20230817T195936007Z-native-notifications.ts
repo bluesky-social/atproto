372e898a5af13f4a4d7d14c2f3cc8f766d3a09bf
Devin Ivy <devinivy@gmail.com>
2023-08-17 17:25:43 -0400

    hoist push notif migration to present, update model to remove endpoint

--- packages/bsky/src/db/migrations/20230817T195936007Z-native-notifications.ts
+++ packages/bsky/src/db/migrations/20230817T195936007Z-native-notifications.ts
@@ -6,7 +6,6 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     .addColumn('did', 'varchar', (col) => col.notNull())
     .addColumn('platform', 'varchar', (col) => col.notNull())
     .addColumn('token', 'varchar', (col) => col.notNull())
-    .addColumn('endpoint', 'varchar', (col) => col.notNull())
     .addColumn('appId', 'varchar', (col) => col.notNull())
     .addPrimaryKeyConstraint('notification_push_token_pkey', ['did', 'token'])
     .execute()
--- packages/bsky/src/db/tables/notification-push-token.ts
+++ packages/bsky/src/db/tables/notification-push-token.ts
@@ -4,7 +4,6 @@ export interface NotificationPushToken {
   did: string
   platform: 'ios' | 'android' | 'web'
   token: string
-  endpoint: string
   appId: string
 }
 
