a8b3f62fab0253eddf1d56fc5bd739b16cfe6f0b
Daniel Holmgren <dtholmgren@gmail.com>
2023-08-21 16:35:19 -0500

    Bugfix: Application ratelimits relative import (#1500)
    dont use relative xrpc-server path

--- packages/xrpc-server/src/index.ts
+++ packages/xrpc-server/src/index.ts
@@ -5,4 +5,4 @@ export * from './stream'
 export * from './rate-limiter'
 
 export type { ServerTiming } from './util'
-export { serverTimingHeader, ServerTimer } from './util'
+export { getReqIp, serverTimingHeader, ServerTimer } from './util'
