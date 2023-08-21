8c255046ef06c6fd489219b9fe1aaf86e0b8e02a
dholms <dtholmgren@gmail.com>
2023-08-21 17:48:34 -0500

    tweak rate limits

--- packages/pds/src/api/com/atproto/server/createSession.ts
+++ packages/pds/src/api/com/atproto/server/createSession.ts
@@ -1,4 +1,4 @@
-import { AuthRequiredError, getReqIp } from '@atproto/xrpc-server'
+import { AuthRequiredError } from '@atproto/xrpc-server'
 import AppContext from '../../../../context'
 import { softDeleted } from '../../../../db/util'
 import { Server } from '../../../../lexicon'
@@ -11,14 +11,12 @@ export default function (server: Server, ctx: AppContext) {
       {
         durationMs: DAY,
         points: 200,
-        calcKey: ({ req, input }) =>
-          `${getReqIp(req)}-${input.body.identifier}`,
+        calcKey: ({ input }) => input.body.identifier,
       },
       {
         durationMs: 5 * MINUTE,
         points: 10,
-        calcKey: ({ req, input }) =>
-          `${getReqIp(req)}-${input.body.identifier}`,
+        calcKey: ({ input }) => input.body.identifier,
       },
     ],
     handler: async ({ input }) => {
