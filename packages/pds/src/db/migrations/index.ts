2ecfe879dca8347218808966fbc5b6d652b91c0b
dholms <dtholmgren@gmail.com>
2023-09-13 20:53:36 -0500

    tidy migrations

--- packages/pds/src/db/migrations/index.ts
+++ packages/pds/src/db/migrations/index.ts
@@ -3,12 +3,4 @@
 // this with kysely's FileMigrationProvider, but it doesn't play nicely with the build process.
 
 export * as _20230613T164932261Z from './20230613T164932261Z-init'
-export * as _20230718T170914772Z from './20230718T170914772Z-sequencer-leader-sequence'
-export * as _20230727T172043676Z from './20230727T172043676Z-user-account-cursor-idx'
-export * as _20230801T141349990Z from './20230801T141349990Z-invite-note'
-export * as _20230807T035309811Z from './20230807T035309811Z-feed-item-delete-invite-for-user-idx'
-export * as _20230808T172813122Z from './20230808T172813122Z-repo-rev'
-export * as _20230810T203412859Z from './20230810T203412859Z-action-duration'
-export * as _20230818T134357818Z from './20230818T134357818Z-runtime-flags'
-export * as _20230825T142507884Z from './20230825T142507884Z-blob-tempkey-idx'
-export * as _20230828T153013575Z from './20230828T153013575Z-repo-history-rewrite'
+export * as _20230914T014727199Z from './20230914T014727199Z-repo-v3'
