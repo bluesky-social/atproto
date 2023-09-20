a924f6218c5a72492809d7088a8b417850712db5
Foysal Ahamed <cfaion341@gmail.com>
2023-09-20 17:35:47 +0200

    :sparkles: Use html-to-text plugin for moderation mailer transport

--- packages/pds/src/mailer/moderation.ts
+++ packages/pds/src/mailer/moderation.ts
@@ -1,5 +1,6 @@
 import { Transporter } from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
+import { htmlToText } from 'nodemailer-html-to-text'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 import { ServerConfig } from '../config'
 import { mailerLogger } from '../logger'
@@ -14,22 +15,14 @@ export class ModerationMailer {
   ) {
     this.config = config
     this.transporter = transporter
-  }
-
-  isContentHtml(content: string): boolean {
-    return /<\/?[a-z][\s\S]*>/i.test(content)
+    this.transporter.use('compile', htmlToText())
   }
 
   async send({ content }: { content: string }, mailOpts: Mail.Options) {
     const mail = {
       ...mailOpts,
+      html: content,
       from: this.config.moderationEmailAddress,
-    }
-
-    if (this.isContentHtml(content)) {
-      mail.html = content
-    } else {
-      mail.text = content
     }
 
     const res = await this.transporter.sendMail(mail)
