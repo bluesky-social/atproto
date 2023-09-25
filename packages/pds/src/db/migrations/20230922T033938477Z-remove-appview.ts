c47f8b2731795ce27a4ad5ea4a12ecc928feef48
dholms <dtholmgren@gmail.com>
2023-09-25 18:38:53 -0500

    small tidy

--- packages/pds/src/db/migrations/20230922T033938477Z-remove-appview.ts
+++ packages/pds/src/db/migrations/20230922T033938477Z-remove-appview.ts
@@ -24,6 +24,6 @@ export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema.dropTable('view_param').execute()
 }
 
-export async function down(db: Kysely<unknown>): Promise<void> {
+export async function down(_db: Kysely<unknown>): Promise<void> {
   // Migration code
 }
