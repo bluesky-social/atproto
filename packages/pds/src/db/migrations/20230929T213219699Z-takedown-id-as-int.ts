8db0a8277892a2a316e1bb6978269c252b445c05
Devin Ivy <devinivy@gmail.com>
2023-09-29 17:53:12 -0400

    tidy/fix migration

--- packages/pds/src/db/migrations/20230929T213219699Z-takedown-id-as-int.ts
+++ packages/pds/src/db/migrations/20230929T213219699Z-takedown-id-as-int.ts
@@ -1,23 +1,17 @@
 import { Kysely, sql } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-  sql`alter table "repo_root" alter column "takedownId" type integer using "takedownId"::integer`.execute(
-    db,
-  )
-  sql`alter table "repo_blob" alter column "takedownId" type integer using "takedownId"::integer`.execute(
-    db,
-  )
-  sql`alter table "record" alter column "takedownId" type integer using "takedownId"::integer`.execute(
-    db,
-  )
+  await sql`
+    alter table "repo_root" alter column "takedownId" type integer using "takedownId"::integer;
+    alter table "repo_blob" alter column "takedownId" type integer using "takedownId"::integer;
+    alter table "record" alter column "takedownId" type integer using "takedownId"::integer;
+  `.execute(db)
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  sql`alter table "repo_root" alter column "takedownId" type varchar`.execute(
-    db,
-  )
-  sql`alter table "repo_blob" alter column "takedownId" type varchar`.execute(
-    db,
-  )
-  sql`alter table "record" alter column "takedownId" type varchar`.execute(db)
+  await sql`
+    alter table "repo_root" alter column "takedownId" type varchar;
+    alter table "repo_blob" alter column "takedownId" type varchar;
+    alter table "record" alter column "takedownId" type varchar;
+  `.execute(db)
 }
