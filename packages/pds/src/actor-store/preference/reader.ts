542617c5184bbfe33cc65e2c578ab4d14c764ccc
dholms <dtholmgren@gmail.com>
2023-10-26 15:55:13 -0500

    user pref -> account pref

--- packages/pds/src/actor-store/preference/reader.ts
+++ packages/pds/src/actor-store/preference/reader.ts
@@ -3,9 +3,9 @@ import { ActorDb } from '../db'
 export class PreferenceReader {
   constructor(public db: ActorDb) {}
 
-  async getPreferences(namespace?: string): Promise<UserPreference[]> {
+  async getPreferences(namespace?: string): Promise<AccountPreference[]> {
     const prefsRes = await this.db.db
-      .selectFrom('user_pref')
+      .selectFrom('account_pref')
       .orderBy('id')
       .selectAll()
       .execute()
@@ -15,7 +15,7 @@ export class PreferenceReader {
   }
 }
 
-export type UserPreference = Record<string, unknown> & { $type: string }
+export type AccountPreference = Record<string, unknown> & { $type: string }
 
 export const prefMatchNamespace = (namespace: string, fullname: string) => {
   return fullname === namespace || fullname.startsWith(`${namespace}.`)
--- packages/pds/src/api/app/bsky/actor/putPreferences.ts
+++ packages/pds/src/api/app/bsky/actor/putPreferences.ts
@@ -1,7 +1,7 @@
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
 import { InvalidRequestError } from '@atproto/xrpc-server'
-import { UserPreference } from '../../../../actor-store/preference/reader'
+import { AccountPreference } from '../../../../actor-store/preference/reader'
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.actor.putPreferences({
@@ -9,10 +9,10 @@ export default function (server: Server, ctx: AppContext) {
     handler: async ({ auth, input }) => {
       const { preferences } = input.body
       const requester = auth.credentials.did
-      const checkedPreferences: UserPreference[] = []
+      const checkedPreferences: AccountPreference[] = []
       for (const pref of preferences) {
         if (typeof pref.$type === 'string') {
-          checkedPreferences.push(pref as UserPreference)
+          checkedPreferences.push(pref as AccountPreference)
         } else {
           throw new InvalidRequestError('Preference is missing a $type')
         }
