2df6f2b836509aed83b7559c644f63797f617861
devin ivy <devinivy@gmail.com>
2023-10-31 00:00:00 -0400

    Support S3-compatible credentials for pds blobstore (#1787)
    * support s3 credentials for pds blobstore
    
    * tidy

--- packages/pds/src/config/config.ts
+++ packages/pds/src/config/config.ts
@@ -54,15 +54,23 @@ export const envToCfg = (env: ServerEnvironment): ServerConfig => {
     throw new Error('Cannot set both S3 and disk blobstore env vars')
   }
   if (env.blobstoreS3Bucket) {
-    blobstoreCfg = { provider: 's3', bucket: env.blobstoreS3Bucket }
-    if (env.blobstoreS3Region) {
-      blobstoreCfg.region = env.blobstoreS3Region
+    blobstoreCfg = {
+      provider: 's3',
+      bucket: env.blobstoreS3Bucket,
+      region: env.blobstoreS3Region,
+      endpoint: env.blobstoreS3Endpoint,
+      forcePathStyle: env.blobstoreS3ForcePathStyle,
     }
-    if (env.blobstoreS3Endpoint) {
-      blobstoreCfg.endpoint = env.blobstoreS3Endpoint
-    }
-    if (env.blobstoreS3ForcePathStyle !== undefined) {
-      blobstoreCfg.forcePathStyle = env.blobstoreS3ForcePathStyle
+    if (env.blobstoreS3AccessKeyId || env.blobstoreS3SecretAccessKey) {
+      if (!env.blobstoreS3AccessKeyId || !env.blobstoreS3SecretAccessKey) {
+        throw new Error(
+          'Must specify both S3 access key id and secret access key blobstore env vars',
+        )
+      }
+      blobstoreCfg.credentials = {
+        accessKeyId: env.blobstoreS3AccessKeyId,
+        secretAccessKey: env.blobstoreS3SecretAccessKey,
+      }
     }
   } else if (env.blobstoreDiskLocation) {
     blobstoreCfg = {
@@ -250,6 +258,10 @@ export type S3BlobstoreConfig = {
   region?: string
   endpoint?: string
   forcePathStyle?: boolean
+  credentials?: {
+    accessKeyId: string
+    secretAccessKey: string
+  }
 }
 
 export type DiskBlobstoreConfig = {
