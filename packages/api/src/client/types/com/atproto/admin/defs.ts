697f5d36270f05891167b81a98e4c55cef3c1210
Foysal Ahamed <foysal@blueskyweb.xyz>
2023-11-07 23:26:31 +0000

    :sparkles: Expose emailConfirmedAt field from admin getRepo (#1757)
    * :sparkles: Expose emailConfirmedAt field from admin getRepo
    
    * :recycle: Fix typing for repo result
    
    * :broom: Cleanup unnecessary import
    
    * :sparkles: Adapt to the new pds based get account info method
    
    * :broom: Cleanup unused pds util

--- packages/api/src/client/types/com/atproto/admin/defs.ts
+++ packages/api/src/client/types/com/atproto/admin/defs.ts
@@ -241,6 +241,7 @@ export interface RepoViewDetail {
   invites?: ComAtprotoServerDefs.InviteCode[]
   invitesDisabled?: boolean
   inviteNote?: string
+  emailConfirmedAt?: string
   [k: string]: unknown
 }
 
@@ -264,6 +265,7 @@ export interface AccountView {
   invitedBy?: ComAtprotoServerDefs.InviteCode
   invites?: ComAtprotoServerDefs.InviteCode[]
   invitesDisabled?: boolean
+  emailConfirmedAt?: string
   inviteNote?: string
   [k: string]: unknown
 }
