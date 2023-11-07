ad35ade6e0707fa107db008f9d88fe3ce0395c80
Devin Ivy <devinivy@gmail.com>
2023-11-07 14:10:36 -0500

    better guarantee that acct deletion makes it out to pds behind entryway

--- packages/pds/src/api/com/atproto/server/deleteAccount.ts
+++ packages/pds/src/api/com/atproto/server/deleteAccount.ts
@@ -1,8 +1,13 @@
 import { MINUTE } from '@atproto/common'
-import { AuthRequiredError, UpstreamFailureError } from '@atproto/xrpc-server'
+import {
+  AuthRequiredError,
+  InvalidRequestError,
+  UpstreamFailureError,
+} from '@atproto/xrpc-server'
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
 import { isThisPds } from '../../../proxy'
+import { retryHttp } from '../../../../util/retry'
 
 const REASON_ACCT_DELETION = 'account_deletion'
 
@@ -14,19 +19,24 @@ export default function (server: Server, ctx: AppContext) {
     },
     handler: async ({ input, req }) => {
       const { did, password, token } = input.body
-      const validPass = await ctx.services
-        .account(ctx.db)
-        .verifyAccountPassword(did, password)
+      const accountService = ctx.services.account(ctx.db)
+      const validPass = await accountService.verifyAccountPassword(
+        did,
+        password,
+      )
       if (!validPass) {
         throw new AuthRequiredError('Invalid did or password')
       }
 
-      await ctx.services
-        .account(ctx.db)
-        .assertValidToken(did, 'delete_account', token)
+      const account = await accountService.getAccount(did, true)
+      if (!account) {
+        throw new InvalidRequestError('account not found', 'AccountNotFound')
+      }
+
+      await accountService.assertValidToken(did, 'delete_account', token)
 
       await ctx.db.transaction(async (dbTxn) => {
-        const accountService = ctx.services.account(dbTxn)
+        const accountTxn = ctx.services.account(dbTxn)
         const moderationTxn = ctx.services.moderation(dbTxn)
         const currState = await moderationTxn.getRepoTakedownState(did)
         // Do not disturb an existing takedown, continue with account deletion
@@ -36,38 +46,45 @@ export default function (server: Server, ctx: AppContext) {
             ref: REASON_ACCT_DELETION,
           })
         }
-        await accountService.deleteEmailToken(did, 'delete_account')
+        await accountTxn.deleteEmailToken(did, 'delete_account')
       })
 
-      ctx.backgroundQueue.add(async (db) => {
-        // in the background perform the hard account deletion work
+      const { pdsDid } = account
+      if (ctx.cfg.service.isEntryway && pdsDid && !isThisPds(ctx, pdsDid)) {
         try {
-          const recordService = ctx.services.record(db)
-          const repoService = ctx.services.repo(db)
-          const accountService = ctx.services.account(db)
-          const account = await accountService.getAccount(did, true)
-          const pdsDid = account?.pdsDid
-          await recordService.deleteForActor(did)
-          await repoService.deleteRepo(did)
-          await accountService.deleteAccount(did)
-          // propagate to pds behind entryway, or sequence tombstone on this pds
-          if (ctx.cfg.service.isEntryway && pdsDid && !isThisPds(ctx, pdsDid)) {
-            const pds = await accountService.getPds(pdsDid, { cached: true })
-            if (!pds) {
-              throw new UpstreamFailureError('unknown pds')
-            }
-            // both entryway and pds behind it need to clean-up account state, then pds sequences tombstone.
-            // the long flow is: pds(server.deleteAccount) -> entryway(server.deleteAccount) -> pds(admin.deleteAccount)
-            const agent = ctx.pdsAgents.get(pds.host)
-            await agent.com.atproto.admin.deleteAccount(
+          const pds = await accountService.getPds(pdsDid, { cached: true })
+          if (!pds) {
+            throw new UpstreamFailureError('unknown pds')
+          }
+          // both entryway and pds behind it need to clean-up account state, then pds sequences tombstone.
+          // the long flow is: pds(server.deleteAccount) -> entryway(server.deleteAccount) -> pds(admin.deleteAccount)
+          const agent = ctx.pdsAgents.get(pds.host)
+          await retryHttp(() =>
+            agent.com.atproto.admin.deleteAccount(
               { did },
               {
                 encoding: 'application/json',
                 headers: ctx.authVerifier.createAdminRoleHeaders(),
               },
-            )
-          } else {
-            await accountService.sequenceTombstone(did)
+            ),
+          )
+        } catch (err) {
+          req.log.error(
+            { did, pdsDid, err },
+            'account deletion failed on pds behind entryway',
+          )
+        }
+      }
+
+      ctx.backgroundQueue.add(async (db) => {
+        // in the background perform the hard account deletion work
+        try {
+          await ctx.services.record(db).deleteForActor(did)
+          await ctx.services.repo(db).deleteRepo(did)
+          await ctx.services.account(db).deleteAccount(did)
+          if (!ctx.cfg.service.isEntryway || isThisPds(ctx, pdsDid)) {
+            // if this is the user's pds sequence the tombstone, otherwise taken care of by their pds behind the entryway.
+            await ctx.services.account(db).sequenceTombstone(did)
           }
         } catch (err) {
           req.log.error({ did, err }, 'account deletion failed')
