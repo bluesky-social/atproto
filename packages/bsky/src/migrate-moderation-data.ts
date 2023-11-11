1bf5acc672841ab803d6c49d43041ba5a93b3766
Foysal Ahamed <cfaion341@gmail.com>
2023-11-11 00:34:31 +0000

    :sparkles: Implement direct sql query to create events from actions and reports

--- packages/bsky/src/migrate-moderation-data.ts
+++ packages/bsky/src/migrate-moderation-data.ts
@@ -1,28 +1,6 @@
-import { TimeCidKeyset, paginate } from './db/pagination'
+import { sql } from 'kysely'
 import { DatabaseCoordinator, PrimaryDatabase } from './index'
 import { adjustModerationSubjectStatus } from './services/moderation/status'
-import { ModerationEventRow } from './services/moderation/types'
-
-type ModerationAction = Omit<ModerationEventRow, 'meta' | 'comment'> & {
-  reason: string
-  reversedAt: string | null
-  reversedBy: string | null
-  reversedReason: string | null
-}
-
-type ModerationReport = Pick<
-  ModerationEventRow,
-  | 'id'
-  | 'subjectType'
-  | 'subjectDid'
-  | 'subjectUri'
-  | 'subjectCid'
-  | 'createdAt'
-> & {
-  reasonType: string
-  reason: string | null
-  reportedByDid: string
-}
 
 const getEnv = () => ({
   DB_URL:
@@ -31,176 +9,134 @@ const getEnv = () => ({
   DB_POOL_SIZE: Number(process.env.MODERATION_MIGRATION_DB_URL) || 10,
   DB_SCHEMA: process.env.MODERATION_MIGRATION_DB_SCHEMA || 'bsky',
 })
-export class ListKeyset extends TimeCidKeyset<{
-  createdAt: string
-  id: string
-}> {
-  labelResult(result: { createdAt: string; id: string }) {
-    return { primary: result.createdAt, secondary: `${result.id}` }
-  }
+
+const countEntries = async (db: PrimaryDatabase) => {
+  const [allActions, allReports] = await Promise.all([
+    db.db
+      // @ts-ignore
+      .selectFrom('moderation_action')
+      // @ts-ignore
+      .select((eb) => eb.fn.count<number>('id').as('count'))
+      .executeTakeFirstOrThrow(),
+    db.db
+      // @ts-ignore
+      .selectFrom('moderation_report')
+      // @ts-ignore
+      .select((eb) => eb.fn.count<number>('id').as('count'))
+      .executeTakeFirstOrThrow(),
+  ])
+
+  return { reportsCount: allReports.count, actionsCount: allActions.count }
 }
 
-const transformActionToEvent = (actionEntry: ModerationAction) => {
-  const {
-    id,
-    reversedAt,
-    reversedBy,
-    reversedReason,
-    action,
-    reason,
-    ...rest
-  } = actionEntry
-  const actionName = action.split('#')[1]
-  const event = {
-    ...rest,
-    comment: reason,
-    action:
-      `com.atproto.admin.defs#modEvent${actionName[0].toUpperCase()}${actionName.slice(
-        1,
-      )}` as ModerationEventRow['action'],
-  }
-
-  return event
-}
-
-const transformReportToEvent = (actionEntry: ModerationReport) => {
-  const { id, reasonType, reason, reportedByDid, ...rest } = actionEntry
-  const event = {
-    ...rest,
-    comment: reason,
-    createdBy: reportedByDid,
-    meta: { reportType: reasonType },
-    action:
-      'com.atproto.admin.defs#modEventReport' as ModerationEventRow['action'],
-  }
-
-  return event
-}
-
-// TODO: Figure out what to do with `reversed` actions
-const migrateActions = async (db: PrimaryDatabase) => {
-  const allActions = await db.db
-    // @ts-ignore
-    .selectFrom('moderation_action')
-    // @ts-ignore
+const countEvents = async (db: PrimaryDatabase) => {
+  const events = await db.db
+    .selectFrom('moderation_event')
     .select((eb) => eb.fn.count<number>('id').as('count'))
     .executeTakeFirstOrThrow()
 
-  const chunkSize = 10
-  const totalChunks = Math.ceil(allActions.count / chunkSize)
+  return events.count
+}
 
-  console.log(
-    `Initiating migration for ${allActions.count} actions in ${totalChunks} chunks`,
-  )
+const countStatuses = async (db: PrimaryDatabase) => {
+  const events = await db.db
+    .selectFrom('moderation_subject_status')
+    .select((eb) => eb.fn.count<number>('id').as('count'))
+    .executeTakeFirstOrThrow()
 
-  await db.transaction(async (tx) => {
-    let currentChunk = 1
-    let lastProcessedId = 0
-    do {
-      const actions = (await tx.db
+  return events.count
+}
+
+const createEvents = async (db: PrimaryDatabase) => {
+  const commonColumns = [
+    'subjectDid',
+    'subjectUri',
+    'subjectType',
+    'subjectCid',
+    sql`reason`.as('comment'),
+    'createdAt',
+  ]
+
+  const insertQuery = db.db
+    .insertInto('moderation_event')
+    .columns([
+      'subjectDid',
+      'subjectUri',
+      'subjectType',
+      'subjectCid',
+      'comment',
+      'createdAt',
+      'action',
+      'createLabelVals',
+      'negateLabelVals',
+      'createdBy',
+      'durationInHours',
+      'expiresAt',
+      'meta',
+    ])
+    .expression((eb) =>
+      eb
         // @ts-ignore
         .selectFrom('moderation_action')
         // @ts-ignore
-        .where('id', '>', lastProcessedId)
-        // @ts-ignore
-        .orderBy('id', 'asc')
-        .selectAll()
-        .limit(chunkSize)
-        .execute()) as ModerationAction[]
+        .select([
+          ...commonColumns,
+          sql`CONCAT('com.atproto.admin.defs#modEvent', UPPER(SUBSTRING(SPLIT_PART(action, '#', 2) FROM 1 FOR 1)), SUBSTRING(SPLIT_PART(action, '#', 2) FROM 2))`.as('action'),
+          'createLabelVals',
+          'negateLabelVals',
+          'createdBy',
+          'durationInHours',
+          'expiresAt',
+          sql`NULL`.as('meta'),
+        ])
+        .unionAll(
+          eb
+            // @ts-ignore
+            .selectFrom('moderation_report')
+            // @ts-ignore
+            .select([
+              ...commonColumns,
+              sql`'com.atproto.admin.defs#modEventReport'`.as('action'),
+              sql`NULL`.as('createLabelVals'),
+              sql`NULL`.as('negateLabelVals'),
+              sql`"reportedByDid"`.as('createdBy'),
+              sql`NULL`.as('durationInHours'),
+              sql`NULL`.as('expiresAt'),
+              sql`json_build_object('reportType', "reasonType")`.as('meta'),
+            ]),
+        )
+        .orderBy('createdAt', 'asc'),
+    )
 
-      await tx.db
-        .insertInto('moderation_event')
-        .values(actions.map(transformActionToEvent))
-        .execute()
+  await insertQuery.execute()
+  const totalEvents = await countEvents(db)
+  console.log(`Created ${totalEvents} events`)
 
-      lastProcessedId = actions[actions.length - 1].id
-      console.log(
-        `Processed actions chunk ${currentChunk} of ${totalChunks}. lastProcessedId: ${lastProcessedId}`,
-      )
-      currentChunk++
-    } while (currentChunk <= totalChunks)
-  })
-
-  console.log(`Actions migration complete!`)
-}
-
-const migrateReports = async (db: PrimaryDatabase) => {
-  const allReports = await db.db
-    // @ts-ignore
-    .selectFrom('moderation_report')
-    // @ts-ignore
-    .select((eb) => eb.fn.count<number>('id').as('count'))
-    .executeTakeFirstOrThrow()
-
-  const chunkSize = 10
-  const totalChunks = Math.ceil(allReports.count / chunkSize)
-
-  console.log(
-    `Initiating migration for ${allReports.count} reports in ${totalChunks} chunks`,
-  )
-
-  await db.transaction(async (tx) => {
-    let currentChunk = 1
-    let lastProcessedId = 0
-    do {
-      const reports = (await tx.db
-        // @ts-ignore
-        .selectFrom('moderation_report')
-        // @ts-ignore
-        .where('id', '>', lastProcessedId)
-        // @ts-ignore
-        .orderBy('id', 'asc')
-        .selectAll()
-        .limit(chunkSize)
-        .execute()) as ModerationReport[]
-
-      await tx.db
-        .insertInto('moderation_event')
-        .values(reports.map(transformReportToEvent))
-        .execute()
-
-      lastProcessedId = reports[reports.length - 1].id
-      console.log(
-        `Processed reports chunk ${currentChunk} of ${totalChunks}. lastProcessedId: ${lastProcessedId}`,
-      )
-      currentChunk++
-    } while (currentChunk <= totalChunks)
-  })
-
-  console.log(`Reports migration complete!`)
+  return
 }
 
 const createStatusFromEvents = async (db: PrimaryDatabase) => {
-  const { ref } = db.db.dynamic
   const allEvents = await db.db
-    // @ts-ignore
     .selectFrom('moderation_event')
-    // @ts-ignore
+    .where('action', '!=', 'com.atproto.admin.defs#modEventReport')
     .select((eb) => eb.fn.count<number>('id').as('count'))
     .executeTakeFirstOrThrow()
 
   const chunkSize = 10
   const totalChunks = Math.ceil(allEvents.count / chunkSize)
 
-  console.log(
-    `Initiating re-processing of ${allEvents.count} events in ${totalChunks} chunks`,
-  )
+  console.log(`Processing ${allEvents.count} actions in ${totalChunks} chunks`)
 
   await db.transaction(async (tx) => {
     let currentChunk = 1
-    let cursor
+    let lastProcessedId = 0
     do {
-      const keyset = new ListKeyset(ref('createdAt'), ref('id'))
-
-      const eventsQuery = paginate(tx.db.selectFrom('moderation_event'), {
-        keyset,
-        cursor,
-        limit: chunkSize,
-        direction: 'asc' as const,
-      }).selectAll()
+      const eventsQuery = tx.db
+        .selectFrom('moderation_event')
+        .where('id', '>', lastProcessedId)
+        .limit(chunkSize)
+        .selectAll()
       const events = await eventsQuery.execute()
-      // @ts-ignore
-      cursor = keyset.packFromResult(events)
 
       // TODO: Figure out how to handle blob cids here
       for (const event of events) {
@@ -208,11 +144,15 @@ const createStatusFromEvents = async (db: PrimaryDatabase) => {
       }
 
       console.log(`Processed events chunk ${currentChunk} of ${totalChunks}`)
+      lastProcessedId = events.at(-1)?.id ?? 0
       currentChunk++
-    } while (cursor)
+    } while (currentChunk < totalChunks)
   })
 
   console.log(`Events migration complete!`)
+
+  const totalStatuses = await countStatuses(db)
+  console.log(`Created ${totalStatuses} statuses`)
 }
 
 async function main() {
@@ -227,10 +167,12 @@ async function main() {
   })
 
   const primaryDb = db.getPrimary()
-  // TODO: Is this safe? importing actions and reports sequentially will cause the id sequence to fall out of order
-  // Meaning we will have to depend on `createdAt` in order to identify the chronological order of events and can no longer depend on id
-  await migrateActions(primaryDb)
-  await migrateReports(primaryDb)
+
+  const counts = await countEntries(primaryDb)
+  const totalEntries = counts.actionsCount + counts.reportsCount
+
+  console.log(`Migrating ${totalEntries} rows of actions and reports`)
+  await createEvents(primaryDb)
   await createStatusFromEvents(primaryDb)
 }
 
