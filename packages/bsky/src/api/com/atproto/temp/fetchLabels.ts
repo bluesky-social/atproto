e6fb39dc8453bf95942305e8e69aa035062448cf
Daniel Holmgren <dtholmgren@gmail.com>
2023-11-16 12:27:46 -0600

    Bugfix: fetchLabels (#1862)
    * fix fetch labels
    
    * tidy
    
    * update cast to parseInt

--- packages/bsky/src/api/com/atproto/temp/fetchLabels.ts
+++ packages/bsky/src/api/com/atproto/temp/fetchLabels.ts
@@ -7,13 +7,18 @@ export default function (server: Server, ctx: AppContext) {
     const db = ctx.db.getReplica()
     const since =
       params.since !== undefined ? new Date(params.since).toISOString() : ''
-    const labels = await db.db
+    const labelRes = await db.db
       .selectFrom('label')
       .selectAll()
       .orderBy('label.cts', 'asc')
       .where('cts', '>', since)
       .limit(limit)
       .execute()
+
+    const labels = labelRes.map((l) => ({
+      ...l,
+      cid: l.cid === '' ? undefined : l.cid,
+    }))
 
     return {
       encoding: 'application/json',
