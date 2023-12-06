d9ca2dba4bc6001533d95d3977a41fb9cf8b0b23
dholms <dtholmgren@gmail.com>
2023-12-05 19:40:09 -0600

    threads impl

--- packages/bsky/src/data-plane/server/routes/threads.ts
+++ packages/bsky/src/data-plane/server/routes/threads.ts
@@ -1,8 +1,54 @@
-export default {
+import { ServiceImpl } from '@connectrpc/connect'
+import { Service } from '../../gen/bsky_connect'
+import * as ui8 from 'uint8arrays'
+import { Database } from '../../../db'
+import { keyBy } from '@atproto/common'
+import {
+  getAncestorsAndSelfQb,
+  getDescendentsQb,
+} from '../../../services/util/post'
+
+export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
   async getThread(req) {
-    throw new Error('unimplemented')
+    const { postUri, above, below } = req
+    const [ancestors, descendents] = await Promise.all([
+      getAncestorsAndSelfQb(db.db, {
+        uri: postUri,
+        parentHeight: above,
+      })
+        .selectFrom('ancestor')
+        .selectAll()
+        .execute(),
+      getDescendentsQb(db.db, {
+        uri: postUri,
+        depth: below,
+      })
+        .selectFrom('descendent')
+        .selectAll()
+        .execute(),
+    ])
+    const uris = [
+      ...ancestors.map((p) => p.uri),
+      ...descendents.map((p) => p.uri),
+    ]
+    return { uris }
   },
+
   async getThreadgates(req) {
-    throw new Error('unimplemented')
+    if (req.uris.length === 0) {
+      return { records: [] }
+    }
+    const res = await db.db
+      .selectFrom('record')
+      .selectAll()
+      .where('uri', 'in', req.uris)
+      .execute()
+    const byUri = keyBy(res, 'uri')
+    const records = req.uris.map((uri) => {
+      const row = byUri[uri]
+      const json = row ? row.json : JSON.stringify(null)
+      return ui8.fromString(json, 'utf8')
+    })
+    return { records }
   },
-}
+})
