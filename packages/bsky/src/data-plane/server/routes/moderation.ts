de217c8f51e971e030755a612b295227d9d0f16f
dholms <dtholmgren@gmail.com>
2023-12-05 19:32:00 -0600

    feeds impl

--- packages/bsky/src/data-plane/server/routes/moderation.ts
+++ packages/bsky/src/data-plane/server/routes/moderation.ts
@@ -1,8 +1,23 @@
-export default {
+import { ServiceImpl } from '@connectrpc/connect'
+import { Service } from '../../gen/bsky_connect'
+import { Database } from '../../../db'
+import { sql } from 'kysely'
+
+export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
   async getBlobTakedown(req) {
+    const { cid } = req
+    const takedown = await db.db
+      .selectFrom('moderation_subject_status')
+      .select('id')
+      .where('blobCids', '@>', sql`CAST(${JSON.stringify([cid])} AS JSONB)`)
+      .where('takendown', 'is', true)
+      .executeTakeFirst()
+    return {
+      takenDown: !!takedown,
+    }
+  },
+
+  async updateTakedown(_req) {
     throw new Error('unimplemented')
   },
-  async updateTakedown(req) {
-    throw new Error('unimplemented')
-  },
-}
+})
