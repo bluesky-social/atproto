f3d5c82cdbf5e4ef9fe608421d72c998322156b9
dholms <dtholmgren@gmail.com>
2023-12-07 14:29:10 -0600

    missed one

--- packages/bsky/src/data-plane/server/routes/threads.ts
+++ packages/bsky/src/data-plane/server/routes/threads.ts
@@ -1,8 +1,6 @@
 import { ServiceImpl } from '@connectrpc/connect'
 import { Service } from '../../gen/bsky_connect'
-import * as ui8 from 'uint8arrays'
 import { Database } from '../../../db'
-import { keyBy } from '@atproto/common'
 import {
   getAncestorsAndSelfQb,
   getDescendentsQb,
@@ -32,23 +30,5 @@ export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
       ...descendents.map((p) => p.uri),
     ]
     return { uris }
-  },
-
-  async getThreadgates(req) {
-    if (req.uris.length === 0) {
-      return { records: [] }
-    }
-    const res = await db.db
-      .selectFrom('record')
-      .selectAll()
-      .where('uri', 'in', req.uris)
-      .execute()
-    const byUri = keyBy(res, 'uri')
-    const records = req.uris.map((uri) => {
-      const row = byUri[uri]
-      const json = row ? row.json : JSON.stringify(null)
-      return ui8.fromString(json, 'utf8')
-    })
-    return { records }
   },
 })
