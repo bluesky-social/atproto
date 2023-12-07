4d941c588b21702b544a159b5fbdad9a4d798727
dholms <dtholmgren@gmail.com>
2023-12-07 10:46:56 -0600

    feed hydration

--- packages/bsky/src/hydration/feed.ts
+++ packages/bsky/src/hydration/feed.ts
@@ -3,13 +3,16 @@ import { Record as PostRecord } from '../lexicon/types/app/bsky/feed/post'
 import { Record as LikeRecord } from '../lexicon/types/app/bsky/feed/like'
 import { Record as FeedGenRecord } from '../lexicon/types/app/bsky/feed/generator'
 import { Record as ThreadgateRecord } from '../lexicon/types/app/bsky/feed/threadgate'
-import { HydrationMap } from './util'
+import { HydrationMap, RecordInfo, parseRecord, parseString } from './util'
+import { AtUri } from '@atproto/syntax'
+import { ids } from '../lexicon/lexicons'
 
-export type Post = PostRecord
+export type Post = RecordInfo<PostRecord>
 export type Posts = HydrationMap<Post>
 
 export type PostViewerState = {
   like?: string
+  repost?: string
 }
 
 export type PostViewerStates = HydrationMap<PostViewerState>
@@ -31,7 +34,7 @@ export type FeedGenAgg = {
 
 export type FeedGenAggs = HydrationMap<FeedGenAgg>
 
-export type FeedGen = FeedGenRecord
+export type FeedGen = RecordInfo<FeedGenRecord>
 export type FeedGens = HydrationMap<FeedGen>
 
 export type FeedGenViewerState = {
@@ -40,44 +43,108 @@ export type FeedGenViewerState = {
 
 export type FeedGenViewerStates = HydrationMap<FeedGenViewerState>
 
-export type Threadgate = ThreadgateRecord
+export type Threadgate = RecordInfo<ThreadgateRecord>
 export type Threadgates = HydrationMap<Threadgate>
 
 export class FeedHydrator {
   constructor(public dataplane: DataPlaneClient) {}
 
   async getPosts(uris: string[]): Promise<Posts> {
-    throw new Error('unimplemented')
+    const res = await this.dataplane.getPosts({ uris })
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, parseRecord<PostRecord>(res.records[i]) ?? null)
+    }, new HydrationMap<Post>())
   }
 
   async getPostViewerStates(
     uris: string[],
     viewer: string,
   ): Promise<PostViewerStates> {
-    throw new Error('unimplemented')
+    const stateForPost = async (uri: string) => {
+      const [like, repost] = await Promise.all([
+        this.dataplane.getLikeByActorAndSubject({
+          actorDid: viewer,
+          subjectUri: uri,
+        }),
+        this.dataplane.getRepostByActorAndSubject({
+          actorDid: viewer,
+          subjectUri: uri,
+        }),
+      ])
+      return {
+        like: parseString(like.uri),
+        repost: parseString(repost.uri),
+      }
+    }
+    const states = await Promise.all(uris.map((uri) => stateForPost(uri)))
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, states[i])
+    }, new HydrationMap<PostViewerState>())
   }
 
   async getPostAggregates(uris: string[]): Promise<PostAggs> {
-    throw new Error('unimplemented')
+    const [likes, reposts, replies] = await Promise.all([
+      this.dataplane.getLikeCounts({ uris }),
+      this.dataplane.getRepostCounts({ uris }),
+      this.dataplane.getPostReplyCounts({ uris }),
+    ])
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, {
+        likes: likes.counts[i] ?? 0,
+        reposts: reposts.counts[i] ?? 0,
+        replies: replies.counts[i] ?? 0,
+      })
+    }, new HydrationMap<PostAgg>())
   }
 
   async getFeedGens(uris: string[]): Promise<FeedGens> {
-    throw new Error('unimplemented')
+    const res = await this.dataplane.getFeedGenerators({ uris })
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, parseRecord<FeedGenRecord>(res.records[i]) ?? null)
+    }, new HydrationMap<FeedGen>())
   }
 
   async getFeedGenViewerStates(
     uris: string[],
     viewer: string,
   ): Promise<FeedGenViewerStates> {
-    throw new Error('unimplemented')
+    const stateForPost = async (uri: string) => {
+      const like = await this.dataplane.getLikeByActorAndSubject({
+        actorDid: viewer,
+        subjectUri: uri,
+      })
+      return {
+        like: parseString(like.uri),
+      }
+    }
+    const states = await Promise.all(uris.map((uri) => stateForPost(uri)))
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, states[i])
+    }, new HydrationMap<FeedGenViewerState>())
   }
 
   async getFeedGenAggregates(uris: string[]): Promise<FeedGenAggs> {
-    throw new Error('unimplemented')
+    const likes = await this.dataplane.getLikeCounts({ uris })
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, {
+        likes: likes.counts[i] ?? 0,
+      })
+    }, new HydrationMap<FeedGenAgg>())
   }
 
   async getThreadgatesForPosts(postUris: string[]): Promise<Threadgates> {
-    throw new Error('unimplemented')
+    const uris = postUris.map((uri) => {
+      const parsed = new AtUri(uri)
+      return AtUri.make(
+        parsed.hostname,
+        ids.AppBskyFeedThreadgate,
+        parsed.rkey,
+      ).toString()
+    })
+    const res = await this.dataplane.getThreadgates({ uris })
+    return uris.reduce((acc, uri, i) => {
+      return acc.set(uri, parseRecord<ThreadgateRecord>(res.records[i]) ?? null)
+    }, new HydrationMap<Threadgate>())
   }
 
   // @TODO may not be supported yet by data plane
