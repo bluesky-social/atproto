9d5932a0f56743c41a06e6b108b8d03c1cf9dde3
Matthieu Sieben <matthieu.sieben@gmail.com>
2023-12-08 16:07:33 +0100

    fix(update-main-to-dist): allow a package not to contain publishConfig

--- update-main-to-dist.js
+++ update-main-to-dist.js
@@ -5,5 +5,9 @@ const [dir] = process.argv.slice(2)
 
 pkgJson
   .load(path.resolve(__dirname, dir))
-  .then((pkg) => pkg.update({ main: pkg.content.publishConfig.main }))
+  .then((pkg) =>
+    pkg.update(
+      pkg.content.publishConfig ? { main: pkg.content.publishConfig.main } : {},
+    ),
+  )
   .then((pkg) => pkg.save())
