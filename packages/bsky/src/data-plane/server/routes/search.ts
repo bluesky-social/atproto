9f923743aa654484f01f4efba58975c9663337b9
dholms <dtholmgren@gmail.com>
2023-12-13 15:15:08 -0600

    add search mocks

--- packages/bsky/src/data-plane/server/routes/search.ts
+++ packages/bsky/src/data-plane/server/routes/search.ts
@@ -1,12 +1,60 @@
 import { ServiceImpl } from '@connectrpc/connect'
 import { Service } from '../../gen/bsky_connect'
 import { Database } from '../../../db'
+import {
+  IndexedAtDidKeyset,
+  TimeCidKeyset,
+  paginate,
+} from '../../../db/pagination'
 
 export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
   async searchActors(req) {
-    throw new Error('unimplemented')
+    const { term, limit, cursor } = req
+    const { ref } = db.db.dynamic
+    let builder = db.db
+      .selectFrom('actor')
+      .where('actor.handle', 'like', `%${term}%`)
+      .selectAll()
+
+    const keyset = new IndexedAtDidKeyset(
+      ref('actor.indexedAt'),
+      ref('actor.did'),
+    )
+    builder = paginate(builder, {
+      limit,
+      cursor,
+      keyset,
+      tryIndex: true,
+    })
+
+    const res = await builder.execute()
+
+    return {
+      dids: res.map((row) => row.did),
+      cursor: keyset.packFromResult(res),
+    }
   },
+
   async searchPosts(req) {
-    throw new Error('unimplemented')
+    const { term, limit, cursor } = req
+    const { ref } = db.db.dynamic
+    let builder = db.db
+      .selectFrom('post')
+      .where('post.text', 'like', `%${term}%`)
+      .selectAll()
+
+    const keyset = new TimeCidKeyset(ref('post.sortAt'), ref('post.cid'))
+    builder = paginate(builder, {
+      limit,
+      cursor,
+      keyset,
+      tryIndex: true,
+    })
+
+    const res = await builder.execute()
+    return {
+      uris: res.map((row) => row.uri),
+      cursor: keyset.packFromResult(res),
+    }
   },
 })
