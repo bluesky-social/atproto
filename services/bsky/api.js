83718796ed9f7fb23110d0aaef5fd721394dabcc
Devin Ivy <devinivy@gmail.com>
2023-12-21 14:19:25 -0500

    cull bsky service entrypoint

--- services/bsky/api.js
+++ services/bsky/api.js
@@ -11,195 +11,35 @@ require('dd-trace') // Only works with commonjs
   })
 
 // Tracer code above must come before anything else
-const path = require('path')
-const assert = require('assert')
-const {
-  BunnyInvalidator,
-  CloudfrontInvalidator,
-  MultiImageInvalidator,
-} = require('@atproto/aws')
+const path = require('nose:path')
+const assert = require('node:assert')
 const { Secp256k1Keypair } = require('@atproto/crypto')
-const {
-  DatabaseCoordinator,
-  PrimaryDatabase,
-  ServerConfig,
-  BskyAppView,
-  ViewMaintainer,
-  makeAlgos,
-  PeriodicModerationEventReversal,
-} = require('@atproto/bsky')
+const { ServerConfig, BskyAppView, makeAlgos } = require('@atproto/bsky')
+const { MemoryCache: MemoryDidCache } = require('@atproto/identity')
 
 const main = async () => {
   const env = getEnv()
-  assert(env.dbPrimaryPostgresUrl, 'missing configuration for db')
-
-  if (env.enableMigrations) {
-    // separate db needed for more permissions
-    const migrateDb = new PrimaryDatabase({
-      url: env.dbMigratePostgresUrl,
-      schema: env.dbPostgresSchema,
-      poolSize: 2,
-    })
-    await migrateDb.migrateToLatestOrThrow()
-    await migrateDb.close()
-  }
-
-  const db = new DatabaseCoordinator({
-    schema: env.dbPostgresSchema,
-    primary: {
-      url: env.dbPrimaryPostgresUrl,
-      poolSize: env.dbPrimaryPoolSize || env.dbPoolSize,
-      poolMaxUses: env.dbPoolMaxUses,
-      poolIdleTimeoutMs: env.dbPoolIdleTimeoutMs,
-    },
-    replicas: env.dbReplicaPostgresUrls?.map((url, i) => {
-      return {
-        url,
-        poolSize: env.dbPoolSize,
-        poolMaxUses: env.dbPoolMaxUses,
-        poolIdleTimeoutMs: env.dbPoolIdleTimeoutMs,
-        tags: getTagsForIdx(env.dbReplicaTags, i),
-      }
-    }),
-  })
-  const cfg = ServerConfig.readEnv({
-    port: env.port,
-    version: env.version,
-    dbPrimaryPostgresUrl: env.dbPrimaryPostgresUrl,
-    dbReplicaPostgresUrls: env.dbReplicaPostgresUrls,
-    dbReplicaTags: env.dbReplicaTags,
-    dbPostgresSchema: env.dbPostgresSchema,
-    publicUrl: env.publicUrl,
-    didPlcUrl: env.didPlcUrl,
-    imgUriSalt: env.imgUriSalt,
-    imgUriKey: env.imgUriKey,
-    imgUriEndpoint: env.imgUriEndpoint,
-    blobCacheLocation: env.blobCacheLocation,
-  })
-
+  const config = ServerConfig.readEnv()
+  assert(env.serviceSigningKey, 'must set BSKY_SERVICE_SIGNING_KEY')
   const signingKey = await Secp256k1Keypair.import(env.serviceSigningKey)
-
-  // configure zero, one, or both image invalidators
-  let imgInvalidator
-  const bunnyInvalidator = env.bunnyAccessKey
-    ? new BunnyInvalidator({
-        accessKey: env.bunnyAccessKey,
-        urlPrefix: cfg.imgUriEndpoint,
-      })
-    : undefined
-  const cfInvalidator = env.cfDistributionId
-    ? new CloudfrontInvalidator({
-        distributionId: env.cfDistributionId,
-        pathPrefix: cfg.imgUriEndpoint && new URL(cfg.imgUriEndpoint).pathname,
-      })
-    : undefined
-
-  if (bunnyInvalidator && imgInvalidator) {
-    imgInvalidator = new MultiImageInvalidator([
-      bunnyInvalidator,
-      imgInvalidator,
-    ])
-  } else if (bunnyInvalidator) {
-    imgInvalidator = bunnyInvalidator
-  } else if (cfInvalidator) {
-    imgInvalidator = cfInvalidator
-  }
-
   const algos = env.feedPublisherDid ? makeAlgos(env.feedPublisherDid) : {}
+  const didCache = new MemoryDidCache() // @TODO persistent, shared cache
   const bsky = BskyAppView.create({
-    db,
+    config,
     signingKey,
-    config: cfg,
-    imgInvalidator,
+    didCache,
     algos,
   })
-  // separate db needed for more permissions
-  const viewMaintainerDb = new PrimaryDatabase({
-    url: env.dbMigratePostgresUrl,
-    schema: env.dbPostgresSchema,
-    poolSize: 2,
-  })
-  const viewMaintainer = new ViewMaintainer(viewMaintainerDb, 1800)
-  const viewMaintainerRunning = viewMaintainer.run()
-
-  const periodicModerationEventReversal = new PeriodicModerationEventReversal(
-    bsky.ctx,
-  )
-  const periodicModerationEventReversalRunning =
-    periodicModerationEventReversal.run()
-
   await bsky.start()
-  // Graceful shutdown (see also https://aws.amazon.com/blogs/containers/graceful-shutdowns-with-ecs/)
   process.on('SIGTERM', async () => {
-    // Gracefully shutdown periodic-moderation-event-reversal before destroying bsky instance
-    periodicModerationEventReversal.destroy()
-    await periodicModerationEventReversalRunning
     await bsky.destroy()
-    viewMaintainer.destroy()
-    await viewMaintainerRunning
-    await viewMaintainerDb.close()
   })
 }
 
 const getEnv = () => ({
-  enableMigrations: process.env.ENABLE_MIGRATIONS === 'true',
-  port: parseInt(process.env.PORT),
-  version: process.env.BSKY_VERSION,
-  dbMigratePostgresUrl:
-    process.env.DB_MIGRATE_POSTGRES_URL || process.env.DB_PRIMARY_POSTGRES_URL,
-  dbPrimaryPostgresUrl: process.env.DB_PRIMARY_POSTGRES_URL,
-  dbPrimaryPoolSize: maybeParseInt(process.env.DB_PRIMARY_POOL_SIZE),
-  dbReplicaPostgresUrls: process.env.DB_REPLICA_POSTGRES_URLS
-    ? process.env.DB_REPLICA_POSTGRES_URLS.split(',')
-    : undefined,
-  dbReplicaTags: {
-    '*': getTagIdxs(process.env.DB_REPLICA_TAGS_ANY), // e.g. DB_REPLICA_TAGS_ANY=0,1
-    timeline: getTagIdxs(process.env.DB_REPLICA_TAGS_TIMELINE),
-    feed: getTagIdxs(process.env.DB_REPLICA_TAGS_FEED),
-    search: getTagIdxs(process.env.DB_REPLICA_TAGS_SEARCH),
-    thread: getTagIdxs(process.env.DB_REPLICA_TAGS_THREAD),
-  },
-  dbPostgresSchema: process.env.DB_POSTGRES_SCHEMA || undefined,
-  dbPoolSize: maybeParseInt(process.env.DB_POOL_SIZE),
-  dbPoolMaxUses: maybeParseInt(process.env.DB_POOL_MAX_USES),
-  dbPoolIdleTimeoutMs: maybeParseInt(process.env.DB_POOL_IDLE_TIMEOUT_MS),
-  serviceSigningKey: process.env.SERVICE_SIGNING_KEY,
-  publicUrl: process.env.PUBLIC_URL,
-  didPlcUrl: process.env.DID_PLC_URL,
-  imgUriSalt: process.env.IMG_URI_SALT,
-  imgUriKey: process.env.IMG_URI_KEY,
-  imgUriEndpoint: process.env.IMG_URI_ENDPOINT,
-  blobCacheLocation: process.env.BLOB_CACHE_LOC,
-  bunnyAccessKey: process.env.BUNNY_ACCESS_KEY,
-  cfDistributionId: process.env.CF_DISTRIBUTION_ID,
-  feedPublisherDid: process.env.FEED_PUBLISHER_DID,
+  serviceSigningKey: process.env.BSKY_SERVICE_SIGNING_KEY || undefined,
+  feedPublisherDid: process.env.BSKY_FEED_PUBLISHER_DID || undefined,
 })
-
-/**
- * @param {Record<string, number[]>} tags
- * @param {number} idx
- */
-const getTagsForIdx = (tagMap, idx) => {
-  const tags = []
-  for (const [tag, indexes] of Object.entries(tagMap)) {
-    if (indexes.includes(idx)) {
-      tags.push(tag)
-    }
-  }
-  return tags
-}
-
-/**
- * @param {string} str
- */
-const getTagIdxs = (str) => {
-  return str ? str.split(',').map((item) => parseInt(item, 10)) : []
-}
-
-const maybeParseInt = (str) => {
-  const parsed = parseInt(str)
-  return isNaN(parsed) ? undefined : parsed
-}
 
 const maintainXrpcResource = (span, req) => {
   // Show actual xrpc method as resource rather than the route pattern
