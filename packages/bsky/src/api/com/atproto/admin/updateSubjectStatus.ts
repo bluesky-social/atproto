23ea84dd950358fb37fcd90973fbe7375ec31ab7
dholms <dtholmgren@gmail.com>
2023-12-21 11:35:56 -0600

    rm try/catch

--- packages/bsky/src/api/com/atproto/admin/updateSubjectStatus.ts
+++ packages/bsky/src/api/com/atproto/admin/updateSubjectStatus.ts
@@ -22,57 +22,52 @@ export default function (server: Server, ctx: AppContext) {
 
       const modService = ctx.services.moderation(ctx.db.getPrimary())
 
-      try {
-        const { subject, takedown } = input.body
-        if (takedown) {
-          if (isRepoRef(subject)) {
-            const did = subject.did
-            if (takedown.applied) {
-              await modService.takedownRepo({
-                takedownId: takedown.ref ?? new Date().toISOString(),
-                did,
-              })
-            } else {
-              await modService.reverseTakedownRepo({ did })
-            }
-          } else if (isStrongRef(subject)) {
-            const uri = new AtUri(subject.uri)
-            const cid = CID.parse(subject.cid)
-            if (takedown.applied) {
-              await modService.takedownRecord({
-                takedownId: takedown.ref ?? new Date().toISOString(),
-                uri,
-                cid,
-              })
-            } else {
-              await modService.reverseTakedownRecord({ uri })
-            }
-          } else if (isRepoBlobRef(subject)) {
-            const { did, cid } = subject
-            if (takedown.applied) {
-              await modService.takedownBlob({
-                takedownId: takedown.ref ?? new Date().toISOString(),
-                did,
-                cid,
-              })
-            } else {
-              await modService.reverseTakedownBlob({ did, cid })
-            }
+      const { subject, takedown } = input.body
+      if (takedown) {
+        if (isRepoRef(subject)) {
+          const did = subject.did
+          if (takedown.applied) {
+            await modService.takedownRepo({
+              takedownId: takedown.ref ?? new Date().toISOString(),
+              did,
+            })
           } else {
-            throw new InvalidRequestError('Invalid subject')
+            await modService.reverseTakedownRepo({ did })
           }
+        } else if (isStrongRef(subject)) {
+          const uri = new AtUri(subject.uri)
+          const cid = CID.parse(subject.cid)
+          if (takedown.applied) {
+            await modService.takedownRecord({
+              takedownId: takedown.ref ?? new Date().toISOString(),
+              uri,
+              cid,
+            })
+          } else {
+            await modService.reverseTakedownRecord({ uri })
+          }
+        } else if (isRepoBlobRef(subject)) {
+          const { did, cid } = subject
+          if (takedown.applied) {
+            await modService.takedownBlob({
+              takedownId: takedown.ref ?? new Date().toISOString(),
+              did,
+              cid,
+            })
+          } else {
+            await modService.reverseTakedownBlob({ did, cid })
+          }
+        } else {
+          throw new InvalidRequestError('Invalid subject')
         }
+      }
 
-        return {
-          encoding: 'application/json',
-          body: {
-            subject,
-            takedown,
-          },
-        }
-      } catch (err) {
-        console.log('ERR: ', err)
-        throw err
+      return {
+        encoding: 'application/json',
+        body: {
+          subject,
+          takedown,
+        },
       }
     },
   })
