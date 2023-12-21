50f209e6507d8bfff20fb78eae1d3ec963e926de
bnewbold <bnewbold@robocracy.org>
2023-12-21 18:01:21 +0100

    bsky: add top-level and robots.txt endpoints (#1979)
    These provide helpful context (instead of just a generic 404).
    
    The robots.txt in particular will show up for public.api.bsky.app

--- packages/bsky/src/api/health.ts
+++ packages/bsky/src/api/health.ts
@@ -5,6 +5,20 @@ import AppContext from '../context'
 export const createRouter = (ctx: AppContext): express.Router => {
   const router = express.Router()
 
+  router.get('/', function (req, res) {
+    res.type('text/plain')
+    res.send(
+      'This is an AT Protocol Application View (AppView) for the "bsky.app" application: https://github.com/bluesky-social/atproto\n\nMost API routes are under /xrpc/',
+    )
+  })
+
+  router.get('/robots.txt', function (req, res) {
+    res.type('text/plain')
+    res.send(
+      '# Hello Friends!\n\n# Crawling the public parts of the API is allowed. HTTP 429 ("backoff") status codes are used for rate-limiting. Up to a handful concurrent requests should be ok.\nUser-agent: *\nAllow: /',
+    )
+  })
+
   router.get('/xrpc/_health', async function (req, res) {
     const { version } = ctx.cfg
     const db = ctx.db.getPrimary()
