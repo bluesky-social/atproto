52f7ea6d21a06ea2f7a157ebe2603e53a84a9f7c
Eric Bailey <git@esb.lol>
2024-01-04 18:53:09 -0600

    Revert debug change

--- packages/pds/src/account-manager/helpers/auth.ts
+++ packages/pds/src/account-manager/helpers/auth.ts
@@ -42,7 +42,7 @@ export const createAccessToken = (opts: {
     jwtKey,
     serviceDid,
     scope = AuthScope.Access,
-    expiresIn = '5s',
+    expiresIn = '120mins',
   } = opts
   const signer = new jose.SignJWT({ scope })
     .setProtectedHeader({ alg: 'HS256' }) // only symmetric keys supported
