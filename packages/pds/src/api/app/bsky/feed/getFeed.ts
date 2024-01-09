d3eb545fce5582ed8142058de2731171b3878739
Cooper Edmunds <cooper6789@gmail.com>
2024-01-09 11:59:20 -0500

    Pass headers to feed generator

--- packages/pds/src/api/app/bsky/feed/getFeed.ts
+++ packages/pds/src/api/app/bsky/feed/getFeed.ts
@@ -4,7 +4,7 @@ import AppContext from '../../../../context'
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.getFeed({
     auth: ctx.authVerifier.access,
-    handler: async ({ params, auth }) => {
+    handler: async ({ req, params, auth }) => {
       const requester = auth.credentials.did
 
       const { data: feed } =
@@ -12,9 +12,22 @@ export default function (server: Server, ctx: AppContext) {
           { feed: params.feed },
           await ctx.appviewAuthHeaders(requester),
         )
+      const serviceAuthHeaders = await ctx.serviceAuthHeaders(
+        requester,
+        feed.view.did,
+      )
+      const headersToPassThru: Record<string, string> = {}
+      for (const [key, value] of Object.entries(req.headers)) {
+        if (typeof value === 'string') {
+          headersToPassThru[key] = value
+        }
+      }
+      const feedOpts = {
+        headers: { ...headersToPassThru, ...serviceAuthHeaders.headers },
+      }
       const res = await ctx.appViewAgent.api.app.bsky.feed.getFeed(
         params,
-        await ctx.serviceAuthHeaders(requester, feed.view.did),
+        feedOpts,
       )
       return {
         encoding: 'application/json',
