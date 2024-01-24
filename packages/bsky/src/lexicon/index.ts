ecf94e3c16d8773929db9ccb665843c6bb7aed72
dholms <dtholmgren@gmail.com>
2024-01-23 20:44:24 -0600

    fix codegen

--- packages/bsky/src/lexicon/index.ts
+++ packages/bsky/src/lexicon/index.ts
@@ -166,39 +166,39 @@ export class Server {
 
 export class ComNS {
   _server: Server
-  atproto: AtprotoNS
+  atproto: ComAtprotoNS
 
   constructor(server: Server) {
     this._server = server
-    this.atproto = new AtprotoNS(server)
+    this.atproto = new ComAtprotoNS(server)
   }
 }
 
-export class AtprotoNS {
+export class ComAtprotoNS {
   _server: Server
-  admin: AdminNS
-  identity: IdentityNS
-  label: LabelNS
-  moderation: ModerationNS
-  repo: RepoNS
-  server: ServerNS
-  sync: SyncNS
-  temp: TempNS
+  admin: ComAtprotoAdminNS
+  identity: ComAtprotoIdentityNS
+  label: ComAtprotoLabelNS
+  moderation: ComAtprotoModerationNS
+  repo: ComAtprotoRepoNS
+  server: ComAtprotoServerNS
+  sync: ComAtprotoSyncNS
+  temp: ComAtprotoTempNS
 
   constructor(server: Server) {
     this._server = server
-    this.admin = new AdminNS(server)
-    this.identity = new IdentityNS(server)
-    this.label = new LabelNS(server)
-    this.moderation = new ModerationNS(server)
-    this.repo = new RepoNS(server)
-    this.server = new ServerNS(server)
-    this.sync = new SyncNS(server)
-    this.temp = new TempNS(server)
+    this.admin = new ComAtprotoAdminNS(server)
+    this.identity = new ComAtprotoIdentityNS(server)
+    this.label = new ComAtprotoLabelNS(server)
+    this.moderation = new ComAtprotoModerationNS(server)
+    this.repo = new ComAtprotoRepoNS(server)
+    this.server = new ComAtprotoServerNS(server)
+    this.sync = new ComAtprotoSyncNS(server)
+    this.temp = new ComAtprotoTempNS(server)
   }
 }
 
-export class AdminNS {
+export class ComAtprotoAdminNS {
   _server: Server
 
   constructor(server: Server) {
@@ -459,7 +459,7 @@ export class AdminNS {
   }
 }
 
-export class IdentityNS {
+export class ComAtprotoIdentityNS {
   _server: Server
 
   constructor(server: Server) {
@@ -489,7 +489,7 @@ export class IdentityNS {
   }
 }
 
-export class LabelNS {
+export class ComAtprotoLabelNS {
   _server: Server
 
   constructor(server: Server) {
@@ -519,7 +519,7 @@ export class LabelNS {
   }
 }
 
-export class ModerationNS {
+export class ComAtprotoModerationNS {
   _server: Server
 
   constructor(server: Server) {
@@ -538,7 +538,7 @@ export class ModerationNS {
   }
 }
 
-export class RepoNS {
+export class ComAtprotoRepoNS {
   _server: Server
 
   constructor(server: Server) {
@@ -634,7 +634,7 @@ export class RepoNS {
   }
 }
 
-export class ServerNS {
+export class ComAtprotoServerNS {
   _server: Server
 
   constructor(server: Server) {
@@ -873,7 +873,7 @@ export class ServerNS {
   }
 }
 
-export class SyncNS {
+export class ComAtprotoSyncNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1013,7 +1013,7 @@ export class SyncNS {
   }
 }
 
-export class TempNS {
+export class ComAtprotoTempNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1078,37 +1078,37 @@ export class TempNS {
 
 export class AppNS {
   _server: Server
-  bsky: BskyNS
+  bsky: AppBskyNS
 
   constructor(server: Server) {
     this._server = server
-    this.bsky = new BskyNS(server)
+    this.bsky = new AppBskyNS(server)
   }
 }
 
-export class BskyNS {
+export class AppBskyNS {
   _server: Server
-  actor: ActorNS
-  embed: EmbedNS
-  feed: FeedNS
-  graph: GraphNS
-  notification: NotificationNS
-  richtext: RichtextNS
-  unspecced: UnspeccedNS
+  actor: AppBskyActorNS
+  embed: AppBskyEmbedNS
+  feed: AppBskyFeedNS
+  graph: AppBskyGraphNS
+  notification: AppBskyNotificationNS
+  richtext: AppBskyRichtextNS
+  unspecced: AppBskyUnspeccedNS
 
   constructor(server: Server) {
     this._server = server
-    this.actor = new ActorNS(server)
-    this.embed = new EmbedNS(server)
-    this.feed = new FeedNS(server)
-    this.graph = new GraphNS(server)
-    this.notification = new NotificationNS(server)
-    this.richtext = new RichtextNS(server)
-    this.unspecced = new UnspeccedNS(server)
+    this.actor = new AppBskyActorNS(server)
+    this.embed = new AppBskyEmbedNS(server)
+    this.feed = new AppBskyFeedNS(server)
+    this.graph = new AppBskyGraphNS(server)
+    this.notification = new AppBskyNotificationNS(server)
+    this.richtext = new AppBskyRichtextNS(server)
+    this.unspecced = new AppBskyUnspeccedNS(server)
   }
 }
 
-export class ActorNS {
+export class AppBskyActorNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1193,7 +1193,7 @@ export class ActorNS {
   }
 }
 
-export class EmbedNS {
+export class AppBskyEmbedNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1201,7 +1201,7 @@ export class EmbedNS {
   }
 }
 
-export class FeedNS {
+export class AppBskyFeedNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1385,7 +1385,7 @@ export class FeedNS {
   }
 }
 
-export class GraphNS {
+export class AppBskyGraphNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1536,7 +1536,7 @@ export class GraphNS {
   }
 }
 
-export class NotificationNS {
+export class AppBskyNotificationNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1588,7 +1588,7 @@ export class NotificationNS {
   }
 }
 
-export class RichtextNS {
+export class AppBskyRichtextNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1596,7 +1596,7 @@ export class RichtextNS {
   }
 }
 
-export class UnspeccedNS {
+export class AppBskyUnspeccedNS {
   _server: Server
 
   constructor(server: Server) {
@@ -1670,11 +1670,13 @@ type RouteRateLimitOpts<T> = {
   calcKey?: (ctx: T) => string
   calcPoints?: (ctx: T) => number
 }
+type HandlerOpts = { blobLimit?: number }
 type HandlerRateLimitOpts<T> = SharedRateLimitOpts<T> | RouteRateLimitOpts<T>
 type ConfigOf<Auth, Handler, ReqCtx> =
   | Handler
   | {
       auth?: Auth
+      opts?: HandlerOpts
       rateLimit?: HandlerRateLimitOpts<ReqCtx> | HandlerRateLimitOpts<ReqCtx>[]
       handler: Handler
     }
