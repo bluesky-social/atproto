2178b15e4bb5c376f9c34e84347bd542747dec87
devin ivy <devinivy@gmail.com>
2024-01-23 19:03:47 -0500

    Appview v2 initial implementation for getPopularFeedGenerators (#2072)
    add an initial implementation for getPopularFeedGenerators on appview v2

--- packages/bsky/src/api/app/bsky/unspecced/getPopularFeedGenerators.ts
+++ packages/bsky/src/api/app/bsky/unspecced/getPopularFeedGenerators.ts
@@ -1,13 +1,35 @@
+import { mapDefined } from '@atproto/common'
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
+import { parseString } from '../../../../hydration/util'
 
 // THIS IS A TEMPORARY UNSPECCED ROUTE
+// @TODO currently mirrors getSuggestedFeeds and ignores the "query" param.
+// In the future may take into consideration popularity via likes w/ its own dataplane endpoint.
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.unspecced.getPopularFeedGenerators({
     auth: ctx.authVerifier.standardOptional,
-    handler: async (_reqCtx) => {
-      // @TODO for appview v2
-      throw new Error('unimplemented')
+    handler: async ({ auth, params }) => {
+      const viewer = auth.credentials.iss
+
+      const suggestedRes = await ctx.dataplane.getSuggestedFeeds({
+        actorDid: viewer ?? undefined,
+        limit: params.limit,
+        cursor: params.cursor,
+      })
+      const uris = suggestedRes.uris
+      const hydration = await ctx.hydrator.hydrateFeedGens(uris, viewer)
+      const feedViews = mapDefined(uris, (uri) =>
+        ctx.views.feedGenerator(uri, hydration),
+      )
+
+      return {
+        encoding: 'application/json',
+        body: {
+          feeds: feedViews,
+          cursor: parseString(suggestedRes.cursor),
+        },
+      }
     },
   })
 }
