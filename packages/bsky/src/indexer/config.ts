9c4ec3d869d17ef13f0535f25b5f7c51dadddead
dholms <dtholmgren@gmail.com>
2024-01-29 15:56:13 -0600

    rm fuzzy matcher & abyss

--- packages/bsky/src/indexer/config.ts
+++ packages/bsky/src/indexer/config.ts
@@ -15,11 +15,7 @@ export interface IndexerConfigValues {
   didCacheMaxTTL: number
   handleResolveNameservers?: string[]
   hiveApiKey?: string
-  abyssEndpoint?: string
-  abyssPassword?: string
   imgUriEndpoint?: string
-  fuzzyMatchB64?: string
-  fuzzyFalsePositiveB64?: string
   labelerKeywords: Record<string, string>
   moderationPushUrl: string
   courierUrl?: string
@@ -89,8 +85,6 @@ export class IndexerConfig {
       process.env.BSKY_COURIER_IGNORE_BAD_TLS === 'true'
     assert(courierHttpVersion === '1.1' || courierHttpVersion === '2')
     const hiveApiKey = process.env.HIVE_API_KEY || undefined
-    const abyssEndpoint = process.env.ABYSS_ENDPOINT
-    const abyssPassword = process.env.ABYSS_PASSWORD
     const imgUriEndpoint = process.env.IMG_URI_ENDPOINT
     const indexerPartitionIds =
       overrides?.indexerPartitionIds ||
@@ -109,9 +103,6 @@ export class IndexerConfig {
     const ingesterPartitionCount =
       maybeParseInt(process.env.INGESTER_PARTITION_COUNT) ?? 64
     const labelerKeywords = {}
-    const fuzzyMatchB64 = process.env.FUZZY_MATCH_B64 || undefined
-    const fuzzyFalsePositiveB64 =
-      process.env.FUZZY_FALSE_POSITIVE_B64 || undefined
     const pushNotificationEndpoint = process.env.PUSH_NOTIFICATION_ENDPOINT
     assert(dbPostgresUrl)
     assert(redisHost || (redisSentinelName && redisSentinelHosts?.length))
@@ -135,8 +126,6 @@ export class IndexerConfig {
       courierHttpVersion,
       courierIgnoreBadTls,
       hiveApiKey,
-      abyssEndpoint,
-      abyssPassword,
       imgUriEndpoint,
       indexerPartitionIds,
       indexerConcurrency,
@@ -146,8 +135,6 @@ export class IndexerConfig {
       indexerPort,
       ingesterPartitionCount,
       labelerKeywords,
-      fuzzyMatchB64,
-      fuzzyFalsePositiveB64,
       pushNotificationEndpoint,
       ...stripUndefineds(overrides ?? {}),
     })
@@ -225,14 +212,6 @@ export class IndexerConfig {
     return this.cfg.hiveApiKey
   }
 
-  get abyssEndpoint() {
-    return this.cfg.abyssEndpoint
-  }
-
-  get abyssPassword() {
-    return this.cfg.abyssPassword
-  }
-
   get imgUriEndpoint() {
     return this.cfg.imgUriEndpoint
   }
@@ -267,14 +246,6 @@ export class IndexerConfig {
 
   get labelerKeywords() {
     return this.cfg.labelerKeywords
-  }
-
-  get fuzzyMatchB64() {
-    return this.cfg.fuzzyMatchB64
-  }
-
-  get fuzzyFalsePositiveB64() {
-    return this.cfg.fuzzyFalsePositiveB64
   }
 
   get pushNotificationEndpoint() {
