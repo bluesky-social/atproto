d79bafffa4a9367456b89752f35e67d70bdfa3fc
dholms <dtholmgren@gmail.com>
2024-01-30 11:24:22 -0600

    hooking up courier

--- packages/pds/src/signup-queue/notifier.ts
+++ packages/pds/src/signup-queue/notifier.ts
@@ -1,11 +1,30 @@
+import { Timestamp } from '@bufbuild/protobuf'
+import { CourierClient } from '../courier'
 import { ServerMailer } from '../mailer'
 
 export class ActivationNotifier {
-  constructor(public mailer: ServerMailer) {}
+  constructor(
+    public mailer: ServerMailer,
+    public courierClient: CourierClient,
+  ) {}
 
   async sendEmail(email: string, handle: string) {
     await this.mailer.sendAccountActivated({ handle }, { to: email })
   }
 
-  async sendPushNotif(email: string) {}
+  async sendPushNotif(did: string) {
+    await this.courierClient.pushNotifications({
+      notifications: [
+        {
+          id: `${did}-account-activated`,
+          recipientDid: did,
+          title: 'Great news!',
+          message: 'Your Bluesky account is ready to go',
+          collapseKey: 'account-activated',
+          alwaysDeliver: true,
+          timestamp: Timestamp.fromDate(new Date()),
+        },
+      ],
+    })
+  }
 }
