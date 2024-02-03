32084200e8f2539b51bb7f320977f2e748ba63ce
Daniel Holmgren <dtholmgren@gmail.com>
2024-02-03 15:55:46 -0600

    Add rate limits to email routes (#2129)
    * add rate limits to email routes
    
    * did based only
    
    * tweak

--- packages/pds/src/api/com/atproto/server/requestPasswordReset.ts
+++ packages/pds/src/api/com/atproto/server/requestPasswordReset.ts
@@ -1,20 +1,35 @@
+import { DAY, HOUR } from '@atproto/common'
 import AppContext from '../../../../context'
 import { Server } from '../../../../lexicon'
 
 export default function (server: Server, ctx: AppContext) {
-  server.com.atproto.server.requestPasswordReset(async ({ input }) => {
-    const email = input.body.email.toLowerCase()
+  server.com.atproto.server.requestPasswordReset({
+    rateLimit: [
+      {
+        durationMs: DAY,
+        points: 15,
+        calcKey: ({ input }) => input.body.email.toLowerCase(),
+      },
+      {
+        durationMs: HOUR,
+        points: 5,
+        calcKey: ({ input }) => input.body.email.toLowerCase(),
+      },
+    ],
+    handler: async ({ input }) => {
+      const email = input.body.email.toLowerCase()
 
-    const user = await ctx.services.account(ctx.db).getAccountByEmail(email)
+      const user = await ctx.services.account(ctx.db).getAccountByEmail(email)
 
-    if (user) {
-      const token = await ctx.services
-        .account(ctx.db)
-        .createEmailToken(user.did, 'reset_password')
-      await ctx.mailer.sendResetPassword(
-        { handle: user.handle, token },
-        { to: user.email },
-      )
-    }
+      if (user) {
+        const token = await ctx.services
+          .account(ctx.db)
+          .createEmailToken(user.did, 'reset_password')
+        await ctx.mailer.sendResetPassword(
+          { handle: user.handle, token },
+          { to: user.email },
+        )
+      }
+    },
   })
 }
