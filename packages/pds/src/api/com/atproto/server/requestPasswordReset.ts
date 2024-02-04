34ba54edd2b4944229d66b7063a61a78c047ab16
dholms <dtholmgren@gmail.com>
2024-02-03 18:36:40 -0600

    email rate limits followup

--- packages/pds/src/api/com/atproto/server/requestPasswordReset.ts
+++ packages/pds/src/api/com/atproto/server/requestPasswordReset.ts
@@ -1,32 +1,45 @@
+import { DAY, HOUR } from '@atproto/common'
 import { InvalidRequestError } from '@atproto/xrpc-server'
 import AppContext from '../../../../context'
 import { Server } from '../../../../lexicon'
 import { authPassthru } from '../../../proxy'
 
 export default function (server: Server, ctx: AppContext) {
-  server.com.atproto.server.requestPasswordReset(async ({ input, req }) => {
-    const email = input.body.email.toLowerCase()
+  server.com.atproto.server.requestPasswordReset({
+    rateLimit: [
+      {
+        durationMs: DAY,
+        points: 50,
+      },
+      {
+        durationMs: HOUR,
+        points: 15,
+      },
+    ],
+    handler: async ({ input, req }) => {
+      const email = input.body.email.toLowerCase()
 
-    const account = await ctx.accountManager.getAccountByEmail(email)
+      const account = await ctx.accountManager.getAccountByEmail(email)
 
-    if (!account?.email) {
-      if (ctx.entrywayAgent) {
-        await ctx.entrywayAgent.com.atproto.server.requestPasswordReset(
-          input.body,
-          authPassthru(req, true),
-        )
-        return
+      if (!account?.email) {
+        if (ctx.entrywayAgent) {
+          await ctx.entrywayAgent.com.atproto.server.requestPasswordReset(
+            input.body,
+            authPassthru(req, true),
+          )
+          return
+        }
+        throw new InvalidRequestError('account does not have an email address')
       }
-      throw new InvalidRequestError('account does not have an email address')
-    }
 
-    const token = await ctx.accountManager.createEmailToken(
-      account.did,
-      'reset_password',
-    )
-    await ctx.mailer.sendResetPassword(
-      { identifier: account.handle ?? account.email, token },
-      { to: account.email },
-    )
+      const token = await ctx.accountManager.createEmailToken(
+        account.did,
+        'reset_password',
+      )
+      await ctx.mailer.sendResetPassword(
+        { identifier: account.handle ?? account.email, token },
+        { to: account.email },
+      )
+    },
   })
 }
