de6000287fae79386c51652b53f9e9630e1a6956
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-02-12 17:24:31 +0100

    chore(build): prevent bundling of built output

---
+++ packages/pds/build.templates.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+const hbsPlugin = require('esbuild-plugin-handlebars')
+const { globSync } = require('glob')
+
+require('esbuild').build({
+  logLevel: 'info',
+  watch: process.argv.includes('--watch'),
+  entryPoints: globSync('src/**/*.hbs'),
+  sourcemap: true,
+  outdir: 'dist/mailer/templates',
+  platform: 'node',
+  plugins: [
+    hbsPlugin({
+      filter: /\.(hbs)$/,
+      additionalHelpers: {},
+      precompileOptions: {},
+    }),
+  ],
+})
--- packages/pds/src/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.hbs" {
-  import { TemplateDelegate } from "handlebars";
-  const template: TemplateDelegate<unknown>;
-  export default template;
-}
