925acd869adffdebf5726fef607585de9767f20f
dholms <dtholmgren@gmail.com>
2024-02-15 15:02:03 -0600

    emit event on account activation

--- packages/pds/src/api/com/atproto/server/activateAccount.ts
+++ packages/pds/src/api/com/atproto/server/activateAccount.ts
@@ -1,6 +1,7 @@
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
 import { assertValidDidDocumentForService } from './util'
+import { BlockMap, CidSet } from '@atproto/repo'
 
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.server.activateAccount({
@@ -11,6 +12,20 @@ export default function (server: Server, ctx: AppContext) {
       await assertValidDidDocumentForService(ctx, requester)
 
       await ctx.accountManager.activateAccount(requester)
+
+      const root = await ctx.actorStore.read(requester, (store) =>
+        store.repo.storage.getRootDetailed(),
+      )
+      const commitData = {
+        cid: root.cid,
+        rev: root.rev,
+        since: null,
+        prev: null,
+        newBlocks: new BlockMap(),
+        removedCids: new CidSet(),
+      }
+
+      await ctx.sequencer.sequenceCommit(requester, commitData, [])
     },
   })
 }
