81f8458392f1dabd2e4ce4282dcb213cd14ad3d1
dholms <dtholmgren@gmail.com>
2024-02-15 10:41:49 -0600

    tidy activate account

--- packages/pds/src/api/com/atproto/server/activateAccount.ts
+++ packages/pds/src/api/com/atproto/server/activateAccount.ts
@@ -1,6 +1,6 @@
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
-import { getPdsEndpoint, getSigningKey } from '@atproto/common'
+import { getPdsEndpoint, getSigningDidKey } from '@atproto/common'
 import { InvalidRequestError } from '@atproto/xrpc-server'
 
 export default function (server: Server, ctx: AppContext) {
@@ -8,40 +8,59 @@ export default function (server: Server, ctx: AppContext) {
     auth: ctx.authVerifier.accessNotAppPassword,
     handler: async ({ auth }) => {
       const requester = auth.credentials.did
-      let pdsEndpoint: string | undefined
-      let signingKey: string | undefined
+
       if (requester.startsWith('did:plc')) {
         const resolved = await ctx.plcClient.getDocumentData(requester)
-        pdsEndpoint = resolved.services['atproto_pds']?.endpoint
-        signingKey = resolved.verificationMethods['atproto']
-        if (!resolved.rotationKeys.includes(ctx.plcRotationKey.did())) {
-          throw new InvalidRequestError(
-            'Server rotation key not included in PLC DID data',
-          )
-        }
+        await assertValidDocContents(ctx, requester, {
+          pdsEndpoint: resolved.services['atproto_pds']?.endpoint,
+          signingKey: resolved.verificationMethods['atproto'],
+          rotationKeys: resolved.rotationKeys,
+        })
       } else {
         const resolved = await ctx.idResolver.did.resolve(requester, true)
         if (!resolved) {
           throw new InvalidRequestError('Could not resolve DID')
         }
-        pdsEndpoint = getPdsEndpoint(resolved)
-        // @TODO tidy this up using SDK methods
-        const pubkeyMultibase = getSigningKey(resolved)?.publicKeyMultibase
-        signingKey = pubkeyMultibase ? `did:key:${pubkeyMultibase}` : undefined
-      }
-      const keypair = await ctx.actorStore.keypair(requester)
-      if (!signingKey || signingKey !== keypair.did()) {
-        throw new InvalidRequestError(
-          'DID document verification method does not match expected signing key',
-        )
-      }
-      if (!pdsEndpoint || pdsEndpoint !== ctx.cfg.service.publicUrl) {
-        throw new InvalidRequestError(
-          'DID document atproto_pds service endpoint does not match PDS public url',
-        )
+        await assertValidDocContents(ctx, requester, {
+          pdsEndpoint: getPdsEndpoint(resolved),
+          signingKey: getSigningDidKey(resolved),
+        })
       }
 
       await ctx.accountManager.activateAccount(requester)
     },
   })
+}
+
+const assertValidDocContents = async (
+  ctx: AppContext,
+  did: string,
+  contents: {
+    signingKey?: string
+    pdsEndpoint?: string
+    rotationKeys?: string[]
+  },
+) => {
+  const { signingKey, pdsEndpoint, rotationKeys } = contents
+  if (
+    rotationKeys !== undefined &&
+    !rotationKeys.includes(ctx.plcRotationKey.did())
+  ) {
+    throw new InvalidRequestError(
+      'Server rotation key not included in PLC DID data',
+    )
+  }
+
+  if (!pdsEndpoint || pdsEndpoint !== ctx.cfg.service.publicUrl) {
+    throw new InvalidRequestError(
+      'DID document atproto_pds service endpoint does not match PDS public url',
+    )
+  }
+
+  const keypair = await ctx.actorStore.keypair(did)
+  if (!signingKey || signingKey !== keypair.did()) {
+    throw new InvalidRequestError(
+      'DID document verification method does not match expected signing key',
+    )
+  }
 }
