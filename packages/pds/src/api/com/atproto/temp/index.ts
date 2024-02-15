97fe9239b3f9af9d439fab064731b0ecfd5f09f2
dholms <dtholmgren@gmail.com>
2024-02-15 17:17:48 -0600

    clean old temp lexicons

--- packages/pds/src/api/com/atproto/temp/index.ts
+++ packages/pds/src/api/com/atproto/temp/index.ts
@@ -1,13 +1,7 @@
 import AppContext from '../../../../context'
 import { Server } from '../../../../lexicon'
 import checkSignupQueue from './checkSignupQueue'
-import importRepo from './importRepo'
-import pushBlob from './pushBlob'
-import transferAccount from './transferAccount'
 
 export default function (server: Server, ctx: AppContext) {
   checkSignupQueue(server, ctx)
-  importRepo(server, ctx)
-  pushBlob(server, ctx)
-  transferAccount(server, ctx)
 }
