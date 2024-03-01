f293cf14cbcfc28e5e4dfd959b099b0b3325ee0a
Eric Bailey <git@esb.lol>
2024-02-29 20:14:04 -0600

    Export regexes from rich text detection

---
+++ .changeset/chatty-cows-kick.md
@@ -0,0 +1,5 @@
+---
+'@atproto/api': patch
+---
+
+Export regex from rich text detection
---
+++ packages/api/src/rich-text/util.ts
@@ -0,0 +1,6 @@
+export const MENTION_REGEX = /(^|\s|\()(@)([a-zA-Z0-9.-]+)(\b)/g
+export const URL_REGEX =
+  /(^|\s|\()((https?:\/\/[\S]+)|((?<domain>[a-z][a-z0-9]*(\.[a-z0-9]+)+)[\S]*))/gim
+export const TAG_REGEX =
+  /(^|\s)#((?!\ufe0f)[^\s\u00AD\u2060\u200A\u200B\u200C\u200D]*)?/g
+export const TRAILING_PUNCTUATION_REGEX = /\p{P}+$/gu
