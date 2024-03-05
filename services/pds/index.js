89d944fc022d5c8229a7f3cde23c2c1ab507581d
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-03-05 09:39:30 +0100

    feat(pds): split tracer code in own file

--- services/pds/index.js
+++ services/pds/index.js
@@ -2,32 +2,6 @@
 
 'use strict'
 
-const { registerInstrumentations } = require('@opentelemetry/instrumentation')
-
-const {
-  BetterSqlite3Instrumentation,
-} = require('opentelemetry-plugin-better-sqlite3')
-
-const { TracerProvider } = require('dd-trace') // Only works with commonjs
-  .init({ logInjection: true })
-  .use('express', {
-    hooks: {
-      request: (span, req) => {
-        maintainXrpcResource(span, req)
-      },
-    },
-  })
-
-const tracer = new TracerProvider()
-tracer.register()
-
-registerInstrumentations({
-  tracerProvider: tracer,
-  instrumentations: [new BetterSqlite3Instrumentation()],
-})
-
-// Tracer code above must come before anything else
-const path = require('path')
 const {
   PDS,
   envToCfg,
@@ -53,21 +27,6 @@ const main = async () => {
     await pds.destroy()
     httpLogger.info('pds is stopped')
   })
-}
-
-const maintainXrpcResource = (span, req) => {
-  // Show actual xrpc method as resource rather than the route pattern
-  if (span && req.originalUrl?.startsWith('/xrpc/')) {
-    span.setTag(
-      'resource.name',
-      [
-        req.method,
-        path.posix.join(req.baseUrl || '', req.path || '', '/').slice(0, -1), // Ensures no trailing slash
-      ]
-        .filter(Boolean)
-        .join(' '),
-    )
-  }
 }
 
 main()
