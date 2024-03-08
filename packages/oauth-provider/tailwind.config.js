a1342d54790ca9bddf71c3f396a5beb9d5bc01cd
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-03-08 17:34:23 +0100

    feat(oauth-provider): allow customizing branding of the login page

--- packages/oauth-provider/tailwind.config.js
+++ packages/oauth-provider/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['src/assets/app/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+    },
     extend: {},
   },
   plugins: [],
