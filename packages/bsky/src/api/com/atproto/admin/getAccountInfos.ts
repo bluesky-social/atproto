6af2e94d1b94d5557da1ff2532dcf48da8cf13b3
dholms <dtholmgren@gmail.com>
2024-03-12 15:27:51 -0500

    allow 3p labelers to query appview account infos

--- packages/bsky/src/api/com/atproto/admin/getAccountInfos.ts
+++ packages/bsky/src/api/com/atproto/admin/getAccountInfos.ts
@@ -5,18 +5,25 @@ import { INVALID_HANDLE } from '@atproto/syntax'
 
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.admin.getAccountInfos({
-    auth: ctx.authVerifier.roleOrModService,
-    handler: async ({ params }) => {
+    auth: ctx.authVerifier.optionalStandardOrRole,
+    handler: async ({ params, auth }) => {
       const { dids } = params
+      const { canViewTakedowns } = ctx.authVerifier.parseCreds(auth)
+
       const actors = await ctx.hydrator.actor.getActors(dids, true)
 
       const infos = mapDefined(dids, (did) => {
         const info = actors.get(did)
         if (!info) return
+        if (info.takedownRef && !canViewTakedowns) return
+        const profileRecord =
+          !info.profileTakedownRef || canViewTakedowns
+            ? info.profile
+            : undefined
         return {
           did,
           handle: info.handle ?? INVALID_HANDLE,
-          relatedRecords: info.profile ? [info.profile] : undefined,
+          relatedRecords: profileRecord ? [profileRecord] : undefined,
           indexedAt: (info.sortedAt ?? new Date(0)).toISOString(),
         }
       })
