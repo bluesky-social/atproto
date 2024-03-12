b666af52e2c022e5d3ad3b0c8271ccb6a62a206b
Paul Frazee <pfrazee@gmail.com>
2024-03-12 16:04:01 -0700

    Implement moderation for userlists and feedgens

--- packages/api/src/moderation/subjects/feed-generator.ts
+++ packages/api/src/moderation/subjects/feed-generator.ts
@@ -7,8 +7,17 @@ export function decideFeedGenerator(
   subject: ModerationSubjectFeedGenerator,
   opts: ModerationOpts,
 ): ModerationDecision {
-  // TODO handle labels applied on the feed generator itself
+  const acc = new ModerationDecision()
+
+  acc.setDid(subject.creator.did)
+  acc.setIsMe(subject.creator.did === opts.userDid)
+  if (subject.labels?.length) {
+    for (const label of subject.labels) {
+      acc.addLabel('content', label, opts)
+    }
+  }
   return ModerationDecision.merge(
+    acc,
     decideAccount(subject.creator, opts),
     decideProfile(subject.creator, opts),
   )
--- packages/api/src/moderation/subjects/user-list.ts
+++ packages/api/src/moderation/subjects/user-list.ts
@@ -1,3 +1,4 @@
+import { AtUri } from '@atproto/syntax'
 import { AppBskyActorDefs } from '../../client/index'
 import { ModerationDecision } from '../decision'
 import { ModerationSubjectUserList, ModerationOpts } from '../types'
@@ -8,12 +9,36 @@ export function decideUserList(
   subject: ModerationSubjectUserList,
   opts: ModerationOpts,
 ): ModerationDecision {
-  // TODO handle labels applied on the list itself
-  const account = AppBskyActorDefs.isProfileViewBasic(subject.creator)
-    ? decideAccount(subject.creator, opts)
-    : new ModerationDecision()
-  const profile = AppBskyActorDefs.isProfileViewBasic(subject.creator)
-    ? decideProfile(subject.creator, opts)
-    : new ModerationDecision()
-  return ModerationDecision.merge(account, profile)
+  const acc = new ModerationDecision()
+
+  const creator = isProfile(subject.creator) ? subject.creator : undefined
+
+  if (creator) {
+    acc.setDid(creator.did)
+    acc.setIsMe(creator.did === opts.userDid)
+    if (subject.labels?.length) {
+      for (const label of subject.labels) {
+        acc.addLabel('content', label, opts)
+      }
+    }
+    return ModerationDecision.merge(
+      acc,
+      decideAccount(creator, opts),
+      decideProfile(creator, opts),
+    )
+  }
+
+  const creatorDid = new AtUri(subject.uri).hostname
+  acc.setDid(creatorDid)
+  acc.setIsMe(creatorDid === opts.userDid)
+  if (subject.labels?.length) {
+    for (const label of subject.labels) {
+      acc.addLabel('content', label, opts)
+    }
+  }
+  return acc
+}
+
+function isProfile(v: any): v is AppBskyActorDefs.ProfileViewBasic {
+  return v && typeof v === 'object' && 'did' in v
 }
