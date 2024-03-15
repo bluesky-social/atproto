e944b576d622ed709d54905546e32da7a07569a3
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-03-15 11:51:52 +0100

    fix(oauth-provider): do not throw when essential claims are not available (with exceptions)

--- packages/oauth-provider/src/parameters/claims-requested.ts
+++ packages/oauth-provider/src/parameters/claims-requested.ts
@@ -9,22 +9,99 @@ export function claimRequested(
   parameters: AuthorizationParameters,
   entityType: ClaimsEntityType,
   claimName: EntityClaims,
-  available: boolean,
+  value: unknown,
 ): boolean {
+  if (claimAvailable(parameters, entityType, claimName, value)) {
+    return true
+  }
+
+  const entityClaims = parameters.claims?.[entityType]
+  if (entityClaims?.[claimName]?.essential === true) {
+    // https://openid.net/specs/openid-connect-core-1_0.html#rfc.section.5.5.1
+    //
+    // > By requesting Claims as Essential Claims, the RP indicates to the
+    // > End-User that releasing these Claims will ensure a smooth
+    // > authorization for the specific task requested by the End-User. Note
+    // > that even if the Claims are not available because the End-User did
+    // > not authorize their release or they are not present, the
+    // > Authorization Server MUST NOT generate an error when Claims are not
+    // > returned, whether they are Essential or Voluntary, unless otherwise
+    // > specified in the description of the specific claim.
+    switch (claimName) {
+      case 'acr':
+        // https://openid.net/specs/openid-connect-core-1_0.html#rfc.section.5.5.1.1
+        //
+        // > If this is an Essential Claim and the requirement cannot be met,
+        // > then the Authorization Server MUST treat that outcome as a failed
+        // > authentication attempt.
+        throw new InvalidRequestError(
+          `Unable to provide essential claim: ${claimName}`,
+        )
+    }
+  }
+
+  return false
+}
+
+function claimAvailable(
+  parameters: AuthorizationParameters,
+  entityType: ClaimsEntityType,
+  claimName: EntityClaims,
+  value: unknown,
+): boolean {
+  if (value === undefined) return false
+
   if (parameters.claims) {
     const entityClaims = parameters.claims[entityType]
     if (entityClaims === undefined) return false
 
     const claimConfig = entityClaims[claimName]
     if (claimConfig === undefined) return false
-    if (claimConfig === null) return available
+    if (claimConfig === null) return true
 
-    if (!available && claimConfig?.essential === true) {
-      throw new InvalidRequestError(
-        `Unable to provide requested claim: ${claimName}`,
-      )
+    if (
+      claimConfig.value !== undefined &&
+      !compareClaimValue(claimConfig.value, value)
+    ) {
+      return false
+    }
+
+    if (
+      claimConfig?.values !== undefined &&
+      !claimConfig.values.some((v) => compareClaimValue(v, value))
+    ) {
+      return false
     }
   }
 
-  return available
+  return true
+}
+
+type DefinedValue = NonNullable<unknown> | null
+
+function compareClaimValue(
+  expectedValue: DefinedValue,
+  value: DefinedValue,
+): boolean {
+  const expectedType = typeof expectedValue
+  const valueType = typeof value
+
+  if (expectedType !== valueType) return false
+
+  switch (typeof expectedValue) {
+    case 'undefined':
+    case 'string':
+    case 'number':
+    case 'boolean':
+      return expectedValue === value
+    // @ts-expect-error
+    case 'object':
+      if (expectedValue === null) return value === null
+    // TODO (?): allow for object comparison
+    // falls through
+    default:
+      throw new InvalidRequestError(
+        `Unable to compare claim value of type ${expectedType}`,
+      )
+  }
 }
