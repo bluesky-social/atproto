ca8f724056c6e8526fda673e300e5ae8dd2ef04c
Devin Ivy <devinivy@gmail.com>
2024-03-15 10:55:55 -0400

    appview/ozone: fix admin repo search

--- packages/bsky/src/api/app/bsky/actor/searchActors.ts
+++ packages/bsky/src/api/app/bsky/actor/searchActors.ts
@@ -26,9 +26,13 @@ export default function (server: Server, ctx: AppContext) {
   server.app.bsky.actor.searchActors({
     auth: ctx.authVerifier.standardOptional,
     handler: async ({ auth, params, req }) => {
-      const viewer = auth.credentials.iss
+      const { viewer, includeTakedowns } = ctx.authVerifier.parseCreds(auth)
       const labelers = ctx.reqLabelers(req)
-      const hydrateCtx = await ctx.hydrator.createContext({ viewer, labelers })
+      const hydrateCtx = await ctx.hydrator.createContext({
+        viewer,
+        labelers,
+        includeTakedowns,
+      })
       const results = await searchActors({ ...params, hydrateCtx }, ctx)
       return {
         encoding: 'application/json',
