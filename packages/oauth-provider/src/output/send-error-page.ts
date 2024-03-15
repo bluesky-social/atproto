62681dc23162b62d6c44371793ed99861f2d94d8
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-03-15 12:30:37 +0100

    refactor: split html page generation logic

--- packages/oauth-provider/src/output/send-error-page.ts
+++ packages/oauth-provider/src/output/send-error-page.ts
@@ -5,7 +5,7 @@ import { cssCode, html } from '@atproto/html'
 import { getAsset } from '../assets/index.js'
 import { Branding, buildBrandingCss, buildBrandingData } from './branding.js'
 import { buildErrorPayload, buildErrorStatus } from './build-error-payload.js'
-import { declareBrowserGlobalVar, sendWebApp } from './send-web-app.js'
+import { declareBrowserGlobalVar, sendWebPage } from './send-web-page.js'
 
 export async function sendErrorPage(
   req: IncomingMessage,
@@ -13,7 +13,7 @@ export async function sendErrorPage(
   err: unknown,
   branding?: Branding,
 ): Promise<void> {
-  return sendWebApp(req, res, {
+  return sendWebPage(req, res, {
     status: buildErrorStatus(err),
     scripts: [
       declareBrowserGlobalVar('__brandingData', buildBrandingData(branding)),
---
+++ packages/oauth-provider/src/output/send-web-page.ts
@@ -0,0 +1,120 @@
+import { createHash } from 'node:crypto'
+import { IncomingMessage, ServerResponse } from 'node:http'
+
+import { html, Html, jsonCode } from '@atproto/html'
+import { writeHtml } from '@atproto/http-util'
+
+import { Asset } from '../assets/asset.js'
+
+export type WebPageOptions = {
+  lang?: string
+  base?: URL
+  meta?: Record<string, Html | string>
+  head?: Html
+  title: string
+  scripts?: (Html | Asset)[]
+  styles?: (Html | Asset)[]
+  body: Html
+}
+
+export function buildWebPage({
+  lang = 'en',
+  head,
+  title,
+  body,
+  base,
+  meta,
+  scripts,
+  styles,
+}: WebPageOptions) {
+  if (!meta?.viewport) {
+    meta = { ...meta, viewport: 'width=device-width, initial-scale=1.0' }
+  }
+
+  return html`
+    <!DOCTYPE html>
+    <html lang="${lang}">
+      <head>
+        <meta charset="UTF-8" />
+        ${base && html`<base href="${base.href}" />`}
+        ${Object.entries(meta).map(([name, content]) => {
+          return html`<meta name="${name}" content="${content}" />`
+        })}
+        <title>${title}</title>
+        ${head} ${styles?.map(styleToHtml)}
+      </head>
+      <body>
+        ${body} ${scripts?.map(scriptToHtml)}
+      </body>
+    </html>
+  `
+}
+
+export function declareBrowserGlobalVar(name: string, data: unknown) {
+  const nameJson = jsonCode(name)
+  const dataJson = jsonCode(data)
+  return html`window[${nameJson}]=${dataJson};document.currentScript.remove();`
+}
+
+function scriptToHtml(script: Html | Asset) {
+  return script instanceof Html
+    ? // prettier-ignore
+      html`<script>${script}</script>` // hash validity requires no space around the content
+    : html`<script type="module" src="${script.url}?${script.sha256}"></script>`
+}
+
+function styleToHtml(style: Html | Asset) {
+  return style instanceof Html
+    ? // prettier-ignore
+      html`<style>${style}</style>` // hash validity requires no space around the content
+    : html`<link rel="stylesheet" href="${style.url}?${style.sha256}" />`
+}
+
+export function sendWebPage(
+  req: IncomingMessage,
+  res: ServerResponse,
+  { status = 200, ...options }: WebPageOptions & { status?: number },
+): void {
+  res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless')
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-origin')
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+  res.setHeader('Referrer-Policy', 'same-origin')
+  res.setHeader('X-Frame-Options', 'DENY')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-XSS-Protection', '0')
+  res.setHeader('Permissions-Policy', 'otp-credentials=*, document-domain=()')
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000')
+  res.setHeader('Cache-Control', 'no-store')
+  res.setHeader(
+    'Content-Security-Policy',
+    [
+      `default-src 'none'`,
+      `frame-ancestors 'none'`,
+      `form-action 'none'`,
+      `base-uri ${options.base?.origin || `'none'`}`,
+      `script-src 'self' ${
+        options.scripts?.map(assetToHash).map(hashToCspRule).join(' ') ?? ''
+      }`,
+      `style-src 'self' ${
+        options.styles?.map(assetToHash).map(hashToCspRule).join(' ') ?? ''
+      }`,
+      `img-src 'self' data: https:`,
+      `connect-src 'self'`,
+      `upgrade-insecure-requests`,
+    ].join('; '),
+  )
+
+  const webPage = buildWebPage(options)
+
+  writeHtml(res, webPage.toString(), status)
+}
+
+function assetToHash(asset: Html | Asset): string {
+  return asset instanceof Html
+    ? createHash('sha256').update(asset.toString()).digest('base64')
+    : asset.sha256
+}
+
+function hashToCspRule(hash: string): string {
+  return `'sha256-${hash}'`
+}
