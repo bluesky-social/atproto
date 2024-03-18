debaa9ed78aaa39d8d1ace1e04a83bfeec7bf3c9
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-03-18 13:10:35 +0100

    refactor: group code

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -1,8 +1,11 @@
-import { encode } from './encode.js'
+import {
+  HtmlValue,
+  cssEscaper,
+  htmlEscaper,
+  javascriptEscaper,
+  jsonEscaper,
+} from './escapers.js'
 import { Html } from './html.js'
-import { cssEscaper, javascriptEscaper, jsonEscaper } from './util.js'
-
-type NestedArray<V> = V | readonly NestedArray<V>[]
 
 export { Html }
 
@@ -13,7 +16,7 @@ export const javascriptCode = (code: string) =>
   Html.dangerouslyCreate(javascriptEscaper(code))
 
 /**
- * Escapes a value to use as an JSON variable definition inside a `<script>` tag.
+ * Escapes a value to be used as a JSON string inside a `<script>` tag.
  *
  * @see {@link https://redux.js.org/usage/server-rendering#security-considerations}
  */
@@ -21,51 +24,11 @@ export const jsonCode = (value: unknown) =>
   Html.dangerouslyCreate(jsonEscaper(value))
 
 /**
- * Escapes a value to use as an CSS variable definition inside a `<style>` tag.
+ * Escapes a value to be uses as CSS styles inside a `<style>` tag.
  */
 export const cssCode = (code: string) =>
   Html.dangerouslyCreate(cssEscaper(code))
 
-export type HtmlVariable = Html | string | number | null | undefined
-
-export function html(
-  htmlFragment: TemplateStringsArray,
-  ...values: readonly NestedArray<HtmlVariable>[]
-): Html {
-  const fragments: Iterable<Html | string> = combineTemplateStringsFragments(
-    htmlFragment,
-    values,
-  )
-  return Html.dangerouslyCreate(fragments)
-}
-
-function* combineTemplateStringsFragments(
-  htmlFragment: TemplateStringsArray,
-  values: readonly NestedArray<HtmlVariable>[],
-): Generator<string | Html, void, undefined> {
-  for (let i = 0; i < htmlFragment.length; i++) {
-    yield htmlFragment[i]!
-    if (i < values.length) {
-      const value = values[i]
-      yield* valueToFragment(value)
-    }
-  }
-}
-
-function* valueToFragment(
-  value: NestedArray<HtmlVariable>,
-): Generator<string | Html, void, undefined> {
-  if (value == null) {
-    return
-  } else if (typeof value === 'number') {
-    yield String(value)
-  } else if (typeof value === 'string') {
-    yield encode(value)
-  } else if (value instanceof Html) {
-    yield value
-  } else if (Array.isArray(value)) {
-    for (const v of value) {
-      yield* valueToFragment(v)
-    }
-  }
-}
+export { type HtmlValue }
+export const html = (tpl: TemplateStringsArray, ...val: readonly HtmlValue[]) =>
+  Html.dangerouslyCreate(htmlEscaper(tpl, val))
--- packages/html/src/util.ts
+++ packages/html/src/util.ts
@@ -1,8 +1,10 @@
+export type NestedArray<V> = V | readonly NestedArray<V>[]
+
 export function* stringReplacer(
   source: string,
   searchValue: string,
   replaceValue: string,
-) {
+): Generator<string, void, undefined> {
   let previousIndex = 0
   let index = source.indexOf(searchValue)
   while (index !== -1) {
@@ -12,21 +14,6 @@ export function* stringReplacer(
     index = source.indexOf(searchValue, previousIndex)
   }
   yield source.slice(previousIndex)
-}
-
-export function* javascriptEscaper(code: string) {
-  // "</script>" can only appear in javascript strings, so we can safely escape
-  // the "<" without breaking the javascript.
-  yield* stringReplacer(code, '</script>', '\\u003c/script>')
-}
-
-export function* jsonEscaper(value: unknown) {
-  // https://redux.js.org/usage/server-rendering#security-considerations
-  yield* stringReplacer(JSON.stringify(value), '<', '\\u003c')
-}
-
-export function* cssEscaper(css: string) {
-  yield* stringReplacer(css, '</style>', '\\u003c/style>')
 }
 
 export function isString(value: unknown): value is string {
