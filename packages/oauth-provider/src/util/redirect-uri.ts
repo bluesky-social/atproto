31db749ea3cf08566178dbfcf13c59d898fdfe93
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-04-08 21:43:00 +0200

    docs(oauth-provider): improve comments in redirect_uri matching algorithm

--- packages/oauth-provider/src/util/redirect-uri.ts
+++ packages/oauth-provider/src/util/redirect-uri.ts
@@ -6,6 +6,13 @@ export function matchRedirectUri(
   allowed_uri: string,
   request_uri: string,
 ): boolean {
+  // https://datatracker.ietf.org/doc/html/rfc8252#section-8.4
+  //
+  // > Authorization servers MUST require clients to register their complete
+  // > redirect URI (including the path component) and reject authorization
+  // > requests that specify a redirect URI that doesn't exactly match the
+  // > one that was registered; the exception is loopback redirects, where
+  // > an exact match is required except for the port URI component.
   if (allowed_uri === request_uri) return true
 
   const allowed_uri_parsed = new URL(allowed_uri)
@@ -21,13 +28,10 @@ export function matchRedirectUri(
 
   const request_uri_parsed = new URL(request_uri)
 
-  // https://datatracker.ietf.org/doc/html/rfc8252#section-8.4
-  //
-  // > Authorization servers MUST require clients to register their complete
-  // > redirect URI (including the path component) and reject authorization
-  // > requests that specify a redirect URI that doesn't exactly match the
-  // > one that was registered; the exception is loopback redirects, where
-  // > an exact match is required except for the port URI component.
+  // > The authorization server MUST allow any port to be specified at the
+  // > time of the request for loopback IP redirect URIs, to accommodate
+  // > clients that obtain an available ephemeral port from the operating
+  // > system at the time of the request.
   return (
     // allowed_uri_parsed.port === request_uri_parsed.port &&
     allowed_uri_parsed.hostname === request_uri_parsed.hostname &&
