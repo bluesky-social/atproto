fad343df0dc211a4e8aee68f65e83cab77a6a51f
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-04-10 14:27:07 +0200

    fix(dpop-fetch): properly parse "use_dpop_nonce" error in response fix(dpop-fetch): never leave a stream unconsumed

--- packages/fetch-dpop/src/index.ts
+++ packages/fetch-dpop/src/index.ts
@@ -1,7 +1,25 @@
 import { b64uEncode } from '@atproto/b64'
 import { GenericStore } from '@atproto/caching'
-import { Fetch } from '@atproto/fetch'
+import { Fetch, Json } from '@atproto/fetch'
 import { Key } from '@atproto/jwk'
+
+export type DpopFetchWrapperOptions = {
+  key: Key
+  iss: string
+  alg?: string
+  sha256?: (input: string) => Promise<string>
+  nonceCache?: GenericStore<string, string>
+
+  /**
+   * Is the intended server an authorization server (true) or a resource server
+   * (false)? Setting this may allow to avoid parsing the response body to
+   * determine the dpop-nonce.
+   *
+   * @default undefined
+   */
+  isAuthServer?: boolean
+  fetch?: Fetch
+}
 
 export function dpopFetchWrapper({
   key,
@@ -10,15 +28,9 @@ export function dpopFetchWrapper({
   sha256 = typeof crypto !== 'undefined' && crypto.subtle != null
     ? subtleSha256
     : undefined,
+  isAuthServer,
   nonceCache,
-}: {
-  key: Key
-  iss: string
-  alg?: string
-  sha256?: (input: string) => Promise<string>
-  nonceCache?: GenericStore<string, string>
-  fetch?: Fetch
-}): Fetch {
+}: DpopFetchWrapperOptions): Fetch {
   if (!sha256) {
     throw new Error(
       `crypto.subtle is not available in this environment. Please provide a sha256 function.`,
@@ -34,6 +46,7 @@ export function dpopFetchWrapper({
       alg,
       sha256,
       nonceCache,
+      isAuthServer,
       fetch,
     )
   }
@@ -47,6 +60,7 @@ export async function dpopFetch(
   alg: string = key.alg || 'ES256',
   sha256: (input: string) => string | PromiseLike<string> = subtleSha256,
   nonceCache?: GenericStore<string, string>,
+  isAuthServer?: boolean,
   fetch = globalThis.fetch as Fetch,
 ): Promise<Response> {
   const authorizationHeader = request.headers.get('Authorization')
@@ -54,43 +68,63 @@ export async function dpopFetch(
     ? await sha256(authorizationHeader.slice(5))
     : undefined
 
-  const { origin } = new URL(request.url)
+  const { method, url } = request
+  const { origin } = new URL(url)
+  let nonce: string | undefined
 
   // Clone request for potential retry
   const clonedRequest = request.clone()
 
-  // Try with the previously known nonce
-  const oldNonce = await Promise.resolve()
-    .then(() => nonceCache?.get(origin))
-    .catch(() => undefined) // Ignore cache.get errors
-
-  request.headers.set(
-    'DPoP',
-    await buildProof(key, alg, iss, request.method, request.url, oldNonce, ath),
-  )
-
-  const response = await fetch(request)
-
-  const nonce = response.headers.get('DPoP-Nonce')
-  if (!nonce) return response
-
-  // Store the fresh nonce for future requests
+  // Use the cached nonce if available
   try {
-    await nonceCache?.set(origin, nonce)
+    nonce = await nonceCache?.get(origin)
   } catch {
-    // Ignore cache.set errors
+    // Ignore cache.get errors
   }
 
-  if (!(await isUseDpopNonceError(response))) {
-    return response
+  // Make sure the clonedRequest body gets (finally) consumed.
+  try {
+    const dpopProof = await buildProof(key, alg, iss, method, url, nonce, ath)
+    request.headers.set('DPoP', dpopProof)
+
+    const response = await fetch(request)
+
+    // Make sure the response body is consumed. Either by the caller (when the
+    // response is returned), of if an error is thrown (catch block).
+    try {
+      nonce = response.headers.get('DPoP-Nonce') || undefined
+      if (!nonce) {
+        // We don't have a nonce, so we can't retry
+        return response
+      }
+
+      // Store the fresh nonce for future requests
+      try {
+        await nonceCache?.set(origin, nonce)
+      } catch {
+        // Ignore cache.set errors
+      }
+
+      if (!(await isUseDpopNonceError(response, isAuthServer))) {
+        // Not a use_dpop_nonce error, so there is no need to retry
+        return response
+      }
+
+      // If the response was not returned to the caller, make sure the body is
+      // consumed.
+      await response.body?.cancel()
+
+      const dpopProof = await buildProof(key, alg, iss, method, url, nonce, ath)
+      clonedRequest.headers.set('DPoP', dpopProof)
+
+      return await fetch(clonedRequest)
+    } catch (err) {
+      await response.body?.cancel(err)
+      throw err
+    }
+  } finally {
+    await clonedRequest.body?.cancel()
   }
-
-  clonedRequest.headers.set(
-    'DPoP',
-    await buildProof(key, alg, iss, request.method, request.url, nonce, ath),
-  )
-
-  return fetch(clonedRequest)
 }
 
 async function buildProof(
@@ -128,23 +162,39 @@ async function buildProof(
   )
 }
 
-async function isUseDpopNonceError(response: Response): Promise<boolean> {
-  if (response.status !== 400) {
-    return false
+async function isUseDpopNonceError(
+  response: Response,
+  isAuthServer?: boolean,
+): Promise<boolean> {
+  // https://datatracker.ietf.org/doc/html/rfc6750#section-3
+  // https://datatracker.ietf.org/doc/html/rfc9449#name-resource-server-provided-no
+  if (isAuthServer == null || isAuthServer === false) {
+    const wwwAuth = response.headers.get('WWW-Authenticate')
+    if (wwwAuth?.startsWith('DPoP')) {
+      return wwwAuth.includes('error="use_dpop_nonce"')
+    }
   }
 
-  const ct = response.headers.get('Content-Type')
-  const mime = ct?.split(';')[0]?.trim()
-  if (mime !== 'application/json') {
-    return false
+  // https://datatracker.ietf.org/doc/html/rfc9449#name-authorization-server-provid
+  if (isAuthServer == null || isAuthServer === true) {
+    if (response.status === 400) {
+      const mime = response.headers.get('Content-Type')?.split(';')[0]?.trim()
+      if (mime === 'application/json') {
+        try {
+          const body: Json = await response.clone().json()
+          return (
+            typeof body === 'object' &&
+            !Array.isArray(body) &&
+            body?.['error'] === 'use_dpop_nonce'
+          )
+        } catch {
+          return false
+        }
+      }
+    }
   }
 
-  try {
-    const body = await response.clone().json()
-    return body?.error === 'use_dpop_nonce'
-  } catch {
-    return false
-  }
+  return false
 }
 
 function subtleSha256(input: string): Promise<string> {
