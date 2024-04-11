d2543b7b58fd5695d50ad7c88ac331efb7cc62e8
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-04-11 15:50:30 +0200

    docs(oauth-provider): better document AuthorizationRequestHook

--- packages/oauth-provider/src/request/request-hooks.ts
+++ packages/oauth-provider/src/request/request-hooks.ts
@@ -3,11 +3,25 @@ import { Client } from '../client/client.js'
 import { AuthorizationParameters } from '../parameters/authorization-parameters.js'
 import { Awaitable } from '../util/awaitable.js'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { InvalidAuthorizationDetailsError } from '../errors/invalid-authorization-details-error.js'
+import type { AccessDeniedError } from '../errors/access-denied-error.js'
+import type { AccountSelectionRequiredError } from '../errors/account-selection-required-error.js'
+import type { ConsentRequiredError } from '../errors/consent-required-error.js'
+import type { InvalidParametersError } from '../errors/invalid-parameters-error.js'
+import type { LoginRequiredError } from '../errors/login-required-error.js'
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
 /**
  * Allows validating and modifying the authorization parameters before the
  * authorization request is processed.
  *
- * @throws {InvalidAuthorizationDetailsError}
+ * @see {@link InvalidAuthorizationDetailsError}
+ * @see {@link AccessDeniedError}
+ * @see {@link AccountSelectionRequiredError}
+ * @see {@link ConsentRequiredError}
+ * @see {@link InvalidParametersError}
+ * @see {@link LoginRequiredError}
  */
 export type AuthorizationRequestHook = (
   this: null,
