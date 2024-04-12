c7c361bb84d074af6b73ea419217480b31b5c6d9
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-04-12 13:58:50 +0200

    docs(oauth-client-react-native): add implementation details

--- packages/oauth-client-react-native/src/react-native-key.ts
+++ packages/oauth-client-react-native/src/react-native-key.ts
@@ -16,13 +16,9 @@ export class ReactNativeKey extends Key {
     kid: string,
     allowedAlgos: string[] = ['ES256'],
   ): Promise<ReactNativeKey> {
-    if (!allowedAlgos.includes('ES256')) {
-      throw new Error(
-        `None of the allowed algorithms (${allowedAlgos}) are supported (only ES256)`,
-      )
-    }
-
-    const privateJwk: Jwk = await OauthClientReactNative.createES256Jwk()
+    // Note: OauthClientReactNative.createJwk should throw if it supports none
+    // of the allowed algorithms
+    const privateJwk: Jwk = await OauthClientReactNative.createJwk(allowedAlgos)
     return new ReactNativeKey({ ...privateJwk, kid })
   }
 
@@ -34,6 +30,23 @@ export class ReactNativeKey extends Key {
     P extends VerifyPayload = JwtPayload,
     C extends string = string,
   >(token: Jwt, options?: VerifyOptions<C>): Promise<VerifyResult<P, C>> {
-    return OauthClientReactNative.verifyJwt(token, options, this.jwk)
+    const result = await OauthClientReactNative.verifyJwt(
+      token,
+      options,
+      this.jwk,
+    )
+
+    // TODO (?): add any check not performed by the native module
+    //  - result.payload.aud - must match options?.audience
+    //  - result.payload.iss - must match options?.issuer
+    //  - result.payload.sub - must match options?.subject
+    //  - result.header.typ - must match options?.typ
+    //  - result.payload - must contain all of options?.requiredClaims as keys
+    //  - result.payload.iat - must be present
+    //  - result.payload.iat - must not older than (options?.currentDate - options?.maxTokenAge +- options?.clockTolerance)
+    //  - result.payload.nbf - if present (options?.currentDate +- options?.clockTolerance)
+    //  - result.payload.exp - if present (options?.currentDate +- options?.clockTolerance)
+
+    return result as VerifyResult<P, C>
   }
 }
