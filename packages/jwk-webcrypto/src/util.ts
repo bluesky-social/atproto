998ec3e8e9b06f21b6f9cb1278ee4fd88c61fb31
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-04-12 15:22:57 +0200

    fix(jwk-webcrypto): prefer jwk algorithms that yield smaller signatures

--- packages/jwk-webcrypto/src/util.ts
+++ packages/jwk-webcrypto/src/util.ts
@@ -126,7 +126,8 @@ export async function generateKeypair(
   extractable = false,
 ): Promise<CryptoKeyPair> {
   const errors: unknown[] = []
-  for (const alg of algs) {
+  const algsSorted = Array.from(algs).sort(compareAlgos)
+  for (const alg of algsSorted) {
     try {
       return await crypto.subtle.generateKey(
         toSubtleAlgorithm(alg),
@@ -139,4 +140,30 @@ export async function generateKeypair(
   }
 
   throw new AggregateError(errors, 'Failed to generate keypair')
+}
+
+/**
+ * 256K > ES (256 > 384 > 512) > PS (256 > 384 > 512) > RS (256 > 384 > 512) > other (in original order)
+ */
+function compareAlgos(a: string, b: string): number {
+  if (a === 'ES256K') return -1
+  if (b === 'ES256K') return 1
+
+  for (const prefix of ['ES', 'PS', 'RS']) {
+    if (a.startsWith(prefix)) {
+      if (b.startsWith(prefix)) {
+        const aLen = parseInt(a.slice(2, 5))
+        const bLen = parseInt(b.slice(2, 5))
+
+        // Prefer shorter key lengths
+        return aLen - bLen
+      }
+      return -1
+    } else if (b.startsWith(prefix)) {
+      return 1
+    }
+  }
+
+  // Don't know how to compare, keep original order
+  return 0
 }
