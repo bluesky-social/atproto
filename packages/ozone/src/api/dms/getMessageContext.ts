1c01b988cfdcbc6c9ca66b4abc136a43b77b76f4
dholms <dtholmgren@gmail.com>
2024-05-02 19:48:15 -0500

    scaffold routes

---
+++ packages/ozone/src/api/dms/getMessageContext.ts
@@ -0,0 +1,11 @@
+import { Server } from '../../lexicon'
+import AppContext from '../../context'
+
+export default function (server: Server, ctx: AppContext) {
+  server.chat.bsky.moderation.getMessageContext({
+    auth: ctx.authVerifier.modOrAdminToken,
+    handler: async ({ params }) => {
+      return {} as any
+    },
+  })
+}
