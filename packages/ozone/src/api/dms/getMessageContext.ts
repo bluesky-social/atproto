2bc95e23f2a17be93fd3c6727fdee21badfb3b1f
dholms <dtholmgren@gmail.com>
2024-05-06 14:49:46 -0500

    hook up chat moderation routes to ozone

--- packages/ozone/src/api/dms/getMessageContext.ts
+++ packages/ozone/src/api/dms/getMessageContext.ts
@@ -1,11 +1,35 @@
 import { Server } from '../../lexicon'
 import AppContext from '../../context'
+import { AuthRequiredError, InvalidRequestError } from '@atproto/xrpc-server'
 
 export default function (server: Server, ctx: AppContext) {
   server.chat.bsky.moderation.getMessageContext({
     auth: ctx.authVerifier.modOrAdminToken,
-    handler: async ({ params }) => {
-      return {} as any
+    handler: async ({ params, auth }) => {
+      if (!ctx.chatAgent) {
+        throw new InvalidRequestError('No chat agent configured')
+      }
+      if (!auth.credentials.isModerator) {
+        if (params.after > 0 || params.before > 0) {
+          throw new AuthRequiredError(
+            'Must be a full moderator to view context window',
+          )
+        }
+      }
+      if (params.after > 5 || params.before > 5) {
+        throw new InvalidRequestError(
+          'Cannot view a context window larger than 5',
+        )
+      }
+      const res =
+        await ctx.chatAgent.api.chat.bsky.moderation.getMessageContext(
+          params,
+          await ctx.chatAuth(),
+        )
+      return {
+        encoding: 'application/json',
+        body: res.data,
+      }
     },
   })
 }
