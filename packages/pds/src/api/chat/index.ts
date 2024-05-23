9fcb78d79c2a1745ca82ed26263b09d992d1230d
Daniel Holmgren <dtholmgren@gmail.com>
2024-05-22 21:37:37 -0500

    Do not allow app passwords to communicate with chat service (#2509)
    do not allow app passwords to communicate with chat service

---
+++ packages/pds/src/api/chat/index.ts
@@ -0,0 +1,90 @@
+import AppContext from '../../context'
+import { Server } from '../../lexicon'
+import { pipethrough } from '../../pipethrough'
+
+export default function (server: Server, ctx: AppContext) {
+  server.chat.bsky.actor.deleteAccount({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: async ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.actor.exportAccountData({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.deleteMessageForSelf({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.getConvo({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.getConvoForMembers({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.getLog({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.getMessages({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.leaveConvo({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.listConvos({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.muteConvo({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.sendMessage({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.sendMessageBatch({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.unmuteConvo({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+  server.chat.bsky.convo.updateRead({
+    auth: ctx.authVerifier.accessNotAppPassword,
+    handler: ({ req, auth }) => {
+      return pipethrough(ctx, req, auth.credentials.did)
+    },
+  })
+}
