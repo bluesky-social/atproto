6fdb8c30b90adfeca507cb456dd0a885bd75792e
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-06-03 11:52:17 +0200

    fix(oauth-provider): simplify isSubPath util

--- packages/oauth/oauth-provider/src/lib/util/path.ts
+++ packages/oauth/oauth-provider/src/lib/util/path.ts
@@ -1,17 +1,15 @@
-import { isAbsolute, relative } from 'node:path'
-
 export function isSubUrl(reference: URL, url: URL): boolean {
   if (url.origin !== reference.origin) return false
   if (url.username !== reference.username) return false
   if (url.password !== reference.password) return false
 
-  return (
-    reference.pathname === url.pathname ||
-    isSubPath(reference.pathname, url.pathname)
-  )
+  return isSubPath(reference.pathname, url.pathname)
 }
 
 function isSubPath(reference: string, path: string): boolean {
-  const rel = relative(reference, path)
-  return !rel.startsWith('..') && !isAbsolute(rel)
+  if (reference === path) return true
+  if (!path.startsWith(reference)) return false
+  if (!reference.endsWith('/') && path[reference.length] !== '/') return false
+
+  return true
 }
