8cc445f42dbfa211dda714194849d2463b77ac96
dholms <dtholmgren@gmail.com>
2024-06-04 11:44:35 -0500

    rerun CI

--- packages/pds/src/api/com/atproto/admin/updateSubjectStatus.ts
+++ packages/pds/src/api/com/atproto/admin/updateSubjectStatus.ts
@@ -9,6 +9,7 @@ import {
 import { isMain as isStrongRef } from '../../../../lexicon/types/com/atproto/repo/strongRef'
 import { InvalidRequestError } from '@atproto/xrpc-server'
 
+//
 export default function (server: Server, ctx: AppContext) {
   server.com.atproto.admin.updateSubjectStatus({
     auth: ctx.authVerifier.moderator,
