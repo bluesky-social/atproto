e7a68c1d505ed5e53302f6544fb7aff9bd2fb1b4
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-06-07 11:13:22 +0200

    :bug: Fix migration

--- packages/ozone/src/db/migrations/20240607T085607200Z-configuration.ts
+++ packages/ozone/src/db/migrations/20240607T085607200Z-configuration.ts
@@ -3,13 +3,12 @@ import { Kysely } from 'kysely'
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
     .createTable('configuration')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
     .addColumn('key', 'varchar', (col) => col.notNull())
     .addColumn('value', 'varchar', (col) => col.notNull())
     .addColumn('createdAt', 'timestamptz', (col) => col.notNull())
     .addColumn('updatedAt', 'timestamptz', (col) => col.notNull())
     .addColumn('createdBy', 'varchar', (col) => col.notNull())
-    .addUniqueConstraint('configuration_unique_key_value', ['key', 'value'])
+    .addPrimaryKeyConstraint('configuration_pkey', ['key', 'value'])
     .execute()
 }
 
