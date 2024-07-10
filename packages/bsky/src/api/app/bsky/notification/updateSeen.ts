06b3183f32b0384480c0b79dd22e51d3838dbf4e
Hailey <me@haileyok.com>
2024-07-10 09:43:07 -0700

    use murmur id

--- packages/bsky/src/api/app/bsky/notification/updateSeen.ts
+++ packages/bsky/src/api/app/bsky/notification/updateSeen.ts
@@ -1,6 +1,7 @@
 import { Struct, Timestamp } from '@bufbuild/protobuf'
 import { Server } from '../../../../lexicon'
 import AppContext from '../../../../context'
+import murmur from 'murmurhash'
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.notification.updateSeen({
@@ -8,14 +9,15 @@ export default function (server: Server, ctx: AppContext) {
     handler: async ({ input, auth }) => {
       const { seenAt } = input.body
       const viewer = auth.credentials.iss
+      const date = new Date(seenAt)
       await ctx.dataplane.updateNotificationSeen({
         actorDid: viewer,
-        timestamp: Timestamp.fromDate(new Date(seenAt)),
+        timestamp: Timestamp.fromDate(date),
       })
       await ctx.courierClient.pushNotifications({
         notifications: [
           {
-            id: `${viewer}-${seenAt}`,
+            id: getNotifId(viewer, date),
             clientControlled: true,
             recipientDid: viewer,
             alwaysDeliver: false,
@@ -29,4 +31,11 @@ export default function (server: Server, ctx: AppContext) {
       })
     },
   })
+}
+
+function getNotifId(viewer: string, seenAt: Date) {
+  const key = ['mark-read-generic', viewer, seenAt.getTime().toString()].join(
+    '::',
+  )
+  return murmur.v3(key).toString(16)
 }
