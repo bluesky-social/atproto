66d00b0ded0f851b6c4c6a653e6eab4c5b7195d4
nnabeyang <nabeyang@gmail.com>
2024-07-24 07:28:37 +0900

    Changed length check for hashtags to use `Intl.Segmenter` to accurately account for Unicode Grapheme Clusters.

--- packages/api/src/rich-text/detection.ts
+++ packages/api/src/rich-text/detection.ts
@@ -85,7 +85,7 @@ export function detectFacets(text: UnicodeString): Facet[] | undefined {
       // strip ending punctuation and any spaces
       tag = tag.trim().replace(TRAILING_PUNCTUATION_REGEX, '')
 
-      if (tag.length === 0 || tag.length > 64) continue
+      if (tag.length === 0 || [...new Intl.Segmenter().segment(tag)].length > 64) continue
 
       const index = match.index + leading.length
 
