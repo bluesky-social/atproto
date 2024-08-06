27c4b6599af712d4f69686d65a86d3815713f62a
Eric Bailey <git@esb.lol>
2024-08-06 15:51:40 -0500

    Codegen

--- packages/pds/src/lexicon/types/app/bsky/feed/postgate.ts
+++ packages/pds/src/lexicon/types/app/bsky/feed/postgate.ts
@@ -12,6 +12,13 @@ export interface Record {
   post: string
   /** List of detached quote post URIs. */
   detachedQuotes?: string[]
+  quotepostRules?: (
+    | DisableRule
+    | MentionRule
+    | FollowingRule
+    | ListRule
+    | { $type: string; [k: string]: unknown }
+  )[]
   [k: string]: unknown
 }
 
@@ -26,4 +33,73 @@ export function isRecord(v: unknown): v is Record {
 
 export function validateRecord(v: unknown): ValidationResult {
   return lexicons.validate('app.bsky.feed.postgate#main', v)
+}
+
+/** Disable quoteposts entirely. */
+export interface DisableRule {
+  [k: string]: unknown
+}
+
+export function isDisableRule(v: unknown): v is DisableRule {
+  return (
+    isObj(v) &&
+    hasProp(v, '$type') &&
+    v.$type === 'app.bsky.feed.postgate#disableRule'
+  )
+}
+
+export function validateDisableRule(v: unknown): ValidationResult {
+  return lexicons.validate('app.bsky.feed.postgate#disableRule', v)
+}
+
+/** Allow quoteposts from actors mentioned in your post. */
+export interface MentionRule {
+  [k: string]: unknown
+}
+
+export function isMentionRule(v: unknown): v is MentionRule {
+  return (
+    isObj(v) &&
+    hasProp(v, '$type') &&
+    v.$type === 'app.bsky.feed.postgate#mentionRule'
+  )
+}
+
+export function validateMentionRule(v: unknown): ValidationResult {
+  return lexicons.validate('app.bsky.feed.postgate#mentionRule', v)
+}
+
+/** Allow quoteposts from actors you follow. */
+export interface FollowingRule {
+  [k: string]: unknown
+}
+
+export function isFollowingRule(v: unknown): v is FollowingRule {
+  return (
+    isObj(v) &&
+    hasProp(v, '$type') &&
+    v.$type === 'app.bsky.feed.postgate#followingRule'
+  )
+}
+
+export function validateFollowingRule(v: unknown): ValidationResult {
+  return lexicons.validate('app.bsky.feed.postgate#followingRule', v)
+}
+
+/** Allow quoteposts from actors on a list. */
+export interface ListRule {
+  list: string
+  [k: string]: unknown
+}
+
+export function isListRule(v: unknown): v is ListRule {
+  return (
+    isObj(v) &&
+    hasProp(v, '$type') &&
+    v.$type === 'app.bsky.feed.postgate#listRule'
+  )
+}
+
+export function validateListRule(v: unknown): ValidationResult {
+  return lexicons.validate('app.bsky.feed.postgate#listRule', v)
 }
