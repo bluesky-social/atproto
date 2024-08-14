5043b327f009c3eac77da6908f2f05ac43bb62ba
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-08-14 11:58:38 +0200

    provide proper production handle resolver in example

--- packages/oauth/oauth-client-browser/example/src/main.tsx
+++ packages/oauth/oauth-client-browser/example/src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       // handleResolver="http://localhost:2584"
       // production
       plcDirectoryUrl={undefined}
-      handleResolver="https://bsky.app"
+      handleResolver="https://bsky.social"
     >
       <App />
     </AuthProvider>
