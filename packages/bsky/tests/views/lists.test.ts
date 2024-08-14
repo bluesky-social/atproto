81d807208fe0a4b6c7053b49f3529e541e2ab483
Hailey <me@haileyok.com>
2024-08-13 19:24:21 -0700

    update tests

--- packages/bsky/tests/views/lists.test.ts
+++ packages/bsky/tests/views/lists.test.ts
@@ -1,22 +1,30 @@
 import { AtpAgent } from '@atproto/api'
-import { TestNetwork, SeedClient, basicSeed } from '@atproto/dev-env'
+import { TestNetwork, SeedClient, listsSeed } from '@atproto/dev-env'
 import { forSnapshot } from '../_util'
 
 describe('bsky actor likes feed views', () => {
   let network: TestNetwork
   let agent: AtpAgent
-  let pdsAgent: AtpAgent
   let sc: SeedClient
+
+  let referenceList: string
+  let eve: string
+  let frankie: string
+  let greta: string
 
   beforeAll(async () => {
     network = await TestNetwork.create({
       dbPostgresSchema: 'bsky_views_actor_lists',
     })
     agent = network.bsky.getClient()
-    pdsAgent = network.pds.getClient()
     sc = network.getSeedClient()
-    await basicSeed(sc)
+    await listsSeed(sc)
     await network.processAll()
+    console.log(sc.lists[sc.dids.eve])
+    referenceList = Object.values(sc.lists[sc.dids.eve])[0].ref.uriStr
+    eve = sc.dids.eve
+    frankie = sc.dids.frankie
+    greta = sc.dids.greta
   })
 
   afterAll(async () => {
@@ -24,39 +32,53 @@ describe('bsky actor likes feed views', () => {
   })
 
   it('does not include reference lists in getActorLists', async () => {
-    await pdsAgent.api.app.bsky.graph.list.create(
-      {
-        repo: sc.dids.alice,
-      },
-      {
-        name: 'awesome starter pack list!',
-        description: '',
-        descriptionFacets: [],
-        avatar: undefined,
-        createdAt: new Date().toISOString(),
-        purpose: 'app.bsky.graph.defs#referencelist',
-      },
-      sc.getHeaders(sc.dids.alice),
-    )
-    await pdsAgent.api.app.bsky.graph.list.create(
-      {
-        repo: sc.dids.alice,
-      },
-      {
-        name: 'cool curate list!',
-        description: '',
-        descriptionFacets: [],
-        avatar: undefined,
-        createdAt: new Date().toISOString(),
-        purpose: 'app.bsky.graph.defs#curatelist',
-      },
-      sc.getHeaders(sc.dids.alice),
-    )
+    await sc.createList(eve, 'cool curate list', 'curate')
     await network.processAll()
     const view = await agent.api.app.bsky.graph.getLists({
-      actor: sc.dids.alice,
+      actor: eve,
     })
     expect(view.data.lists.length).toBe(1)
     expect(forSnapshot(view.data.lists)).toMatchSnapshot()
+  })
+
+  it('does not include users with owner block relationship in reference lists for non-owner, in-list viewers', async () => {
+    const view = await agent.api.app.bsky.graph.getList(
+      {
+        list: referenceList,
+      },
+      { headers: await network.serviceHeaders(frankie) },
+    )
+    expect(view.data.items.length).toBe(2)
+    expect(forSnapshot(view.data.items)).toMatchSnapshot()
+  })
+
+  it('does not include users with owner block relationship in reference lists for non-owner, not-in-list viewers', async () => {
+    const view = await agent.api.app.bsky.graph.getList(
+      {
+        list: referenceList,
+      },
+      { headers: await network.serviceHeaders(greta) },
+    )
+    expect(view.data.items.length).toBe(2)
+    expect(forSnapshot(view.data.items)).toMatchSnapshot()
+  })
+
+  it('does not include users with owner block relationship in reference lists for signed-out viewers', async () => {
+    const view = await agent.api.app.bsky.graph.getList({
+      list: referenceList,
+    })
+    expect(view.data.items.length).toBe(2)
+    expect(forSnapshot(view.data.items)).toMatchSnapshot()
+  })
+
+  it('does include users with owner block relationship in reference lists for owner', async () => {
+    const view = await agent.api.app.bsky.graph.getList(
+      {
+        list: referenceList,
+      },
+      { headers: await network.serviceHeaders(eve) },
+    )
+    expect(view.data.items.length).toBe(3)
+    expect(forSnapshot(view.data.items)).toMatchSnapshot()
   })
 })
--- packages/dev-env/src/seed/lists.ts
+++ packages/dev-env/src/seed/lists.ts
@@ -7,16 +7,28 @@ export default async (sc: SeedClient) => {
   await sc.createAccount('eve', {
     handle: 'eve.test',
     email: 'eve@eve.com',
-    password: '2hunter',
+    password: 'hunter2',
+  })
+  await sc.createAccount('frankie', {
+    handle: 'frankie.test',
+    email: 'frankie@frankie.com',
+    password: '2hunter2real',
+  })
+  await sc.createAccount('greta', {
+    handle: 'greta.test',
+    email: 'greta@greta.com',
+    password: 'hunter4real',
   })
 
   const newList = await sc.createList(
-    sc.dids.alice,
+    sc.dids.eve,
     'blah starter pack list!',
     'reference',
   )
-  await sc.addToList(sc.dids.alice, sc.dids.bob, newList)
-  await sc.addToList(sc.dids.alice, sc.dids.carla, newList)
+  await sc.addToList(sc.dids.eve, sc.dids.eve, newList)
+  await sc.addToList(sc.dids.eve, sc.dids.bob, newList)
+  await sc.addToList(sc.dids.eve, sc.dids.frankie, newList)
+  await sc.block(sc.dids.frankie, sc.dids.eve)
 
   return sc
 }
