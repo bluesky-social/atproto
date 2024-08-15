829bb2983835f6b3b4593ffb2e12378c01206179
Eric Bailey <git@esb.lol>
2024-08-15 09:46:31 -0500

    Throw in post uri -> gate utils

--- packages/bsky/src/util/uris.ts
+++ packages/bsky/src/util/uris.ts
@@ -1,18 +1,17 @@
+import assert from 'node:assert'
 import { AtUri } from '@atproto/syntax'
 import { ids } from '../lexicon/lexicons'
 
 export function postUriToThreadgateUri(postUri: string) {
   const urip = new AtUri(postUri)
-  if (urip.collection === ids.AppBskyFeedPost) {
-    urip.collection = ids.AppBskyFeedThreadgate
-  }
+  assert(urip.collection === ids.AppBskyFeedPost)
+  urip.collection = ids.AppBskyFeedThreadgate
   return urip.toString()
 }
 
 export function postUriToPostgateUri(postUri: string) {
   const urip = new AtUri(postUri)
-  if (urip.collection === ids.AppBskyFeedPost) {
-    urip.collection = ids.AppBskyFeedPostgate
-  }
+  assert(urip.collection === ids.AppBskyFeedPost)
+  urip.collection = ids.AppBskyFeedPostgate
   return urip.toString()
 }
