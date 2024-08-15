390316a98c1773bab4ab08b104f374a66f453a89
devin ivy <devinivy@gmail.com>
2024-08-15 18:25:24 -0400

    tidy

--- packages/ozone/src/db/migrations/20240814T003647759Z-event-created-at-index.ts
+++ packages/ozone/src/db/migrations/20240814T003647759Z-event-created-at-index.ts
@@ -2,12 +2,12 @@ import { Kysely } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
-    .createIndex('moderation_event_created_at_index')
+    .createIndex('moderation_event_created_at_idx')
     .on('moderation_event')
     .column('createdAt')
     .execute()
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  await db.schema.dropIndex('moderation_event_created_at_index').execute()
+  await db.schema.dropIndex('moderation_event_created_at_idx').execute()
 }
