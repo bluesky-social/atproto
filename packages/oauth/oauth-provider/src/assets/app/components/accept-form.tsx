e8bf9f051cbd2d3ae021f96ddef2767f39351fa1
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-08-16 16:24:45 +0200

    feat(oauth-provider): rework display of client name

--- packages/oauth/oauth-provider/src/assets/app/components/accept-form.tsx
+++ packages/oauth/oauth-provider/src/assets/app/components/accept-form.tsx
@@ -5,10 +5,8 @@ import { Account } from '../backend-data'
 import { Override } from '../lib/util'
 import { AccountIdentifier } from './account-identifier'
 import { Button } from './button'
-import { ClientIdentifier } from './client-identifier'
 import { ClientName } from './client-name'
 import { FormCard, FormCardProps } from './form-card'
-import { Fieldset } from './fieldset'
 
 export type AcceptFormProps = Override<
   FormCardProps,
@@ -62,54 +60,46 @@ export function AcceptForm({
       }
       {...props}
     >
-      <Fieldset
-        title={
-          <ClientName clientId={clientId} clientMetadata={clientMetadata} />
-        }
-      >
-        {clientTrusted && clientMetadata.logo_uri && (
-          <div key="logo" className="flex items-center justify-center">
-            <img
-              crossOrigin="anonymous"
-              src={clientMetadata.logo_uri}
-              alt={clientMetadata.client_name}
-              className="w-16 h-16 rounded-full"
-            />
-          </div>
-        )}
+      {clientTrusted && clientMetadata.logo_uri && (
+        <div key="logo" className="flex items-center justify-center">
+          <img
+            crossOrigin="anonymous"
+            src={clientMetadata.logo_uri}
+            alt={clientMetadata.client_name}
+            className="w-16 h-16 rounded-full"
+          />
+        </div>
+      )}
 
-        <p>
-          <ClientIdentifier
-            clientId={clientId}
-            clientMetadata={clientMetadata}
-          />{' '}
-          is asking for permission to access your{' '}
-          <AccountIdentifier account={account} /> account.
-        </p>
+      <p>
+        <ClientName clientId={clientId} clientMetadata={clientMetadata} /> is
+        asking for permission to access your account (
+        <AccountIdentifier account={account} />
+        ).
+      </p>
 
-        <p>
-          By clicking <b>{acceptLabel}</b>, you allow this application to access
-          your information in accordance to their{' '}
-          <a
-            href={clientMetadata.tos_uri}
-            rel="nofollow noopener"
-            target="_blank"
-            className="text-brand underline"
-          >
-            terms of service
-          </a>
-          {' and '}
-          <a
-            href={clientMetadata.policy_uri}
-            rel="nofollow noopener"
-            target="_blank"
-            className="text-brand underline"
-          >
-            privacy policy
-          </a>
-          .
-        </p>
-      </Fieldset>
+      <p>
+        By clicking <b>{acceptLabel}</b>, you allow this application to access
+        your information in accordance to their{' '}
+        <a
+          href={clientMetadata.tos_uri}
+          rel="nofollow noopener"
+          target="_blank"
+          className="text-brand underline"
+        >
+          terms of service
+        </a>
+        {' and '}
+        <a
+          href={clientMetadata.policy_uri}
+          rel="nofollow noopener"
+          target="_blank"
+          className="text-brand underline"
+        >
+          privacy policy
+        </a>
+        .
+      </p>
     </FormCard>
   )
 }
