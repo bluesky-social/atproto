d990c44eb9c921f68eb3bc2f7de2e295da4d6618
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-09-13 16:06:36 +0200

    test refactor

--- packages/xrpc-server/tests/parsing.test.ts
+++ packages/xrpc-server/tests/parsing.test.ts
@@ -1,134 +1,89 @@
 import { parseUrlNsid } from '../src/util'
 
+const testValid = (url: string, expected: string) => {
+  expect(parseUrlNsid(url)).toBe(expected)
+}
+
+const testInvalid = (url: string, errorMessage = 'invalid xrpc path') => {
+  expect(() => parseUrlNsid(url)).toThrow(errorMessage)
+}
+
 describe('parseUrlNsid', () => {
-  it('should return the NSID from the URL', () => {
-    expect(parseUrlNsid('/xrpc/123')).toBe('123')
-    expect(parseUrlNsid('/xrpc/com.example.nsid')).toBe('com.example.nsid')
-    expect(parseUrlNsid('/xrpc/com.example-domain.nsid')).toBe(
-      'com.example-domain.nsid',
-    )
+  it('should extract the NSID from the URL', () => {
+    testValid('/xrpc/blee.blah.bloo', 'blee.blah.bloo')
+    testValid('/xrpc/blee.blah.bloo?foo[]', 'blee.blah.bloo')
+    testValid('/xrpc/blee.blah.bloo?foo=bar', 'blee.blah.bloo')
+    testValid('/xrpc/com.example.nsid', 'com.example.nsid')
+    testValid('/xrpc/com.example.nsid?foo=bar', 'com.example.nsid')
+    testValid('/xrpc/com.example-domain.nsid', 'com.example-domain.nsid')
   })
 
-  it('should return the NSID from the URL with query parameters', () => {
-    expect(parseUrlNsid('/xrpc/123?foo=bar')).toBe('123')
-    expect(parseUrlNsid('/xrpc/com.example.nsid?foo=bar')).toBe(
-      'com.example.nsid',
-    )
-  })
-
-  it('should return the NSID from the URL with a trailing slash', () => {
-    expect(parseUrlNsid('/xrpc/123/')).toBe('123')
-    expect(parseUrlNsid('/xrpc/com.example.nsid/')).toBe('com.example.nsid')
+  it('should allow a trailing slash', () => {
+    testValid('/xrpc/blee.blah.bloo/?', 'blee.blah.bloo')
+    testValid('/xrpc/blee.blah.bloo/?foo=', 'blee.blah.bloo')
+    testValid('/xrpc/blee.blah.bloo/?bool', 'blee.blah.bloo')
+    testValid('/xrpc/com.example.nsid/', 'com.example.nsid')
   })
 
   it('should throw an error if the URL is too short', () => {
-    expect(() => parseUrlNsid('/xrpc/a')).toThrow('invalid xrpc path')
+    testInvalid('/xrpc/a')
   })
 
   it('should throw an error if the URL is empty', () => {
-    expect(() => parseUrlNsid('')).toThrow('invalid xrpc path')
-  })
-
-  it('should throw an error if the URL is missing the XRPC path prefix', () => {
-    expect(() => parseUrlNsid('/foo/123')).toThrow('invalid xrpc path')
-    expect(() => parseUrlNsid('/foo/com.example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID starts with a dot', () => {
-    expect(() => parseUrlNsid('/xrpc/.com.example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID contains double dots', () => {
-    expect(() => parseUrlNsid('/xrpc/com..example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID ends with a dot', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid.')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid./')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid.?foo=bar')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid./?foo=bar')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID contains a dot followed by a dash', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example.-nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID contains a dash followed by a dot', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example-.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID ends with a dash', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid-')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID starts with a dash', () => {
-    expect(() => parseUrlNsid('/xrpc/-com.example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-  })
-
-  it('should throw an error if the NSID contains a double dash', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example--domain.nsid')).toThrow(
-      'invalid xrpc path',
-    )
+    testInvalid('')
   })
 
   it('should throw an error if the URL is missing the NSID', () => {
-    expect(() => parseUrlNsid('/xrpc/')).toThrow('invalid xrpc path')
-  })
-
-  it('should throw an error if the URL is missing the NSID with query parameters', () => {
-    expect(() => parseUrlNsid('/xrpc/?foo=bar')).toThrow('invalid xrpc path')
+    testInvalid('/xrpc/')
+    testInvalid('/xrpc/?')
+    testInvalid('/xrpc/?foo=bar')
   })
 
   it('should throw an error if the URL contains extra path segments', () => {
-    expect(() => parseUrlNsid('/xrpc/123/extra')).toThrow('invalid xrpc path')
+    testInvalid('/xrpc/123/extra')
+    testInvalid('/xrpc/123/extra?foo=bar')
   })
 
-  it('should throw an error if the URL contains extra path segments with query parameters', () => {
-    expect(() => parseUrlNsid('/xrpc/123/extra?foo=bar')).toThrow(
-      'invalid xrpc path',
-    )
+  it('should throw an error if the URL is missing the XRPC path prefix', () => {
+    testInvalid('/foo/123')
+    testInvalid('/foo/com.example.nsid')
+  })
+
+  it('should throw an error if the NSID starts with a dot', () => {
+    testInvalid('/xrpc/.')
+    testInvalid('/xrpc/..')
+    testInvalid('/xrpc/....')
+    testInvalid('/xrpc/.com.example.nsid')
+    testInvalid('/xrpc/com..example.nsid')
+    testInvalid('/xrpc/com.example..nsid')
+    testInvalid('/xrpc/com.example.nsid.')
+    testInvalid('/xrpc/com.example.nsid./')
+    testInvalid('/xrpc/com.example.nsid.?foo=bar')
+    testInvalid('/xrpc/com.example.nsid./?foo=bar')
+  })
+
+  it('should throw an error if the NSID contains a misplaced dash', () => {
+    testInvalid('/xrpc/-')
+    testInvalid('/xrpc/com.example.-nsid')
+    testInvalid('/xrpc/com.example-.nsid')
+    testInvalid('/xrpc/com.-example.nsid')
+    testInvalid('/xrpc/com.-example-.nsid')
+    testInvalid('/xrpc/com.example.nsid-')
+    testInvalid('/xrpc/-com.example.nsid')
+    testInvalid('/xrpc/com.example--domain.nsid')
+  })
+
+  it('should throw an error if the URL starts with a space', () => {
+    testInvalid(' /xrpc/com.example.nsid')
   })
 
   it('should throw an error if the NSID contains invalid characters', () => {
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid!')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com.example#?nsid')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/!com.example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com.example.nsid ')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/ com.example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
-    expect(() => parseUrlNsid('/xrpc/com. example.nsid')).toThrow(
-      'invalid xrpc path',
-    )
+    testInvalid('/xrpc/com.example.nsid#')
+    testInvalid('/xrpc/com.example.nsid!')
+    testInvalid('/xrpc/com.example#?nsid')
+    testInvalid('/xrpc/!com.example.nsid')
+    testInvalid('/xrpc/com.example.nsid ')
+    testInvalid('/xrpc/ com.example.nsid')
+    testInvalid('/xrpc/com. example.nsid')
   })
 })
