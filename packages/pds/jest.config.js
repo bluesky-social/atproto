db6700786be7144f6d33ff6d53af6c1c147b46e5
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-10-11 19:45:11 +0200

    fix tests/proxied/admin.test.ts

--- packages/pds/jest.config.js
+++ packages/pds/jest.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   displayName: 'PDS',
   transform: { '^.+\\.(t|j)s$': '@swc/jest' },
-  transformIgnorePatterns: ['/node_modules/.pnpm/(?!(get-port)@)'],
+  // For some obscure reason, "tests/proxied/admin.test.ts" requires that all of
+  // node_modules gets transformed by SWC, not just "get-port". However,
+  // "@puppeteer" does not get properly transformed, resulting in a
+  // "ReferenceError: _opts is not defined" in
+  // (../../node_modules/.pnpm/@puppeteer+browsers@2.4.0/node_modules/@puppeteer/browsers/lib/cjs/launch.js:123:9)
+
+  // transformIgnorePatterns: ['/node_modules/.pnpm/(?!(get-port)@)'],
+  transformIgnorePatterns: [`/node_modules/@puppeteer`],
   testTimeout: 60000,
   setupFiles: ['<rootDir>/../../jest.setup.ts'],
 }
