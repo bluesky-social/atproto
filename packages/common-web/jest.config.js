f8bc3bbd7aa7f6ea94917429c73333bb12a0d7ba
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-10-25 13:45:19 +0200

    Add .js file extension to import statements in generated code

--- packages/common-web/jest.config.js
+++ packages/common-web/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   displayName: 'Common Web',
   transform: { '^.+\\.(t|j)s$': '@swc/jest' },
   setupFiles: ['<rootDir>/../../jest.setup.ts'],
+  moduleNameMapper: { '^(\\.\\.?\\/.+)\\.js$': ['$1.ts', '$1.js'] },
 }
--- packages/repo/jest.config.js
+++ packages/repo/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   displayName: 'Repo',
   transform: { '^.+\\.(t|j)s$': '@swc/jest' },
   setupFiles: ['<rootDir>/../../jest.setup.ts'],
+  moduleNameMapper: { '^(\\.\\.?\\/.+)\\.js$': ['$1.ts', '$1.js'] },
 }
