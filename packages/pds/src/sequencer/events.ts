0200cb39a56d6582f43cd312cef1659dad753750
Daniel Holmgren <dtholmgren@gmail.com>
2024-10-29 16:52:28 -0500

    fix indentation

--- packages/pds/src/sequencer/events.ts
+++ packages/pds/src/sequencer/events.ts
@@ -27,9 +27,9 @@ export const formatSeqCommit = async (
     tooBig = true
     const justRoot = new BlockMap()
     const rootBlock = commitData.newBlocks.get(commitData.cid)
-if (rootBlock) {
-  justRoot.set(commitData.cid, rootBlock)
-}
+    if (rootBlock) {
+      justRoot.set(commitData.cid, rootBlock)
+    }
     carSlice = await blocksToCarFile(commitData.cid, justRoot)
   } else {
     tooBig = false
