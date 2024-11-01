512585a09fa61a542b582cda1a82c23365e50552
rafael <rafael@blueskyweb.xyz>
2024-11-01 00:14:49 -0300

    Enforce feed limit before hydration (#2935)
    * Enforce feed limit before hydration
    
    * Remove limit margin

--- packages/bsky/src/api/app/bsky/feed/getFeed.ts
+++ packages/bsky/src/api/app/bsky/feed/getFeed.ts
@@ -129,13 +129,14 @@ const noBlocksOrMutes = (inputs: RulesFnInput<Context, Params, Skeleton>) => {
       !bam.ancestorAuthorBlocked
     )
   })
+
   return skeleton
 }
 
 const presentation = (
   inputs: PresentationFnInput<Context, Params, Skeleton>,
 ) => {
-  const { ctx, params, skeleton, hydration } = inputs
+  const { ctx, skeleton, hydration } = inputs
   const feed = mapDefined(skeleton.items, (item) => {
     const post = ctx.views.feedViewPost(item, hydration)
     if (!post) return
@@ -143,7 +144,7 @@ const presentation = (
       ...post,
       feedContext: item.feedContext,
     }
-  }).slice(0, params.limit)
+  })
   return {
     feed,
     cursor: skeleton.cursor,
@@ -211,6 +212,7 @@ const skeletonFromFeedGen = async (
     const result = await agent.api.app.bsky.feed.getFeedSkeleton(
       {
         feed: params.feed,
+        // The feedgen is not guaranteed to honor the limit, but we try it.
         limit: params.limit,
         cursor: params.cursor,
       },
@@ -243,7 +245,7 @@ const skeletonFromFeedGen = async (
   }
 
   const { feed: feedSkele, ...skele } = skeleton
-  const feedItems = feedSkele.map((item) => ({
+  const feedItems = feedSkele.slice(0, params.limit).map((item) => ({
     post: { uri: item.post },
     repost:
       typeof item.reason?.repost === 'string'
