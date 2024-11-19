de318f5de966cbbfec905223df648dc93ad63aa7
Javier Alonso (Javinator9889) <dev@javinator9889.com>
2024-11-19 19:00:10 +0100

    [PDS][Handle] Impl: Update API and `well-known` to use the `isSupportedHandle` utility

--- packages/pds/src/well-known.ts
+++ packages/pds/src/well-known.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import AppContext from './context'
+import { isSupportedHandle } from './handle'
 
 export const createRouter = (ctx: AppContext): express.Router => {
   const router = express.Router()
 
   router.get('/.well-known/atproto-did', async function (req, res) {
     const handle = req.hostname
-    const supportedHandle = ctx.cfg.identity.serviceHandleDomains.some(
-      (host) => handle.endsWith(host) || handle === host.slice(1),
+    const supportedHandle = ctx.cfg.identity.serviceHandleDomains.some((host) =>
+      isSupportedHandle(handle, host),
     )
     if (!supportedHandle) {
       return res.status(404).send('User not found')
