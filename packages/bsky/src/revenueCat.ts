57aff9a5a30343e441593811551dd9c5996da63d
rafael <rafael@blueskyweb.xyz>
2024-12-02 17:21:28 -0300

    Replace axios with fetch

--- packages/bsky/src/revenueCat.ts
+++ packages/bsky/src/revenueCat.ts
@@ -1,5 +1,3 @@
-import axios, { AxiosInstance } from 'axios'
-
 type Config = {
   apiKey: string
   url: string
@@ -15,26 +13,38 @@ type GetSubscriberResponse = {
 }
 
 export class RevenueCatClient {
+  private apiKey: string
+  private url: string
   private webhookAuthorization: string | undefined
-  private axios: AxiosInstance
 
   constructor({ apiKey, url, webhookAuthorization }: Config) {
-    this.axios = axios.create({
-      baseURL: url,
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    })
-
+    this.apiKey = apiKey
+    this.url = url
     this.webhookAuthorization = webhookAuthorization
   }
 
+  private async fetch<T extends object>(
+    path: string,
+    method: string = 'GET',
+  ): Promise<T> {
+    const res = await fetch(`${this.url}${path}`, {
+      method,
+      headers: {
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    })
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch ${path}: ${res.statusText}`)
+    }
+
+    return res.json() as T
+  }
+
   async getSubscriber(did: string): Promise<GetSubscriberResponse> {
-    const { data } = await this.axios.get(
+    return this.fetch<GetSubscriberResponse>(
       `/subscribers/${encodeURIComponent(did)}`,
     )
-
-    return data as GetSubscriberResponse
   }
 
   isWebhookAuthorizationValid(authorization: string | undefined): boolean {
