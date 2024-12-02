00b6e2ae64bad36eaae46c6f2513a546fecda344
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-12-02 20:58:21 +0100

    import stuff from other branch

--- packages/lex-cli/src/codegen/common.ts
+++ packages/lex-cli/src/codegen/common.ts
@@ -14,29 +14,33 @@ const PRETTIER_OPTS = {
 export const utilTs = (project) =>
   gen(project, '/util.ts', async (file) => {
     file.replaceWithText(`
+export type $Typed<V> = V & { $type: string }
+
 export type $Type<Id extends string, Hash extends string> = Hash extends 'main'
   ? Id | \`\${Id}#\${Hash}\`
   : \`\${Id}#\${Hash}\`
 
-function has$type<V>(v: V): v is V & object & { $type: unknown } {
-  return v != null && typeof v === 'object' && '$type' in v
+function has$type<V>(v: V): v is $Typed<V & object> {
+  return (
+    v != null &&
+    typeof v === 'object' &&
+    '$type' in v &&
+    typeof v.$type === 'string'
+  )
 }
 
 function check$type<Id extends string, Hash extends string>(
-  $type: unknown,
+  $type: string,
   id: Id,
   hash: Hash,
 ): $type is $Type<Id, Hash> {
-  return (
-    typeof $type === 'string' &&
-    ($type === id
-      ? hash === 'main'
-      : // $type === \`\${id}#\${hash}\`
-        $type.length === id.length + 1 + hash.length &&
-        $type[id.length] === '#' &&
+  return $type === id
+    ? hash === 'main'
+    : // $type === \`\${id}#\${hash}\`
+      $type.length === id.length + 1 + hash.length &&
+        $type.charCodeAt(id.length) === 35 /* '#' */ &&
         $type.startsWith(id) &&
-        $type.endsWith(hash))
-  )
+        $type.endsWith(hash)
 }
 
 export function is$typed<V, Id extends string, Hash extends string>(
