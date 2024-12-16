819d316e8594377ab299b904d9d3d6f1fac83344
Hailey <me@haileyok.com>
2024-12-16 10:16:47 -0800

    codegen

---
+++ packages/bsky/src/api/app/bsky/graph/getTrendingTopics.ts
@@ -0,0 +1,19 @@
+import AppContext from '../../../../context'
+import { Server } from '../../../../lexicon'
+
+export default function (server: Server, ctx: AppContext) {
+  const getTrendingTopics = createPipeline(
+    skeleton,
+    hydration,
+    noBlocksOrMutes,
+    presentation,
+  )
+  server.app.bsky.graph.getTrendingTopics({
+    auth: ctx.authVerifier.standard,
+    handler: async ({ auth, params, req }) => {
+      const viewer = auth.credentials.iss
+      const labelers = ctx.reqLabelers(req)
+      const hydrateCtx = await ctx.hydrator.createContext({ labelers, viewer })
+    },
+  })
+}
---
+++ packages/ozone/src/lexicon/types/app/bsky/unspecced/getTrendingTopics.ts
@@ -0,0 +1,49 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import express from 'express'
+import { ValidationResult, BlobRef } from '@atproto/lexicon'
+import { lexicons } from '../../../../lexicons'
+import { isObj, hasProp } from '../../../../util'
+import { CID } from 'multiformats/cid'
+import { HandlerAuth, HandlerPipeThrough } from '@atproto/xrpc-server'
+import * as AppBskyUnspeccedDefs from './defs'
+
+export interface QueryParams {
+  limit: number
+}
+
+export type InputSchema = undefined
+
+export interface OutputSchema {
+  topics: AppBskyUnspeccedDefs.TrendingTopic[]
+  suggested: AppBskyUnspeccedDefs.TrendingTopic[]
+  /** DID of the account these suggestions are relative to. If this is returned undefined, suggestions are based on the viewer. */
+  relativeToDid?: string
+  [k: string]: unknown
+}
+
+export type HandlerInput = undefined
+
+export interface HandlerSuccess {
+  encoding: 'application/json'
+  body: OutputSchema
+  headers?: { [key: string]: string }
+}
+
+export interface HandlerError {
+  status: number
+  message?: string
+}
+
+export type HandlerOutput = HandlerError | HandlerSuccess | HandlerPipeThrough
+export type HandlerReqCtx<HA extends HandlerAuth = never> = {
+  auth: HA
+  params: QueryParams
+  input: HandlerInput
+  req: express.Request
+  res: express.Response
+}
+export type Handler<HA extends HandlerAuth = never> = (
+  ctx: HandlerReqCtx<HA>,
+) => Promise<HandlerOutput> | HandlerOutput
