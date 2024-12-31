6d1ad37836f275e03bc115e944a3195b82f3398d
Foysal Ahamed <foysal@blueskyweb.xyz>
2024-12-31 16:48:59 +0000

    Remove landing page for ozone (#3310)
    * :broom: Remove landing page for ozone since it's meant to serve the frontend app
    
    * :memo: Add changeset

--- packages/ozone/src/api/health.ts
+++ packages/ozone/src/api/health.ts
@@ -5,31 +5,6 @@ import AppContext from '../context'
 export const createRouter = (ctx: AppContext): express.Router => {
   const router = express.Router()
 
-  router.get('/', function (req, res) {
-    res.type('text/plain')
-    res.send(`
-    ,o888888o.   8888888888',8888'  ,o888888o.     b.             8 8 8888888888
- . 8888     '88.        ,8',8888'. 8888     '88.   888o.          8 8 8888
-,8 8888       '8b      ,8',8888',8 8888       '8b  Y88888o.       8 8 8888
-88 8888        '8b    ,8',8888' 88 8888        '8b .'Y888888o.    8 8 8888
-88 8888         88   ,8',8888'  88 8888         88 8o. 'Y888888o. 8 8 888888888888
-88 8888         88  ,8',8888'   88 8888         88 8'Y8o. 'Y88888o8 8 8888
-88 8888        ,8P ,8',8888'    88 8888        ,8P 8   'Y8o. 'Y8888 8 8888
-'8 8888       ,8P ,8',8888'     '8 8888       ,8P  8      'Y8o. 'Y8 8 8888
- ' 8888     ,88' ,8',8888'       ' 8888     ,88'   8         'Y8o.' 8 8888
-    '8888888P'  ,8',8888888888888   '8888888P'     8            'Yo 8 888888888888
-
-
-This is an AT Protocol Moderation Service API Server.
-
-Most API routes are under /xrpc/
-
-      Code: https://github.com/bluesky-social/atproto
- Self-Host: https://github.com/bluesky-social/ozone
-  Protocol: https://atproto.com
-`)
-  })
-
   router.get('/robots.txt', function (req, res) {
     res.type('text/plain')
     res.send(
