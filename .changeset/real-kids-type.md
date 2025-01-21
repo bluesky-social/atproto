c0a75d310aa92c067799a97d1acc5bd0543114c5
rafael <rafael@blueskyweb.xyz>
2025-01-21 15:49:02 -0300

    posts_with_video filter (#3390)

---
+++ .changeset/real-kids-type.md
@@ -0,0 +1,6 @@
+---
+"@atproto/bsky": patch
+"@atproto/api": patch
+---
+
+posts_with_video filter in getAuthorFeed
--- packages/bsky/src/lexicon/lexicons.ts
+++ packages/bsky/src/lexicon/lexicons.ts
@@ -6230,6 +6230,7 @@ export const schemaDict = {
                 'posts_no_replies',
                 'posts_with_media',
                 'posts_and_author_threads',
+                'posts_with_video',
               ],
               default: 'posts_with_replies',
             },
