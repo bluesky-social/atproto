5e3b8396edf14f0ae8e71c905936094e6953082d
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-02-12 10:25:01 +0100

    tidy

--- packages/oauth/oauth-provider/src/device/device-data.ts
+++ packages/oauth/oauth-provider/src/device/device-data.ts
@@ -3,12 +3,15 @@ import { deviceDetailsSchema } from './device-details.js'
 import { sessionIdSchema } from './session-id.js'
 
 export const deviceDataSchema = deviceDetailsSchema
-  // @NOTE we won't be rotating the session id if the port number changed but
-  // the ip address didn't. This is because the remote port can change from
-  // request to request as the user agent opens new connections to the server.
-  // Because of this, we don't need the DeviceStore to return the port, which is
-  // why it's omitted here.
-  .omit({ port: true })
+  .pick({
+    // @NOTE we won't be rotating the session id if the port number changed but
+    // the ip address didn't. This is because the remote port can change from
+    // request to request as the user agent opens new connections to the server.
+    // Because of this, we don't need the DeviceStore to store the port, which
+    // is why it's omitted here.
+    userAgent: true,
+    ipAddress: true,
+  })
   .extend({
     sessionId: sessionIdSchema,
     lastSeenAt: z.date(),
