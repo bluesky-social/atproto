e87b75219092e1a0c53164b5e138e32d310dde32
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-02-13 13:35:12 +0100

    docs

--- packages/oauth/oauth-provider/src/lib/util/function.ts
+++ packages/oauth/oauth-provider/src/lib/util/function.ts
@@ -1,3 +1,10 @@
+/**
+ * This function serves two purposes:
+ * - It ensures that the return value is a Promise, even if the function returns
+ *   a "thenable" (i.e. a Promise-like object).
+ * - It allows to avoid assigning a `this` context to the function, which is
+ *   particularly useful when the function is a member of a "private" object.
+ */
 export async function callAsync<F extends (...args: any[]) => unknown>(
   this: ThisParameterType<F>,
   fn: F,
