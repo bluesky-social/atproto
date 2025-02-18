010f10c6f212f699ad42c0349a58bbcf2172e3cc
bnewbold <bnewbold@robocracy.org>
2025-02-17 21:15:35 -0500

    com.atproto.sync.listReposByCollection Lexicon, for collections directory (#3524)
    * com.atproto.sync.listReposByCollection, for collections directory
    
    * bump listReposByCollection limit
    
    * update max+default to align more with listRepos
    
    * codegen
    
    * changeset for listReposByCollection

---
+++ .changeset/slimy-trainers-tease.md
@@ -0,0 +1,5 @@
+---
+"@atproto/api": patch
+---
+
+add com.atproto.sync.listReposByCollection Lexicon
---
+++ packages/bsky/src/lexicon/types/com/atproto/sync/listReposByCollection.ts
@@ -0,0 +1,68 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import express from 'express'
+import { ValidationResult, BlobRef } from '@atproto/lexicon'
+import { CID } from 'multiformats/cid'
+import { validate as _validate } from '../../../../lexicons'
+import { $Typed, is$typed as _is$typed, OmitKey } from '../../../../util'
+import { HandlerAuth, HandlerPipeThrough } from '@atproto/xrpc-server'
+
+const is$typed = _is$typed,
+  validate = _validate
+const id = 'com.atproto.sync.listReposByCollection'
+
+export interface QueryParams {
+  collection: string
+  /** Maximum size of response set. Recommend setting a large maximum (1000+) when enumerating large DID lists. */
+  limit: number
+  cursor?: string
+}
+
+export type InputSchema = undefined
+
+export interface OutputSchema {
+  cursor?: string
+  repos: Repo[]
+}
+
+export type HandlerInput = undefined
+
+export interface HandlerSuccess {
+  encoding: 'application/json'
+  body: OutputSchema
+  headers?: { [key: string]: string }
+}
+
+export interface HandlerError {
+  status: number
+  message?: string
+}
+
+export type HandlerOutput = HandlerError | HandlerSuccess | HandlerPipeThrough
+export type HandlerReqCtx<HA extends HandlerAuth = never> = {
+  auth: HA
+  params: QueryParams
+  input: HandlerInput
+  req: express.Request
+  res: express.Response
+  resetRouteRateLimits: () => Promise<void>
+}
+export type Handler<HA extends HandlerAuth = never> = (
+  ctx: HandlerReqCtx<HA>,
+) => Promise<HandlerOutput> | HandlerOutput
+
+export interface Repo {
+  $type?: 'com.atproto.sync.listReposByCollection#repo'
+  did: string
+}
+
+const hashRepo = 'repo'
+
+export function isRepo<V>(v: V) {
+  return is$typed(v, id, hashRepo)
+}
+
+export function validateRepo<V>(v: V) {
+  return validate<Repo & V>(v, id, hashRepo)
+}
