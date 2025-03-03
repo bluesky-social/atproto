6bcbb6d8cd3696280935ff7892d8e191fd21fa49
devin ivy <devinivy@gmail.com>
2025-03-03 18:25:10 -0500

    app.bsky: permit 100mb video uploads (#3602)
    * app.bsky: permit 100mb videos.
    
    * codegen
    
    * changeset
    
    * app.bsky: add description to video embed blob field.

--- packages/api/src/client/types/app/bsky/embed/video.ts
+++ packages/api/src/client/types/app/bsky/embed/video.ts
@@ -13,6 +13,7 @@ const id = 'app.bsky.embed.video'
 
 export interface Main {
   $type?: 'app.bsky.embed.video'
+  /** The mp4 video file. May be up to 100mb, formerly limited to 50mb. */
   video: BlobRef
   captions?: Caption[]
   /** Alt text description of the video, for accessibility. */
