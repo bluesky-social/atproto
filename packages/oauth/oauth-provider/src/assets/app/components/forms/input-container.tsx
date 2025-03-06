4c8be13901f2d4a5b299a16f7cf6526cf40e2544
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-03-06 22:53:08 +0100

    refactor: remove icon-card (used only once)

--- packages/oauth/oauth-provider/src/assets/app/components/forms/input-container.tsx
+++ packages/oauth/oauth-provider/src/assets/app/components/forms/input-container.tsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { JSX, ReactNode, useState } from 'react'
 import { clsx } from '../../lib/clsx.ts'
-import { IconCard, IconCardProps } from '../utils/icon-card.tsx'
+import { Override } from '../../lib/util.ts'
 
-export type InputContainerProps = IconCardProps
+export type InputContainerProps = Override<
+  JSX.IntrinsicElements['div'],
+  {
+    icon: ReactNode
+    append?: ReactNode
+  }
+>
 
 export function InputContainer({
-  className,
   icon,
+  append,
+
+  // div
+  className,
+  children,
   onFocus,
   onBlur,
   ...props
@@ -14,20 +24,8 @@ export function InputContainer({
   const [focused, setFocused] = useState(false)
 
   return (
-    <IconCard
-      className={clsx(
-        'accent-brand',
-        // Background
-        'bg-gray-100 focus:bg-slate-200 has-[:focus]:bg-slate-200',
-        'dark:bg-slate-800 dark:focus:bg-slate-700 dark:has-[:focus]:bg-slate-700',
-        // Border
-        'outline-none',
-        'border-solid border-2 border-transparent',
-        'focus:border-brand has-[:focus]:border-brand',
-        'hover:border-gray-400 hover:focus:border-gray-400',
-        'dark:hover:border-gray-500 dark:hover:focus:border-gray-500',
-        className,
-      )}
+    <div
+      {...props}
       onFocus={(event) => {
         onFocus?.(event)
         if (!event.defaultPrevented) setFocused(true)
@@ -36,10 +34,44 @@ export function InputContainer({
         onBlur?.(event)
         if (!event.defaultPrevented) setFocused(false)
       }}
-      icon={
-        icon && <div className={focused ? 'text-brand' : undefined}>{icon}</div>
-      }
-      {...props}
-    />
+      className={clsx(
+        // Layout
+        'pl-1 pr-2', // Less padding on the left because icon will provide some
+        'min-h-12',
+        'max-w-full',
+        'flex items-center justify-stretch',
+        // Font
+        'text-slate-600 dark:text-slate-300',
+        'accent-brand',
+        // Background
+        'bg-gray-100 focus:bg-slate-200 has-[:focus]:bg-slate-200',
+        'dark:bg-slate-800 dark:focus:bg-slate-700 dark:has-[:focus]:bg-slate-700',
+        // Border
+        'rounded-lg',
+        'outline-none',
+        'border-solid border-2 border-transparent',
+        'focus:border-brand has-[:focus]:border-brand',
+        'hover:border-gray-400 hover:focus:border-gray-400',
+        'dark:hover:border-gray-500 dark:hover:focus:border-gray-500',
+        className,
+      )}
+    >
+      {icon && (
+        <div
+          className={clsx(
+            'self-start shrink-0 grow-0',
+            'w-8 h-12',
+            'flex items-center justify-center',
+            focused ? 'text-brand' : 'text-slate-500',
+          )}
+        >
+          {icon}
+        </div>
+      )}
+      <div className="flex-auto relative">{children}</div>
+      {append && (
+        <div className="ml-1 grow-0 shrink-0 flex items-center">{append}</div>
+      )}
+    </div>
   )
 }
