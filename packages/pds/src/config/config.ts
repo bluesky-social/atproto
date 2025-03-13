e0f77b224534e4adab39a0e4f5d414c69fa9ba78
ledlamp <lamp@owo69.me>
2025-03-12 21:35:56 -0700

    pds: Change default blob upload limit to 100MB to match #3602

--- packages/pds/src/config/config.ts
+++ packages/pds/src/config/config.ts
@@ -25,7 +25,7 @@ export const envToCfg = (env: ServerEnvironment): ServerConfig => {
     termsOfServiceUrl: env.termsOfServiceUrl,
     contactEmailAddress: env.contactEmailAddress,
     acceptingImports: env.acceptingImports ?? true,
-    blobUploadLimit: env.blobUploadLimit ?? 50 * 1024 * 1024, // 50 MiB
+    blobUploadLimit: env.blobUploadLimit ?? 100000000 // 100MB
     devMode: env.devMode ?? false,
   }
 
