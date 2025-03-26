e97a185f1e5202175dd4303522c1358180a1dd7c
Eric Bailey <git@esb.lol>
2025-03-26 16:31:35 -0500

    Simplify identifier validation

--- packages/oauth/oauth-provider-frontend/src/util/useValidateHandle.ts
+++ packages/oauth/oauth-provider-frontend/src/util/useValidateHandle.ts
@@ -32,29 +32,29 @@ export function useValidateHandle(): (v: string) => ValidateHandleResult {
           let message = ''
 
           switch (e.message) {
-            case 'Disallowed characters in handle (ASCII letters, digits, dashes, periods only)':
-              message = _(msg`Handle contains disallowed characters`)
-              break
-            case 'Handle is too long (253 chars max)':
-              message = _(msg`Handle is too long`)
-              break
-            case 'Handle domain needs at least two parts':
-              message = _(msg`Handle domain needs at least two parts`)
-              break
-            case 'Handle parts can not be empty':
-              message = _(msg`Handle parts can not be empty`)
-              break
-            case 'Handle part too long (max 63 chars)':
-              message = _(msg`Handle part is too long`)
-              break
-            case 'Handle parts can not start or end with hyphens':
-              message = _(msg`Handle parts can not start or end with hyphens`)
-              break
-            case 'Handle final component (TLD) must start with ASCII letter':
-              message = _(msg`Handle final component must start with a letter`)
-              break
+            // case 'Disallowed characters in handle (ASCII letters, digits, dashes, periods only)':
+            //   message = _(msg`Handle contains disallowed characters`)
+            //   break
+            // case 'Handle is too long (253 chars max)':
+            //   message = _(msg`Handle is too long`)
+            //   break
+            // case 'Handle domain needs at least two parts':
+            //   message = _(msg`Handle domain needs at least two parts`)
+            //   break
+            // case 'Handle parts can not be empty':
+            //   message = _(msg`Handle parts can not be empty`)
+            //   break
+            // case 'Handle part too long (max 63 chars)':
+            //   message = _(msg`Handle part is too long`)
+            //   break
+            // case 'Handle parts can not start or end with hyphens':
+            //   message = _(msg`Handle parts can not start or end with hyphens`)
+            //   break
+            // case 'Handle final component (TLD) must start with ASCII letter':
+            //   message = _(msg`Handle final component must start with a letter`)
+            //   break
             default:
-              message = _(msg`Handle is invalid`)
+              message = _(msg`Invalid handle`)
               break
           }
 
@@ -66,7 +66,7 @@ export function useValidateHandle(): (v: string) => ValidateHandleResult {
 
         return {
           success: false,
-          message: _(msg`Handle is invalid`),
+          message: _(msg`Invalid handle`),
         }
       }
     },
