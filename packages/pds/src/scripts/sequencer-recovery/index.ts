2b6c8b14e736433c545b65bc3458192a1b5e22de
dholms <dtholmgren@gmail.com>
2025-03-27 19:02:10 -0500

    tidy

--- packages/pds/src/scripts/sequencer-recovery/index.ts
+++ packages/pds/src/scripts/sequencer-recovery/index.ts
@@ -1,9 +1,3 @@
-// import { IdResolver } from '@atproto/identity'
-// import { AccountManager } from '../../account-manager/account-manager'
-// import { ActorStore } from '../../actor-store/actor-store'
-// import { BackgroundQueue } from '../../background'
-// import { Crawlers } from '../../crawlers'
-// import { Sequencer } from '../../sequencer'
 import { parseIntArg } from '../util'
 import { Recoverer, RecovererContextNoDb } from './recoverer'
 import { getRecoveryDbFromSequencerLoc } from './recovery-db'
@@ -27,35 +21,3 @@ export const sequencerRecovery = async (
   )
   await recover.run(cursor)
 }
-
-// const run = async () => {
-//   const backgroundQueue = new BackgroundQueue()
-//   const sequencer = new Sequencer(
-//     './backup/sequencer.sqlite',
-//     new Crawlers('', [], backgroundQueue),
-//   )
-//   const actorStore = new ActorStore(
-//     {
-//       directory: './backup/actors',
-//       cacheSize: 0,
-//       disableWalAutoCheckpoint: false,
-//     },
-//     { blobstore: () => ({}) as any, backgroundQueue },
-//   )
-
-//   const accountManager = new AccountManager(
-//     new IdResolver(),
-//     {} as any, // jwtKey
-//     'did:example:serviceDid',
-//     [], // service handle domains
-//     {
-//       accountDbLoc: './backup/account.sqlite',
-//       disableWalAutoCheckpoint: false,
-//     },
-//   )
-//   const recoveryDb = await getAndMigrateRecoveryDb('./backup/recovery.sqlite')
-//   const ctx = { sequencer, accountManager, actorStore, recoveryDb }
-//   return sequencerRecovery(ctx, [])
-// }
-
-// run()
