4be3789991d76b5b90f3ce22b68a3492434451db
dholms <dtholmgren@gmail.com>
2025-04-04 15:28:57 -0500

    awaits

--- packages/ozone/src/db/migrations/20250404T201720309Z-subject-status-sort-idxs.ts
+++ packages/ozone/src/db/migrations/20250404T201720309Z-subject-status-sort-idxs.ts
@@ -2,15 +2,17 @@ import { Kysely, sql } from 'kysely'
 
 export async function up(db: Kysely<unknown>): Promise<void> {
   const ref = db.dynamic.ref
-  sql`CREATE INDEX moderation_subject_status_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST);`.execute(
+  await sql`CREATE INDEX moderation_subject_status_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST);`.execute(
     db,
   )
-  sql`CREATE INDEX moderation_subject_status_unreviewed_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST); WHERE ${ref('reviewState')} = 'tools.ozone.moderation.defs#reviewNone';`.execute(
+  await sql`CREATE INDEX moderation_subject_status_unreviewed_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST); WHERE ${ref('reviewState')} = 'tools.ozone.moderation.defs#reviewNone';`.execute(
     db,
   )
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  db.schema.dropIndex('moderation_subject_status_sort_idx').execute()
-  db.schema.dropIndex('moderation_subject_status_unreviewed_sort_idx').execute()
+  await db.schema.dropIndex('moderation_subject_status_sort_idx').execute()
+  await db.schema
+    .dropIndex('moderation_subject_status_unreviewed_sort_idx')
+    .execute()
 }
