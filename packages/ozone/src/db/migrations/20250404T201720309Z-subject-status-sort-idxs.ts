f109875b1e1adb7ccd208499a2878951f02d1cc9
dholms <dtholmgren@gmail.com>
2025-04-04 16:28:29 -0500

    fix syntax err

--- packages/ozone/src/db/migrations/20250404T201720309Z-subject-status-sort-idxs.ts
+++ packages/ozone/src/db/migrations/20250404T201720309Z-subject-status-sort-idxs.ts
@@ -5,7 +5,7 @@ export async function up(db: Kysely<unknown>): Promise<void> {
   await sql`CREATE INDEX moderation_subject_status_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST);`.execute(
     db,
   )
-  await sql`CREATE INDEX moderation_subject_status_unreviewed_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST); WHERE ${ref('reviewState')} = 'tools.ozone.moderation.defs#reviewNone';`.execute(
+  await sql`CREATE INDEX moderation_subject_status_unreviewed_sort_idx ON ${ref('moderation_subject_status')} (${ref('lastReportedAt')} DESC NULLS LAST, ${ref('id')} DESC NULLS LAST) WHERE ${ref('reviewState')} = 'tools.ozone.moderation.defs#reviewNone';`.execute(
     db,
   )
 }
