1b50c70d0e95065a2506b8558c36986c17666999
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-04-08 15:53:18 +0200

    Review comments

--- packages/oauth/oauth-provider/src/types/email-otp.ts
+++ packages/oauth/oauth-provider/src/types/email-otp.ts
@@ -1,5 +1,3 @@
 import { z } from 'zod'
 
-export const emailOtpSchema = z
-  .string()
-  .regex(/^[A-Z2-7]{5}-[A-Z2-7]{5}$/, 'Invalid token format')
+export const emailOtpSchema = z.string().min(1)
