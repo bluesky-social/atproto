1e461eab033f728f537db554b3072b7eda7e5e8f
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2025-04-16 10:25:29 +0200

    Fix missing dist files from `@atproto/oauth-provider-ui` (#3754)
    * Remove reference to missing "bin" executable
    
    * Remove "dependency" on `rollup-plugin-bundle-manifest`
    
    * Fix dependencies
    
    * Explicit exported package `files`
    
    * Update pnpm lock

---
+++ .changeset/tasty-buses-shake.md
@@ -0,0 +1,5 @@
+---
+"@atproto/oauth-provider": patch
+---
+
+Remove "dependency" on `rollup-plugin-bundle-manifest`
--- packages/oauth/oauth-provider-frontend/src/hydration-data.d.ts
+++ packages/oauth/oauth-provider-frontend/src/hydration-data.d.ts
@@ -1,17 +1,1 @@
-import {
-  ActiveDeviceSession,
-  CustomizationData,
-} from '@atproto/oauth-provider-api'
-
-export type HydrationData = {
-  'account-page': {
-    /**
-     * needed by `useCustomizationData.ts`
-     */
-    __customizationData: CustomizationData
-    /**
-     * needed by `useDeviceSessionsQuery.ts`
-     */
-    __deviceSessions: readonly ActiveDeviceSession[]
-  }
-}
+../hydration-data.d.ts
