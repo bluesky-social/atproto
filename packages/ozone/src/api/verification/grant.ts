5e3dadc456a28ff59d443485c0cdb025b3c7bfcc
Foysal Ahamed <foysal@blueskyweb.xyz>
2025-04-18 17:45:38 +0200

    :broom: Remove unnecessary property

--- packages/ozone/src/api/verification/grant.ts
+++ packages/ozone/src/api/verification/grant.ts
@@ -26,8 +26,7 @@ export default function (server: Server, ctx: AppContext) {
         return {
           encoding: 'application/json',
           body: {
-            verifications: [],
-            failedVerifications,
+            verifications: [...failedVerifications],
           },
         }
       }
