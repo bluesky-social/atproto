783868212766756c499214fe09970fdc3c2f8127
Eric Bailey <git@esb.lol>
2025-04-30 16:22:43 -0500

    Format

--- packages/bsky/tests/seed/get-post-thread-v2.seed.ts
+++ packages/bsky/tests/seed/get-post-thread-v2.seed.ts
@@ -1,6 +1,6 @@
 import { SeedClient, TestNetwork, TestNetworkNoAppView } from '@atproto/dev-env'
 
-import {createUserStub} from './util'
+import { createUserStub } from './util'
 
 export type Users = typeof userStubs
 export type Seed = Awaited<ReturnType<typeof seed>>
@@ -14,9 +14,7 @@ const userStubs = {
   dan: createUserStub('dan'),
 }
 
-export async function seed(
-  sc: SeedClient<TestNetwork | TestNetworkNoAppView>,
-) {
+export async function seed(sc: SeedClient<TestNetwork | TestNetworkNoAppView>) {
   const users = structuredClone(userStubs)
 
   await sc.createAccount('opp', users.opp)
@@ -33,24 +31,64 @@ export async function seed(
 
   const op1_0 = await sc.reply(users.opp.did, root.ref, root.ref, '(opp) 1_0')
   const op1_1 = await sc.reply(users.opp.did, root.ref, op1_0.ref, '(opp) 1_1')
-  const op1_1_1 = await sc.reply(users.opp.did, root.ref, op1_1.ref, '(opp) 1_1_1')
-  const op1_1_1_1 = await sc.reply(users.opp.did, root.ref, op1_1_1.ref, '(opp) 1_1_1_1')
-  const op1_1_1_1_1 = await sc.reply(users.opp.did, root.ref, op1_1_1_1.ref, '(opp) 1_1_1_1_1')
+  const op1_1_1 = await sc.reply(
+    users.opp.did,
+    root.ref,
+    op1_1.ref,
+    '(opp) 1_1_1',
+  )
+  const op1_1_1_1 = await sc.reply(
+    users.opp.did,
+    root.ref,
+    op1_1_1.ref,
+    '(opp) 1_1_1_1',
+  )
+  const op1_1_1_1_1 = await sc.reply(
+    users.opp.did,
+    root.ref,
+    op1_1_1_1.ref,
+    '(opp) 1_1_1_1_1',
+  )
   const op1_2 = await sc.reply(users.opp.did, root.ref, op1_1.ref, '(opp) 1_2')
 
-  const a1_0 = await sc.reply(users.alice.did, root.ref, root.ref, '(alice) 1_0')
+  const a1_0 = await sc.reply(
+    users.alice.did,
+    root.ref,
+    root.ref,
+    '(alice) 1_0',
+  )
   const b1_0 = await sc.reply(users.bob.did, root.ref, root.ref, '(bob) 1_0')
-  const c1_0 = await sc.reply(users.carla.did, root.ref, root.ref, '(carla) 1_0')
+  const c1_0 = await sc.reply(
+    users.carla.did,
+    root.ref,
+    root.ref,
+    '(carla) 1_0',
+  )
 
   const op2_0 = await sc.reply(users.opp.did, root.ref, root.ref, '(opp) 2_0')
   const op2_1 = await sc.reply(users.opp.did, root.ref, op2_0.ref, '(opp) 2_1')
-  const a2_2 = await sc.reply(users.alice.did, root.ref, op2_1.ref, '(alice) 2_2')
+  const a2_2 = await sc.reply(
+    users.alice.did,
+    root.ref,
+    op2_1.ref,
+    '(alice) 2_2',
+  )
   const op2_3 = await sc.reply(users.opp.did, root.ref, a2_2.ref, '(opp) 2_3')
   const op2_4 = await sc.reply(users.opp.did, root.ref, op2_3.ref, '(opp) 2_4')
 
-  const a2_0 = await sc.reply(users.alice.did, root.ref, root.ref, '(alice) 2_0')
+  const a2_0 = await sc.reply(
+    users.alice.did,
+    root.ref,
+    root.ref,
+    '(alice) 2_0',
+  )
   const b2_0 = await sc.reply(users.bob.did, root.ref, root.ref, '(bob) 2_0')
-  const c2_0 = await sc.reply(users.carla.did, root.ref, root.ref, '(carla) 2_0')
+  const c2_0 = await sc.reply(
+    users.carla.did,
+    root.ref,
+    root.ref,
+    '(carla) 2_0',
+  )
 
   await sc.like(users.opp.did, a2_0.ref)
   await sc.like(users.bob.did, a2_0.ref)
