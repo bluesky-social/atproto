fe2a1d12bb6efc66b7f033eb6e21ec354f6b47fb
Foysal Ahamed <foysal@blueskyweb.xyz>
2025-05-28 10:30:43 +0200

    :bug: Add datetime format in lexicon def for createdAt field on grantVerification input (#3829)

--- packages/ozone/src/lexicon/lexicons.ts
+++ packages/ozone/src/lexicon/lexicons.ts
@@ -16154,6 +16154,7 @@ export const schemaDict = {
           },
           createdAt: {
             type: 'string',
+            format: 'datetime',
             description:
               'Timestamp for verification record. Defaults to current time when not specified.',
           },
