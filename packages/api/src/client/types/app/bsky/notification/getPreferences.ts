28738df2f0fcf8ff4b04fa3c1b801d7a8a78d05a
Samuel Newman <mozzius@protonmail.com>
2025-05-28 20:09:15 +0300

    codegen (broken!)

---
+++ packages/api/src/client/types/app/bsky/notification/getPreferences.ts
@@ -0,0 +1,52 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import { HeadersMap, XRPCError } from '@atproto/xrpc'
+import { type ValidationResult, BlobRef } from '@atproto/lexicon'
+import { CID } from 'multiformats/cid'
+import { validate as _validate } from '../../../../lexicons'
+import {
+  type $Typed,
+  is$typed as _is$typed,
+  type OmitKey,
+} from '../../../../util'
+import type * as AppBskyNotificationDefs from './defs.js'
+
+const is$typed = _is$typed,
+  validate = _validate
+const id = 'app.bsky.notification.getPreferences'
+
+export interface QueryParams {}
+
+export type InputSchema = undefined
+
+export interface OutputSchema {
+  likeNotification: AppBskyNotificationDefs.Preference
+  repostNotification: AppBskyNotificationDefs.Preference
+  followNotification: AppBskyNotificationDefs.Preference
+  replyNotification: AppBskyNotificationDefs.Preference
+  mentionNotification: AppBskyNotificationDefs.Preference
+  quoteNotification: AppBskyNotificationDefs.Preference
+  starterpackJoinedNotification: AppBskyNotificationDefs.Preference
+  verifiedNotification: AppBskyNotificationDefs.Preference
+  unverifiedNotification: AppBskyNotificationDefs.Preference
+  likeViaRepostNotification: AppBskyNotificationDefs.Preference
+  repostViaRepostNotification: AppBskyNotificationDefs.Preference
+  subscribedPostNotification: AppBskyNotificationDefs.Preference
+  chatNotification: AppBskyNotificationDefs.Preference
+}
+
+export interface CallOptions {
+  signal?: AbortSignal
+  headers?: HeadersMap
+}
+
+export interface Response {
+  success: boolean
+  headers: HeadersMap
+  data: OutputSchema
+}
+
+export function toKnownErr(e: any) {
+  return e
+}
