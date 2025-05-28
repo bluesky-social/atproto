e474623d3f7400ff957f230d3870abb4ffe0e383
Jim Calabro <jamesrcalabro@gmail.com>
2025-05-28 10:37:04 -0400

    Add Generic Bsync Proto (#3887)
    Co-authored-by: rafael <rafael@blueskyweb.xyz>

--- packages/bsync/src/db/migrations/index.ts
+++ packages/bsync/src/db/migrations/index.ts
@@ -4,3 +4,4 @@
 
 export * as _20240108T220751294Z from './20240108T220751294Z-init'
 export * as _20240717T224303472Z from './20240717T224303472Z-notif-ops'
+export * as _20250527T022203400Z from './20250527T022203400Z-add-operation'
