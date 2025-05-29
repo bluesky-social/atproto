47b9f73cd54c2f681fc1d76f478e0e338b6d555f
Samuel Newman <mozzius@protonmail.com>
2025-05-29 14:02:44 +0300

    fix codegen

--- packages/bsky/src/lexicon/types/app/bsky/notification/defs.ts
+++ packages/bsky/src/lexicon/types/app/bsky/notification/defs.ts
@@ -1,13 +1,13 @@
 /**
  * GENERATED CODE - DO NOT MODIFY
  */
-import { type ValidationResult, BlobRef } from '@atproto/lexicon'
 import { CID } from 'multiformats/cid'
+import { BlobRef, type ValidationResult } from '@atproto/lexicon'
 import { validate as _validate } from '../../../../lexicons'
 import {
   type $Typed,
-  is$typed as _is$typed,
   type OmitKey,
+  is$typed as _is$typed,
 } from '../../../../util'
 
 const is$typed = _is$typed,
