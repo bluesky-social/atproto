1287e7ecb4b4e4d99415ef9a975358794df9cdfd
Samuel Newman <mozzius@protonmail.com>
2025-05-29 14:00:23 +0300

    add brackets in correct place

--- packages/lex-cli/src/codegen/lex-gen.ts
+++ packages/lex-cli/src/codegen/lex-gen.ts
@@ -331,15 +331,6 @@ export function genArray(
       type: `(${types.join('|')})[]`,
       isExported: true,
     })
-  } else if (def.items.type === 'string') {
-    genComment(
-      file.addTypeAlias({
-        name: toTitleCase(getHash(lexUri)),
-        type: `(${primitiveOrBlobToType(def.items)})[]`,
-        isExported: true,
-      }),
-      def,
-    )
   } else {
     genComment(
       file.addTypeAlias({
@@ -676,7 +667,7 @@ function makeType(
   const types = ([] as string[]).concat(_types)
   if (opts?.nullable) types.push('null')
   const arr = opts?.array ? '[]' : ''
-  if (types.length === 1) return `${types[0]}${arr}`
+  if (types.length === 1) return `(${types[0]})${arr}`
   if (arr) return `(${types.join(' | ')})${arr}`
   return types.join(' | ')
 }
