322de4ef6fccadc84345ac5db9e22b27ae746403
Eric Bailey <git@esb.lol>
2025-06-13 10:53:55 -0500

    Rename files

---
+++ packages/bsky/src/api/app/bsky/assurance/env.ts
@@ -0,0 +1,8 @@
+export const OAUTH_URL =
+  'https://auth.kidswebservices.com/auth/realms/kws/protocol/openid-connect/token'
+export const SEND_EMAIL_URL =
+  'https://api.kidswebservices.com/v1/verifications/send-email'
+export const CLIENT_ID = '451b0e2b-7592-4bd6-afca-bb4b4e0ec927'
+export const API_KEY = 'Ir2Kg3IVZyp9afhXUWpDEnCANUIp8Vuv'
+export const VERIFICATION_SECRET = `6aef387b-0a3d-4619-80d1-24edb05e403f`
+export const WEBHOOK_SECRET = `af945dd7-a5fd-4c3a-9087-50867bfd5653`
