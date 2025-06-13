91a974b109cb2091d4ac815563b2c4750a6f6d86
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-06-13 16:31:18 +0200

    Improve error reporting and logging

--- packages/oauth/oauth-provider/src/lib/util/zod-error.ts
+++ packages/oauth/oauth-provider/src/lib/util/zod-error.ts
@@ -2,12 +2,29 @@ import { ZodError } from 'zod'
 
 export function extractZodErrorMessage(err: unknown): string | undefined {
   if (err instanceof ZodError) {
-    const issue = err.issues[0]
-    if (issue?.path.length) {
-      // "part" will typically be "body" or "query"
-      const [part, ...path] = issue.path
-      return `Validation of "${path.join('.')}" ${part} parameter failed: ${issue.message}`
+    for (const issue of err.issues) {
+      if (issue.path.length) {
+        // "part" will typically be "body" or "query"
+        const [part, ...path] = issue.path
+        const location = path.length ? `"${path.join('.')}" ${part}` : part
+        const message = extractMessage(issue)
+        return `Validation of ${location} parameter failed${message ? `: ${message}` : ''}`
+      }
     }
+
+    const message = extractMessage(err)
+    return `Validation failed${message ? `: ${message}` : ''}`
+  }
+
+  return undefined
+}
+
+function extractMessage({ message }: { message: string }): string | undefined {
+  // message serialization defaults to JSON.stringify(err.issues)
+  if (!message.startsWith('[') && !message.endsWith(']')) {
+    // If the error message does not end with ']', it means it is not a
+    // serialization of the issues, so we can return it directly.
+    return message
   }
 
   return undefined
