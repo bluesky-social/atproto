262cfdcf4c134e2732ad02c6ae618f4fd3dfc09f
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-06-24 16:15:31 +0200

    Improve client validation

--- packages/pds/src/oauth/oauth-scopes.ts
+++ packages/pds/src/oauth/oauth-scopes.ts
@@ -1,10 +1,4 @@
-import { OAuthScope } from '@atproto/oauth-provider'
-
-export function validateScope(scope?: OAuthScope): boolean {
-  return scope == null || scope.split(' ').every(isKnownScope)
-}
-
-function isKnownScope(value) {
+export function isValidPermission(value) {
   return (
     value === 'atproto' ||
     value === 'transition:email' ||
