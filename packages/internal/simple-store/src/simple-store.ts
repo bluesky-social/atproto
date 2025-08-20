1c623b89fdb82589704dfd6b4cddfe8725cd42a0
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-08-20 12:59:18 +0200

    Improve `SimpleStore` api

--- packages/internal/simple-store/src/simple-store.ts
+++ packages/internal/simple-store/src/simple-store.ts
@@ -1,15 +1,15 @@
-export type Awaitable<V> = V | PromiseLike<V>
+import { Awaitable } from './util.js'
 
-export type Key = string | number
+export type Key = NonNullable<unknown>
 export type Value = NonNullable<unknown> | null
 
-export type GetOptions = { signal?: AbortSignal }
+export type GetStoredOptions = { signal?: AbortSignal }
 
-export interface SimpleStore<K extends Key = string, V extends Value = Value> {
+export interface SimpleStore<K extends Key, V extends Value> {
   /**
    * @return undefined if the key is not in the store (which is why Value cannot contain "undefined").
    */
-  get: (key: K, options?: GetOptions) => Awaitable<undefined | V>
+  get: (key: K, options?: GetStoredOptions) => Awaitable<undefined | V>
   set: (key: K, value: V) => Awaitable<void>
   del: (key: K) => Awaitable<void>
   clear?: () => Awaitable<void>
