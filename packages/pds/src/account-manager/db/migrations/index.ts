6d7bf4bffc3fee7a1fca488e6b75699385a04f37
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2025-09-04 11:04:48 +0200

    Remove old, never resolved, lexicons from the database (#4162)

--- packages/pds/src/account-manager/db/migrations/index.ts
+++ packages/pds/src/account-manager/db/migrations/index.ts
@@ -4,6 +4,7 @@ import * as mig003 from './003-privileged-app-passwords'
 import * as mig004 from './004-oauth'
 import * as mig005 from './005-oauth-account-management'
 import * as mig006 from './006-oauth-permission-sets'
+import * as mig007 from './007-lexicon-failures-index'
 
 export default {
   '001': mig001,
@@ -12,4 +13,5 @@ export default {
   '004': mig004,
   '005': mig005,
   '006': mig006,
+  '007': mig007,
 }
