68e6582c834d7b0a13f1b605c87fce64f41979a3
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-09-09 11:27:12 +0200

    Temporarily disable _002 actor db migration

--- packages/pds/src/actor-store/db/migrations/index.ts
+++ packages/pds/src/actor-store/db/migrations/index.ts
@@ -1,7 +1,11 @@
 import * as _001 from './001-init'
 import * as _002 from './002-blob_tempkey_idx-removal'
 
+// @NOTE we currently do not support migrating the Actor DB.
+// Since the _002 migration is more of an optimization, we can
+// skip it for now. We can enable it later if needed.
+
 export default {
   '001': _001,
-  '002': _002,
+  // '002': _002,
 }
