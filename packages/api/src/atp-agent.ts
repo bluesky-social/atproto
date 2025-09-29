bd71a16ab0d3906f92b7c5b33652e276e0fcaa6c
Dan Abramov <dan.abramov@gmail.com>
2025-09-29 16:43:16 +0100

    Split code in two branches

--- packages/api/src/atp-agent.ts
+++ packages/api/src/atp-agent.ts
@@ -411,14 +411,16 @@ export class CredentialSession implements SessionManager {
     } catch (err) {
       // protect against concurrent session updates
       if (this.session === session) {
-        this.session = undefined
-        this.persistSession?.(
+        if (
           err instanceof XRPCError &&
-            ['ExpiredToken', 'InvalidToken'].includes(err.error)
-            ? 'expired'
-            : 'network-error',
-          undefined,
-        )
+          ['ExpiredToken', 'InvalidToken'].includes(err.error)
+        ) {
+          this.session = undefined
+          this.persistSession?.('expired', undefined)
+        } else {
+          this.session = undefined
+          this.persistSession?.('network-error', undefined)
+        }
       }
 
       throw err
