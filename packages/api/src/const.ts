22e2e159a4e4cc622a09ae5ec83b5b7a1d33a20e
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-10-17 14:30:30 +0200

    revert test changes

--- packages/api/src/const.ts
+++ packages/api/src/const.ts
@@ -1,10 +1,1 @@
-import * as com from '@atproto/lex/com'
-import { l } from '@atproto/lex/lex'
-
 export const BSKY_LABELER_DID = 'did:plc:ar7c4by46qjdydhdevvrndac'
-
-export const foo = l.object({
-  bar: com.atproto.repo.defs.commitMeta,
-})
-
-export type Foo = l.Infer<typeof foo>
