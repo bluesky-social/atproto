cbfc29556e41a2e2f8ff1983512d6374ca37f6ee
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-10-23 09:05:36 +0200

    codegen

--- packages/ozone/src/lexicon/types/app/bsky/feed/repost.ts
+++ packages/ozone/src/lexicon/types/app/bsky/feed/repost.ts
@@ -15,7 +15,7 @@ const is$typed = _is$typed,
   validate = _validate
 const id = 'app.bsky.feed.repost'
 
-export interface Record {
+export interface Main {
   $type: 'app.bsky.feed.repost'
   subject: ComAtprotoRepoStrongRef.Main
   createdAt: string
@@ -23,12 +23,18 @@ export interface Record {
   [k: string]: unknown
 }
 
-const hashRecord = 'main'
+const hashMain = 'main'
 
-export function isRecord<V>(v: V) {
-  return is$typed(v, id, hashRecord)
+export function isMain<V>(v: V) {
+  return is$typed(v, id, hashMain)
 }
 
-export function validateRecord<V>(v: V) {
-  return validate<Record & V>(v, id, hashRecord, true)
+export function validateMain<V>(v: V) {
+  return validate<Main & V>(v, id, hashMain, true)
+}
+
+export {
+  type Main as Record,
+  isMain as isRecord,
+  validateMain as validateRecord,
 }
