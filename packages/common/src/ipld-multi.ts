d72b9ac2b34f1550a6468a607de173c2f68640b5
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-11-04 12:47:18 +0100

    Smaller bundle code footprint

--- packages/common/src/ipld-multi.ts
+++ packages/common/src/ipld-multi.ts
@@ -1,16 +1,19 @@
-import * as cborx from 'cbor-x'
+import { Decoder, addExtension } from 'cbor-x/decode'
 import { CID } from 'multiformats/cid'
 
+// @NOTE We use "cbor-x" here because it supports decoding multiple CBOR items
+// from a single byte array (and because it is fast). We do *NOT* use it for
+// encoding because it does not yield DAG-CBOR compliant CBOR.
+
 // add extension for decoding CIDs
-// decoding code taken from @ipld/dag-cbor
-// does not support encoding cids
-cborx.addExtension({
+addExtension({
   Class: CID,
   tag: 42,
   encode: () => {
     throw new Error('cannot encode cids')
   },
   decode: (bytes: Uint8Array): CID => {
+    // @NOTE code taken from @ipld/dag-cbor
     if (bytes[0] !== 0) {
       throw new Error('Invalid CID for CBOR tag 42; expected leading 0x00')
     }
@@ -18,16 +21,12 @@ cborx.addExtension({
   },
 })
 
-const decoder = new cborx.Decoder({
-  // @ts-ignore
-  int64AsNumber: true, // not in types for some reason
+const decoder = new Decoder({
+  // @ts-expect-error: not in types for some reason
+  int64AsNumber: true,
   useRecords: false,
 })
 
 export const cborDecodeMulti = (encoded: Uint8Array): unknown[] => {
-  const decoded: unknown[] = []
-  decoder.decodeMultiple(encoded, (value) => {
-    decoded.push(value)
-  })
-  return decoded
+  return decoder.decodeMultiple(encoded)!
 }
