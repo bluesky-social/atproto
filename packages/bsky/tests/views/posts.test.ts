14e3e9fbed5d5b024c0a4b5d11f23b510818046f
rafaeleyng <rafaeleyng@gmail.com>
2025-11-20 16:13:03 -0300

    Address PR review

--- packages/bsky/tests/views/posts.test.ts
+++ packages/bsky/tests/views/posts.test.ts
@@ -73,12 +73,15 @@ describe('pds posts views', () => {
   })
 
   it(`omits not-found posts`, async () => {
+    // This is a valid post AT-URI (from a prod post), but it shouldn't exist in the test env.
+    const badPostUri =
+      'at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.post/3m5yqexldn22q'
+
     const uris = [
       sc.posts[sc.dids.alice][0].ref.uriStr,
       sc.posts[sc.dids.alice][1].ref.uriStr,
       sc.posts[sc.dids.bob][0].ref.uriStr,
-      // This is a valid post AT-URI (from a prod post), but it shouldn't exist in the test env.
-      'at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.post/3m5yqexldn22q',
+      badPostUri,
     ]
     const posts = await agent.app.bsky.feed.getPosts(
       { uris },
@@ -91,15 +94,18 @@ describe('pds posts views', () => {
     )
 
     expect(posts.data.posts.length).toBe(uris.length - 1)
-    expect(forSnapshot(posts.data.posts)).toMatchSnapshot()
+    expect(posts.data.posts.map((p) => p.uri).includes(badPostUri)).toBe(false)
   })
 
   it(`omits taken-down posts`, async () => {
+    // Taken-down post.
+    const badPostUri = sc.posts[sc.dids.eve][0].ref.uriStr
+
     const uris = [
       sc.posts[sc.dids.alice][0].ref.uriStr,
       sc.posts[sc.dids.alice][1].ref.uriStr,
       sc.posts[sc.dids.bob][0].ref.uriStr,
-      sc.posts[sc.dids.eve][0].ref.uriStr, // Taken-down post.
+      badPostUri,
     ]
     const posts = await agent.app.bsky.feed.getPosts(
       { uris },
@@ -112,15 +118,18 @@ describe('pds posts views', () => {
     )
 
     expect(posts.data.posts.length).toBe(uris.length - 1)
-    expect(forSnapshot(posts.data.posts)).toMatchSnapshot()
+    expect(posts.data.posts.map((p) => p.uri).includes(badPostUri)).toBe(false)
   })
 
   it(`omits posts by taken-down accounts`, async () => {
+    // Taken-down account.
+    const badPostUri = sc.posts[sc.dids.frankie][0].ref.uriStr
+
     const uris = [
       sc.posts[sc.dids.alice][0].ref.uriStr,
       sc.posts[sc.dids.alice][1].ref.uriStr,
       sc.posts[sc.dids.bob][0].ref.uriStr,
-      sc.posts[sc.dids.frankie][0].ref.uriStr, // Taken-down account.
+      badPostUri,
     ]
     const posts = await agent.app.bsky.feed.getPosts(
       { uris },
@@ -133,7 +142,7 @@ describe('pds posts views', () => {
     )
 
     expect(posts.data.posts.length).toBe(uris.length - 1)
-    expect(forSnapshot(posts.data.posts)).toMatchSnapshot()
+    expect(posts.data.posts.map((p) => p.uri).includes(badPostUri)).toBe(false)
   })
 
   it('fetches posts unauthed', async () => {
