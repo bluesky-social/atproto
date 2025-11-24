33da5c559c5de24d46bfb46b26c5782e1e2a755c
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-11-24 16:17:35 +0100

    tidy

--- packages/lex/lex-schema/src/schema/string.ts
+++ packages/lex/lex-schema/src/schema/string.ts
@@ -101,7 +101,6 @@ export class StringSchema<
   }
 }
 
-const utf8Decoder = /*#__PURE__*/ new TextDecoder('utf-8', { fatal: false })
 export function coerceToString(input: unknown): string | null {
   switch (typeof input) {
     case 'string':
@@ -115,14 +114,6 @@ export function coerceToString(input: unknown): string | null {
         return input.toString()
       }
 
-      if (input instanceof ArrayBuffer || input instanceof Uint8Array) {
-        try {
-          return utf8Decoder.decode(input)
-        } catch {
-          return null
-        }
-      }
-
       if (input instanceof Date) {
         if (Number.isNaN(input.getTime())) return null
         return input.toISOString()
@@ -132,12 +123,12 @@ export function coerceToString(input: unknown): string | null {
         return input.toString()
       }
 
+      const cid = CID.asCID(input)
+      if (cid) return cid.toString()
+
       if (input instanceof String) {
         return input.valueOf()
       }
-
-      const cid = CID.asCID(input)
-      if (cid) return cid.toString()
     }
 
     // falls through
