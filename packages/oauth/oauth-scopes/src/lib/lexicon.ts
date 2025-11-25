91c61a96490d04ad389cc168952de5e1620372ab
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-11-25 21:42:16 +0100

    tidy

--- packages/oauth/oauth-scopes/src/lib/lexicon.ts
+++ packages/oauth/oauth-scopes/src/lib/lexicon.ts
@@ -9,7 +9,13 @@ export type LexiconPermission<P extends string = string> = {
   readonly [x: string]: undefined | ParamValue | readonly ParamValue[]
 }
 
+type LangMap = { readonly [Lang in string]?: string }
+
 export type LexiconPermissionSet = {
   readonly type: 'permission-set'
   readonly permissions: readonly LexiconPermission<string>[]
+  readonly title?: string
+  readonly 'title:lang'?: LangMap
+  readonly detail?: string
+  readonly 'detail:lang'?: LangMap
 }
