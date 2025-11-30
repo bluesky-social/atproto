73c5d9f7c518b76cf7d2947dde05758f9ff1a617
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-11-30 14:34:09 +0100

    tidy

--- packages/syntax/src/at-identifier.ts
+++ packages/syntax/src/at-identifier.ts
@@ -1,14 +1,22 @@
 import { DidString, ensureValidDidRegex } from './did.js'
-import { HandleString, ensureValidHandleRegex } from './handle.js'
+import {
+  HandleString,
+  InvalidHandleError,
+  ensureValidHandleRegex,
+} from './handle.js'
 
 export type AtIdentifierString = DidString | HandleString
 
 export function ensureValidAtIdentifier(
   input: string,
 ): asserts input is AtIdentifierString {
-  if (input.startsWith('did:')) {
-    ensureValidDidRegex(input)
-  } else {
-    ensureValidHandleRegex(input)
+  try {
+    if (input.startsWith('did:')) {
+      ensureValidDidRegex(input)
+    } else {
+      ensureValidHandleRegex(input)
+    }
+  } catch (cause) {
+    throw new InvalidHandleError('Invalid DID or handle', { cause })
   }
 }
