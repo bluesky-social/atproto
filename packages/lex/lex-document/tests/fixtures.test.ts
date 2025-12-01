4964b5a8813b12401fdef62409758b5ae4d84118
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-12-01 12:36:59 +0100

    Add lex-document test cases

---
+++ packages/lex/lex-document/tests/fixtures.test.ts
@@ -0,0 +1,23 @@
+import { lexiconDocumentSchema } from '..'
+import invalidLexicons from './lexicon-invalid.json' with { type: 'json' }
+import validLexicons from './lexicon-valid.json' with { type: 'json' }
+
+describe('fixtures', () => {
+  describe('valid lexicons', () => {
+    for (const { name, lexicon } of validLexicons) {
+      it(name, () => {
+        expect(lexiconDocumentSchema.parse(lexicon)).toBe(lexicon)
+      })
+    }
+  })
+
+  describe('invalid lexicons', () => {
+    for (const { name, lexicon } of invalidLexicons) {
+      it(name, () => {
+        expect(lexiconDocumentSchema.safeParse(lexicon)).toMatchObject({
+          success: false,
+        })
+      })
+    }
+  })
+})
