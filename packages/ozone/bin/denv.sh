1fa445bf78b0c256d700fe2408ce01cb975407e4
rafaeleyng <rafaeleyng@gmail.com>
2025-12-23 19:06:44 -0300

    running ozone

---
+++ packages/ozone/bin/denv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -e
+
+# corepack depends on the bind-mounted volume, so it needs to happen in run-time, not on build-time.
+corepack prepare --activate
+pnpm --dir packages/ozone install
+pnpm --dir packages/ozone denv
