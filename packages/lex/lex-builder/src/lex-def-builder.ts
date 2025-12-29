194f6d24f04512f815e3521e7797b9945be422e3
Matthieu Sieben <matthieu.sieben@gmail.com>
2025-12-29 17:40:25 +0100

    tidy

--- packages/lex/lex-builder/src/lex-def-builder.ts
+++ packages/lex/lex-builder/src/lex-def-builder.ts
@@ -455,11 +455,10 @@ export class LexDefBuilder {
     // Special case for JSON object payloads
     if (def.encoding === 'application/json' && def.schema?.type === 'object') {
       const properties = await this.compilePropertiesSchemas(def.schema)
-      return this.pure(`l.jsonPayload(${properties.join(',')})`)
+      return this.pure(`l.jsonPayload({${properties.join(',')}})`)
     }
 
     const encodedEncoding = JSON.stringify(def.encoding)
-
     if (def.schema) {
       const bodySchema = await this.compileBodySchema(def.schema)
       return this.pure(`l.payload(${encodedEncoding}, ${bodySchema})`)
@@ -477,7 +476,7 @@ export class LexDefBuilder {
   }
 
   private async compileParamsSchema(def: undefined | LexiconParameters) {
-    if (!def) return this.pure(`l.params({})`)
+    if (!def) return this.pure(`l.params()`)
 
     const properties = await this.compilePropertiesSchemas(def)
     return this.pure(`l.params({${properties.join(',')}})`)
