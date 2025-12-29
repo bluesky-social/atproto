10cf1c10188596724b0c38a5af507d95a382a164
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2025-12-29 13:29:40 +0100

    Improve session management (#4470)
    * Improve session management
    
    * fix test
    
    * tidy
    
    * tidy
    
    * tidy
    
    * tidy

--- packages/ozone/src/lexicon/types/com/atproto/server/refreshSession.ts
+++ packages/ozone/src/lexicon/types/com/atproto/server/refreshSession.ts
@@ -23,6 +23,9 @@ export interface OutputSchema {
   handle: string
   did: string
   didDoc?: { [_ in string]: unknown }
+  email?: string
+  emailConfirmed?: boolean
+  emailAuthFactor?: boolean
   active?: boolean
   /** Hosting status of the account. If not specified, then assume 'active'. */
   status?: 'takendown' | 'suspended' | 'deactivated' | (string & {})
@@ -39,7 +42,7 @@ export interface HandlerSuccess {
 export interface HandlerError {
   status: number
   message?: string
-  error?: 'AccountTakedown'
+  error?: 'AccountTakedown' | 'InvalidToken' | 'ExpiredToken'
 }
 
 export type HandlerOutput = HandlerError | HandlerSuccess
