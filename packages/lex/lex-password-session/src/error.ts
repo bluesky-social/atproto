78a7fb35a6f59d471b6a4b3a1d6ed9ebdab806b9
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-01-08 14:45:29 +0100

    tidy

--- packages/lex/lex-password-session/src/error.ts
+++ packages/lex/lex-password-session/src/error.ts
@@ -1,11 +1,11 @@
-import { LexRpcError, LexRpcResponseError } from '@atproto/lex-client'
+import { LexError, LexRpcResponseError } from '@atproto/lex-client'
 import { com } from './lexicons'
 
 export type AuthFactorTokenRequiredResponse = LexRpcResponseError<
   typeof com.atproto.server.createSession.main
 >
 
-export class AuthFactorTokenError extends LexRpcError {
+export class AuthFactorTokenError extends LexError {
   name = 'AuthFactorTokenError'
 
   constructor(readonly cause: AuthFactorTokenRequiredResponse) {
