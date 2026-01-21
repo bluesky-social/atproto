f62a19b2ec9b542924aa59e9b55cd8bf879855e4
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-01-21 16:43:49 +0100

    Update performances of cbog encoding/decoding

--- packages/lex/lex-cbor/src/encoding.ts
+++ packages/lex/lex-cbor/src/encoding.ts
@@ -1,7 +1,6 @@
 import {
   DecodeOptions,
   EncodeOptions,
-  TagDecoder,
   Token,
   Type,
   decode as cborgDecode,
@@ -20,67 +19,50 @@ import { Cid, LexValue, decodeCid, ifCid } from '@atproto/lex-data'
 
 const CID_CBOR_TAG = 42
 
-function cidEncoder(cid: Cid): Token[] {
-  const bytes = new Uint8Array(cid.bytes.byteLength + 1)
-  bytes.set(cid.bytes, 1) // prefix is 0x00, for historical reasons
-  return [new Token(Type.tag, CID_CBOR_TAG), new Token(Type.bytes, bytes)]
-}
-
-function objectEncoder(
-  obj: object,
-  _typ: string,
-  _options: EncodeOptions,
-): Token[] | null {
-  const cid = ifCid(obj)
-  if (cid) return cidEncoder(cid)
-
-  // Fallback to default object encoder
-  return null
-}
-
-function undefinedEncoder(): null {
-  throw new Error('`undefined` is not allowed by the AT Data Model')
-}
-
-function numberEncoder(num: number): null {
-  if (Number.isSafeInteger(num)) return null
-
-  throw new Error('Non-integer numbers are not allowed by the AT Data Model')
-}
-
-function mapEncoder(map: Map<unknown, unknown>): null {
-  for (const key of map.keys()) {
-    if (typeof key !== 'string') {
-      throw new Error(
-        'Only string keys are allowed in CBOR "map" by the AT Data Model',
-      )
-    }
-  }
-  // @NOTE Map will be encoded as CBOR "map", which will be decoded as object.
-  return null
-}
-
-const encodeOptions: EncodeOptions = {
+export const encodeOptions: EncodeOptions = /*#__PURE__*/ Object.freeze({
   float64: true,
   ignoreUndefinedProperties: true,
-  typeEncoders: {
-    Map: mapEncoder,
-    Object: objectEncoder,
-    undefined: undefinedEncoder,
-    number: numberEncoder,
-  },
-}
+  typeEncoders: Object.freeze({
+    Map: (map: Map<unknown, unknown>): null => {
+      for (const key of map.keys()) {
+        if (typeof key !== 'string') {
+          throw new Error(
+            'Only string keys are allowed in CBOR "map" by the AT Data Model',
+          )
+        }
+      }
+      // @NOTE Map will be encoded as CBOR "map", which will be decoded as object.
+      return null
+    },
+    Object: (
+      obj: object,
+      _typ: string,
+      _options: EncodeOptions,
+    ): Token[] | null => {
+      const cid = ifCid(obj)
+      if (cid) {
+        const bytes = new Uint8Array(cid.bytes.byteLength + 1)
+        bytes.set(cid.bytes, 1) // prefix is 0x00, for historical reasons
+        return [new Token(Type.tag, CID_CBOR_TAG), new Token(Type.bytes, bytes)]
+      }
 
-function cidDecoder(bytes: Uint8Array): Cid {
-  if (bytes[0] !== 0) {
-    throw new Error('Invalid CID for CBOR tag 42; expected leading 0x00')
-  }
-  return decodeCid(bytes.subarray(1)) // ignore leading 0x00
-}
+      // Fallback to default object encoder
+      return null
+    },
+    undefined: (): null => {
+      throw new Error('`undefined` is not supported by the AT Data Model')
+    },
+    number: (num: number): null => {
+      if (Number.isSafeInteger(num)) return null
 
-const tagDecoders: TagDecoder[] = []
-tagDecoders[CID_CBOR_TAG] = cidDecoder
-const decodeOptions: DecodeOptions = {
+      throw new Error(
+        `Non-integer numbers (${num}) are not supported by the AT Data Model`,
+      )
+    },
+  }),
+})
+
+export const decodeOptions: DecodeOptions = /*#__PURE__*/ Object.freeze({
   allowIndefinite: false,
   coerceUndefinedToNull: true,
   allowNaN: false,
@@ -89,8 +71,17 @@ const decodeOptions: DecodeOptions = {
   strict: true,
   useMaps: false,
   rejectDuplicateMapKeys: true,
-  tags: tagDecoders,
-}
+  tags: /*#__PURE__*/ Object.freeze(
+    /*#__PURE__*/ Object.assign([], {
+      [CID_CBOR_TAG]: (bytes: Uint8Array): Cid => {
+        if (bytes[0] !== 0) {
+          throw new Error('Invalid CID for CBOR tag 42; expected leading 0x00')
+        }
+        return decodeCid(bytes.subarray(1)) // ignore leading 0x00
+      },
+    }),
+  ),
+})
 
 export function encode<T extends LexValue = LexValue>(data: T): Uint8Array {
   return cborgEncode(data, encodeOptions)
