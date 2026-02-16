dbc4a5976ef74fb118a49ee02103f335e9f5a06c
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-02-16 13:24:37 -0800

    Add recIdStr to get suggestions skeleton

--- packages/api/src/client/lexicons.ts
+++ packages/api/src/client/lexicons.ts
@@ -8685,6 +8685,10 @@ export const schemaDict = {
               },
               recId: {
                 type: 'integer',
+                description: 'DEPRECATED: use recIdStr instead.',
+              },
+              recIdStr: {
+                type: 'string',
                 description:
                   'Snowflake for this recommendation, use when submitting recommendation events.',
               },
