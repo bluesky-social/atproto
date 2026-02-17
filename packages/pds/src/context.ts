f6249563ed1dc5d0108b46887322396c0f61486c
LaunchPad <i@launchpadx.top>
2026-02-17 08:02:35 +0800

    feat: allow override plcRotationKey

--- packages/pds/src/context.ts
+++ packages/pds/src/context.ts
@@ -243,7 +243,7 @@ export class AppContext {
     )
     await accountManager.migrateOrThrow()
 
-    const plcRotationKey =
+    const plcRotationKey = overrides?.plcRotationKey || (
       secrets.plcRotationKey.provider === 'kms'
         ? await KmsKeypair.load({
             keyId: secrets.plcRotationKey.keyId,
@@ -251,6 +251,7 @@ export class AppContext {
         : await crypto.Secp256k1Keypair.import(
             secrets.plcRotationKey.privateKeyHex,
           )
+    )
 
     const localViewer = LocalViewer.creator(
       accountManager,
