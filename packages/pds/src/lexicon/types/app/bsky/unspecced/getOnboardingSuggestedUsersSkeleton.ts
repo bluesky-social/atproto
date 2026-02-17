978a99efad8393247449bebd88af1ac5b602842e
Eric Bailey <git@esb.lol>
2026-02-16 21:48:32 -0600

    [ALG-58] Use existing `getOnboardingSuggestedUsersSkeleton` method name (#4647)
    * Use existing method name
    
    * Fix import
    
    * Wait no, still wrong
    
    * Changeset

--- packages/pds/src/lexicon/types/app/bsky/unspecced/getOnboardingSuggestedUsersSkeleton.ts
+++ packages/pds/src/lexicon/types/app/bsky/unspecced/getOnboardingSuggestedUsersSkeleton.ts
@@ -12,7 +12,7 @@ import {
 
 const is$typed = _is$typed,
   validate = _validate
-const id = 'app.bsky.unspecced.getSuggestedOnboardingUsersSkeleton'
+const id = 'app.bsky.unspecced.getOnboardingSuggestedUsersSkeleton'
 
 export type QueryParams = {
   /** DID of the account making the request (not included for public/unauthenticated queries). */
