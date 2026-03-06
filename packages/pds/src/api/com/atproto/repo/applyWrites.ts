e20210891a37ef37743fc26e26915aecc6d0a05b
Devin Ivy <devinivy@gmail.com>
2026-03-05 23:21:35 -0500

    pds: handle error case where record already exists on create

--- packages/pds/src/api/com/atproto/repo/applyWrites.ts
+++ packages/pds/src/api/com/atproto/repo/applyWrites.ts
@@ -12,6 +12,7 @@ import {
   BadCommitSwapError,
   InvalidRecordError,
   PreparedWrite,
+  RecordAlreadyExistsError,
   prepareCreate,
   prepareDelete,
   prepareUpdate,
@@ -168,9 +169,11 @@ export default function (server: Server, ctx: AppContext) {
           .catch((err) => {
             if (err instanceof BadCommitSwapError) {
               throw new InvalidRequestError(err.message, 'InvalidSwap')
-            } else {
-              throw err
             }
+            if (err instanceof RecordAlreadyExistsError) {
+              throw new InvalidRequestError(err.message, 'RecordAlreadyExists')
+            }
+            throw err
           })
 
         await ctx.sequencer.sequenceCommit(did, commit)
