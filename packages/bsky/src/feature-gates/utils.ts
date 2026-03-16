802562e2bcfe932fc3fd060164c1649a73ad23b7
Eric Bailey <git@esb.lol>
2026-03-16 10:13:54 -0500

    Ensure merging of contexts results in a correct output

--- packages/bsky/src/feature-gates/utils.ts
+++ packages/bsky/src/feature-gates/utils.ts
@@ -21,10 +21,60 @@ export function normalizeUserContext(
      */
     deviceId = did ?? `anon-${crypto.randomUUID()}`
   }
+
   if (!sessionId) {
     /*
      * If we don't have a sessionId by other means, such as a request header,
      * generate a random sessionId.
+     */
+    sessionId = `anon-${crypto.randomUUID()}`
+  }
+
+  return {
+    did,
+    deviceId,
+    sessionId,
+  }
+}
+
+export function mergeUserContexts(
+  base: NormalizedUserContext,
+  overrides?: UserContext,
+): NormalizedUserContext {
+  const did = overrides?.did ?? base.did ?? undefined
+  let deviceId = overrides?.deviceId ?? base.deviceId
+  let sessionId = overrides?.sessionId ?? base.sessionId
+
+  let isDifferentDid = false
+
+  if (did && deviceId.startsWith('anon-')) {
+    /*
+     * If we have a DID, but the existing deviceId is anonymous, use the DID as
+     * the deviceId to ensure proper ordering of events in our event proxy.
+     * This matches the logic in `normalizeUserContext` where we fall back to
+     * the DID for the deviceId if we don't have a deviceId from other means.
+     */
+    deviceId = did
+  } else if (did && deviceId !== did) {
+    /*
+     * If we have both a DID and a deviceId, but they don't match, we may be
+     * overriding context to check a feature that is independent of a single
+     * request handler lifecycle.
+     *
+     * Example: a ScopedFeatureGatesClient was created in the root request
+     * handler with a user context that has a DID, but later on in the request
+     * lifecycle we may check a gate using the DID of the author of the image
+     * we're returning as part of the response.
+     */
+    deviceId = did
+    isDifferentDid = true
+  }
+
+  if (isDifferentDid) {
+    /*
+     * If we're merging in a different DID, we should also generate a new
+     * sessionId to avoid mixing events from different users under the same
+     * session.
      */
     sessionId = `anon-${crypto.randomUUID()}`
   }
