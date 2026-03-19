6947fe56945acc3366b9107033bb3724d85bde77
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-03-19 12:24:15 -0700

    Remove feature gate from enum

--- packages/bsky/src/feature-gates/gates.ts
+++ packages/bsky/src/feature-gates/gates.ts
@@ -5,7 +5,6 @@
  */
 export enum Gate {
   SuggestedUsersDiscoverEnable = 'suggested_users:discover_agent:enable',
-  SuggestedOnboardingUsersDiscoverEnable = 'suggested_onboarding_users:discover_agent:enable',
   SuggestedUsersSocialProofEnable = 'suggested_users:social_proof:enable',
   ThreadsReplyRankingExplorationEnable = 'threads:reply_ranking_exploration:enable',
   SearchFilteringExplorationEnable = 'search:filtering_exploration:enable',
