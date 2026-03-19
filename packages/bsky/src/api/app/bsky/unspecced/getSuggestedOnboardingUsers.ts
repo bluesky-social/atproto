db795be786ba613aa9d244cdf27a557797c7669c
Eric Bailey <git@esb.lol>
2026-03-19 13:58:07 -0500

    Feature Gates Refactor (#4756)
    * Refactor feature gates client to be less handler-specific
    
    * Improve passing around of feature gates
    
    * Ensure merging of contexts results in a correct output
    
    * Sync with main
    
    * Docs
    
    * Changeset
    
    * Remove image gate
    
    * Comments
    
    * Format
    
    * Update new usage

--- packages/bsky/src/api/app/bsky/unspecced/getSuggestedOnboardingUsers.ts
+++ packages/bsky/src/api/app/bsky/unspecced/getSuggestedOnboardingUsers.ts
@@ -33,12 +33,11 @@ export default function (server: Server, ctx: AppContext) {
       const hydrateCtx = await ctx.hydrator.createContext({
         labelers,
         viewer,
-        featureGatesMap: ctx.featureGatesClient.checkGates(
-          ['suggested_onboarding_users:discover_agent:enable'],
-          {
+        features: ctx.featureGatesClient.scope(
+          ctx.featureGatesClient.parseUserContextFromHandler({
             viewer,
             req,
-          },
+          }),
         ),
       })
       const headers = noUndefinedVals({
@@ -109,8 +108,9 @@ const skeletonFromTopics = async (input: SkeletonFnInput<Context, Params>) => {
 }
 
 const skeleton = async (input: SkeletonFnInput<Context, Params>) => {
-  const useDiscover = input.params.hydrateCtx.featureGatesMap.get(
-    'suggested_onboarding_users:discover_agent:enable',
+  const useDiscover = input.params.hydrateCtx.features.checkGate(
+    input.params.hydrateCtx.features.Gate
+      .SuggestedOnboardingUsersDiscoverEnable,
   )
   const skeletonFn = useDiscover ? skeletonFromDiscover : skeletonFromTopics
   return skeletonFn(input)
