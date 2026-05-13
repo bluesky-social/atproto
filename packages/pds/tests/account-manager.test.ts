821ee276013e54232b5f3093d28ff3567a46a04e
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-05-13 21:14:57 +0200

    tidy

--- packages/pds/tests/account-manager.test.ts
+++ packages/pds/tests/account-manager.test.ts
@@ -1,17 +1,10 @@
-import { once } from 'node:events'
-import { Server, createServer } from 'node:http'
-import { AddressInfo } from 'node:net'
 import { type Browser, launch } from 'puppeteer'
 import { TestNetworkNoAppView } from '@atproto/dev-env'
-import { oauthClientAssetsMiddleware } from './_oauth_client_assets_middleware.js'
 import { PageHelper } from './_puppeteer.js'
 
-describe('oauth', () => {
+describe('account manager', () => {
   let browser: Browser
   let network: TestNetworkNoAppView
-  let server: Server
-
-  let appUrl: string
 
   // @NOTE We are using another language than "en" as default language to
   // test the language negotiation.
@@ -22,9 +15,9 @@ describe('oauth', () => {
       browser: 'chrome', // "firefox"
 
       // For debugging:
-      headless: false,
-      devtools: true,
-      slowMo: 150,
+      // headless: false,
+      // devtools: true,
+      // slowMo: 150,
     })
 
     network = await TestNetworkNoAppView.create({
@@ -38,36 +31,19 @@ describe('oauth', () => {
       handle: 'alice.test',
       password: 'alice-pass',
     })
-
-    server = createServer(oauthClientAssetsMiddleware)
-    server.listen(0)
-    await once(server, 'listening')
-
-    const { port } = server.address() as AddressInfo
-
-    appUrl = `http://127.0.0.1:${port}?${new URLSearchParams({
-      plc_directory_url: network.plc.url,
-      handle_resolver: network.pds.url,
-      sign_up_url: network.pds.url,
-      env: 'test',
-      scope: `account:email identity:* repo:*`,
-    })}`
-
-    appUrl
   })
 
   afterAll(async () => {
-    await server?.close()
     await network?.close()
     await browser?.close()
   })
 
-  it.only('allows creating an account', async () => {
+  it('allows creating an account', async () => {
     await using page = await PageHelper.from(browser, { languages })
 
     await page.goto(new URL('/account', network.pds.url))
 
-    await page.checkTitle(`S'identifier`)
+    await page.assertTitle(`S'identifier`)
 
     await page.clickOnText('Créer un nouveau compte')
 
@@ -80,10 +56,10 @@ describe('oauth', () => {
 
     await page.clickOnText("S'inscrire")
 
-    await page.checkTitle(`Mon compte`)
+    await page.assertTitle(`Compte utilisateur`)
 
     await page.ensureTextVisibility('bob.test', 'span')
-    await page.ensureTextVisibility('Votre compte Atmosphere est hébergé par')
+    await page.ensureTextVisibility('Votre compte Atmosphère est hébergé chez')
   })
 
   it('allows switching accounts', async () => {
@@ -91,7 +67,7 @@ describe('oauth', () => {
 
     await page.goto(new URL('/account', network.pds.url))
 
-    await page.checkTitle(`Mon compte`)
+    await page.assertTitle(`Compte utilisateur`)
 
     await page.clickOnAriaLabel(`Sélecteur de compte`)
     await page.clickOnText('Sélectionner un autre compte')
@@ -102,31 +78,29 @@ describe('oauth', () => {
 
     input.press('Enter')
 
-    await page.ensureTextVisibility('alice.test')
+    await page.ensureTextVisibility('alice.test', 'span')
 
     await page.clickOnAriaLabel(`Sélecteur de compte`)
     await page.clickOnText('Sélectionner un autre compte')
 
-    await page.ensureTextVisibility('alice.test')
-    await page.ensureTextVisibility('bob.test')
+    await page.ensureTextVisibility('alice.test', 'span')
+    await page.ensureTextVisibility('bob.test', 'span')
   })
 
-  // @TODO Sing-up should not create a "remember me" account
   it('forgot about the ephemeral session when loading the page again', async () => {
     await using page = await PageHelper.from(browser, { languages })
 
     await page.goto(new URL('/account', network.pds.url))
 
-    await page.checkTitle(`Mon compte`)
+    await page.assertTitle(`Compte utilisateur`)
 
-    await page.ensureTextVisibility('bob.test')
+    await page.ensureTextVisibility('bob.test', 'span')
 
-    await page.ensureTextVisibility('alice.test').then(
+    await page.ensureTextVisibility('alice.test', 'span').then(
       () => {
         throw new Error('Should not be visible')
       },
       (err) => {
-        console.error('Expected error', err)
         expect(err).toBeInstanceOf(Error)
       },
     )
