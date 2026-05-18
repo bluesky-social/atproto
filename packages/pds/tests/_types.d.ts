c2837e3de3df04aadc7a15f420263844100bb342
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-05-18 16:35:51 +0200

    remove "cjs-default-exports" file

--- packages/pds/tests/_types.d.ts
+++ packages/pds/tests/_types.d.ts
@@ -1,10 +1,3 @@
-/**
- * Module declarations for CJS packages that use `export default` but don't
- * resolve correctly under Node16 module resolution. These packages set
- * `__esModule = true` and `exports.default = ...` in their CJS output, but
- * TypeScript's Node16 resolution doesn't interpret the default import correctly.
- */
-
 declare module 'nodemailer/lib/mailer' {
   namespace Mail {
     interface Options {
