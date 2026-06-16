4ddae54bf80d24f7335d3a3f546f73ad92f7ab5d
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-06-16 22:16:42 +0200

    Remove auto-redirect loop

--- packages/oauth/oauth-provider-ui/src/components/redirect-view.tsx
+++ packages/oauth/oauth-provider-ui/src/components/redirect-view.tsx
@@ -1,5 +1,5 @@
 import { Trans } from '@lingui/react/macro'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {
   LayoutTitle,
   LayoutTitleProps,
@@ -15,61 +15,61 @@ export type RedirectViewProps = Override<
 
     autoRedirect?: boolean | 'replace'
     autoRedirectDelay?: number
-    autoRedirectInterval?: number
-
-    manualRedirectInterval?: number
+    manualRedirectCooldown?: number
   }
 >
 
 export function RedirectView({
   url,
 
-  autoRedirect: autoRedirectInit = false,
-  autoRedirectDelay = 500,
-  autoRedirectInterval = 5_000,
+  autoRedirect = false,
+  autoRedirectDelay = 500, // milliseconds
 
-  manualRedirectInterval = 5_000,
+  manualRedirectCooldown = 5, // seconds
 
   // LayoutTitleProps
   ...props
 }: RedirectViewProps) {
-  const [cooldown, setCooldown] = useCountdown(manualRedirectInterval / 1e3)
+  const [cooldown, setCooldown] = useCountdown(manualRedirectCooldown)
   const [showLink, setShowLink] = useState(cooldown === 0)
-  const [autoRedirect, setAutoRedirect] = useState(autoRedirectInit)
 
   useEffect(() => {
     if (cooldown === 0) setShowLink(true)
   }, [cooldown])
 
-  const redirectNow = useCallback(() => {
-    if (autoRedirect === 'replace') {
-      window.location.replace(url)
-    } else {
-      window.location.href = url
-    }
-  }, [autoRedirect, url])
+  // We need a ref to be able to cancel the auto redirect when the user clicks
+  // the link to manually redirect.
+  const cancelAutoRedirect = useRef(() => {})
 
   useEffect(() => {
     if (autoRedirect) {
-      // First try is "immediate", then we retry every autoRedirectInterval
-      // milliseconds
-      let timer = setTimeout(() => {
-        redirectNow()
-        timer = setInterval(redirectNow, autoRedirectInterval)
+      const timer = setTimeout(() => {
+        if (autoRedirect === 'replace') {
+          window.location.replace(url)
+        } else {
+          window.location.href = url
+        }
       }, autoRedirectDelay)
 
-      const cleanup = () => {
-        window.removeEventListener('beforeunload', cleanup)
+      const cancel = () => {
         clearTimeout(timer)
-        clearInterval(timer)
       }
 
       // We really want to avoid redirecting multiple times
-      window.addEventListener('beforeunload', cleanup)
+      window.addEventListener('beforeunload', cancel)
 
-      return cleanup
+      // Allow cancelling from outside the effect
+      cancelAutoRedirect.current = cancel
+
+      return () => {
+        cancel()
+        window.removeEventListener('beforeunload', cancel)
+        if (cancelAutoRedirect.current === cancel) {
+          cancelAutoRedirect.current = () => {}
+        }
+      }
     }
-  }, [autoRedirect, redirectNow])
+  }, [autoRedirect, url])
 
   return (
     <LayoutTitle {...props}>
@@ -81,14 +81,11 @@ export function RedirectView({
           onClick={(event) => {
             if (cooldown > 0) event.preventDefault()
             else {
-              // Stop the auto redirect
-              setAutoRedirect(false)
+              // Prevent the auto redirect
+              cancelAutoRedirect.current()
 
-              // Wait a little big before allowing the user to click again
-
-              // @NOTE we use a setTimeout here to avoid a re-render during the
-              // click event
-              setTimeout(() => setCooldown(manualRedirectInterval), 1)
+              // Wait a little bit before allowing the user to click again
+              setCooldown(manualRedirectCooldown)
             }
           }}
           aria-disabled={cooldown > 0}
