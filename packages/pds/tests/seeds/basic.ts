61c8cc280dafef532afa3d049689dcd45b58fcbc
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-06-18 14:03:15 +0200

    add names to seed function

--- packages/pds/tests/seeds/basic.ts
+++ packages/pds/tests/seeds/basic.ts
@@ -2,10 +2,10 @@ import { EXAMPLE_LABELER, SeedClient, TestBsky } from '@atproto/dev-env'
 import { app, com } from '../../src/lexicons/index.js'
 import usersSeed from './users.js'
 
-export default async (
+export default async function basicSeed(
   sc: SeedClient,
   opts?: { inviteCode?: string; addModLabels?: TestBsky },
-) => {
+) {
   await usersSeed(sc, opts)
 
   const alice = sc.dids.alice
