1f4003f20443eea2810f4eeca243e2491a499150
Eric Bailey <git@esb.lol>
2026-06-19 11:28:16 -0500

    Allow empty mute options

--- packages/api/src/agent.ts
+++ packages/api/src/agent.ts
@@ -505,12 +505,13 @@ export class Agent extends XrpcClient {
   async mute(
     actor: string,
     options: {
-      kind: 'all' | 'reposts'
-    } = {
-      kind: 'all',
-    },
+      kind?: 'all' | 'reposts'
+    } = {},
   ) {
-    return this.app.bsky.graph.muteActor({ actor, kind: options.kind })
+    return this.app.bsky.graph.muteActor({
+      actor,
+      kind: options.kind ?? 'all',
+    })
   }
 
   async unmute(actor: string) {
