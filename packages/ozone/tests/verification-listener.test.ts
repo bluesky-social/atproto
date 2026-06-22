f78f84cc36a50a0353d87cab0804d59d18f76f76
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-06-22 14:54:52 +0200

    fix ozone test

--- packages/ozone/tests/verification-listener.test.ts
+++ packages/ozone/tests/verification-listener.test.ts
@@ -3,33 +3,49 @@ import { createServer } from 'node:http'
 import { AddressInfo } from 'node:net'
 // eslint-disable-next-line import/default
 import httpTerminator from 'http-terminator'
-import { WebSocketServer } from 'ws'
-import { AppBskyGraphVerification } from '@atproto/api'
-import { TestNetwork, basicSeed } from '@atproto/dev-env'
+import { WebSocket, WebSocketServer } from 'ws'
+import { AppBskyGraphVerification, AtpAgent } from '@atproto/api'
+import { SeedClient, TestNetwork, basicSeed } from '@atproto/dev-env'
 import { forSnapshot } from './_util.js'
 
 describe('verification-listener', () => {
-  it('indexes new and revoked verifications', async () => {
+  let network: TestNetwork
+  let sc: SeedClient
+  let adminAgent: AtpAgent
+  let jetstream: WebSocketServer
+  let relay: WebSocket
+  let terminator: httpTerminator.HttpTerminator
+
+  beforeAll(async () => {
     const server = createServer()
-
-    // make sure to always close the server (even in case of test failure)
-    const { terminate } = httpTerminator.createHttpTerminator({ server })
-
-    // @TODO use an disposable stack when it becomes available (NodeJS 24+)
-    await using _ = { [Symbol.asyncDispose]: async () => terminate() }
-
-    const jetstream = new WebSocketServer({ server })
-
-    // @TODO use an disposable stack when it becomes available (NodeJS 24+)
-    await using __ = {
-      [Symbol.asyncDispose]: async () =>
-        new Promise<void>((res) => jetstream.close(res)),
-    }
+    terminator = httpTerminator.createHttpTerminator({ server })
 
     await once(server.listen(0), 'listening')
     const jetstreamPort = (server.address() as AddressInfo).port
 
-    await using network = await TestNetwork.create({
+    jetstream = new WebSocketServer({ server })
+    const relayPromise = new Promise<WebSocket>((resolve, reject) => {
+      const cleanup = () => {
+        jetstream.off('connection', onConnection)
+        jetstream.off('error', onError)
+      }
+
+      const onConnection = (ws: WebSocket) => {
+        cleanup()
+        resolve(ws)
+      }
+
+      const onError = (err: Error) => {
+        cleanup()
+        reject(err)
+      }
+
+      jetstream.on('connection', onConnection)
+      jetstream.on('error', onError)
+    })
+    relayPromise.catch(() => {})
+
+    network = await TestNetwork.create({
       dbPostgresSchema: 'ozone_verification_listener_test',
       ozone: {
         verifierUrl: 'http://localhost:2583',
@@ -38,58 +54,58 @@ describe('verification-listener', () => {
         jetstreamUrl: `ws://localhost:${jetstreamPort}`,
       },
     })
-
-    const sc = network.getSeedClient()
+    sc = network.getSeedClient()
     await basicSeed(sc)
 
-    jetstream.on('connection', (ws) => {
-      const createEvent = {
-        kind: 'commit',
-        did: sc.dids.bob,
-        time_us: 123456789,
-        commit: {
-          rev: 'xyz',
-          operation: 'create',
-          collection: 'app.bsky.graph.verification',
-          rkey: 'abcdefg',
-          cid: 'xyz',
-          record: {
-            $type: 'app.bsky.graph.verification',
-            subject: sc.dids.alice,
-            handle: sc.accounts[sc.dids.alice].handle,
-            displayName: 'Alice',
-            createdAt: new Date().toISOString(),
-          } satisfies AppBskyGraphVerification.Record,
-        },
-      }
-
-      const deleteEvent = {
-        kind: 'commit',
-        did: sc.dids.bob,
-        time_us: 123456799,
-        commit: {
-          rev: 'yza',
-          operation: 'delete',
-          collection: 'app.bsky.graph.verification',
-          rkey: 'abcdefg',
-        },
-      }
-
-      ws.send(JSON.stringify(createEvent))
-      ws.send(JSON.stringify(deleteEvent))
-    })
-
-    const adminAgent = network.pds.getAgent()
+    adminAgent = network.pds.getAgent()
     await adminAgent.login({
       identifier: sc.accounts[sc.dids.alice].handle,
       password: sc.accounts[sc.dids.alice].password,
     })
     await network.ozone.addAdminDid(sc.dids.alice)
 
-    await network.processAll()
+    relay = await relayPromise
+  })
 
+  beforeEach(async () => network.processAll())
+  afterAll(async () => relay.close())
+  afterAll(async () => terminator?.terminate())
+  afterAll(async () => network?.close())
+
+  it('indexes new and revoked verifications', async () => {
     const { verificationListener } = network.ozone.daemon.ctx
-
+    const createEvent = {
+      kind: 'commit',
+      did: sc.dids.bob,
+      time_us: 123456789,
+      commit: {
+        rev: 'xyz',
+        operation: 'create',
+        collection: 'app.bsky.graph.verification',
+        rkey: 'abcdefg',
+        cid: 'xyz',
+        record: {
+          $type: 'app.bsky.graph.verification',
+          subject: sc.dids.alice,
+          handle: sc.accounts[sc.dids.alice].handle,
+          displayName: 'Alice',
+          createdAt: new Date().toISOString(),
+        } satisfies AppBskyGraphVerification.Record,
+      },
+    }
+    const deleteEvent = {
+      kind: 'commit',
+      did: sc.dids.bob,
+      time_us: 123456799,
+      commit: {
+        rev: 'yza',
+        operation: 'delete',
+        collection: 'app.bsky.graph.verification',
+        rkey: 'abcdefg',
+      },
+    }
+    relay.send(JSON.stringify(createEvent))
+    relay.send(JSON.stringify(deleteEvent))
     const verificationService = network.ozone.ctx.verificationService(
       network.ozone.ctx.db,
     )
