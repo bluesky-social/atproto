7edbf6fa9286fd8c9128e503c16b95012e85ff79
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-07-20 13:53:59 +0200

    roast comments

--- packages/pds/bin/pds.mjs
+++ packages/pds/bin/pds.mjs
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 /* eslint-env node */
 
 // This file is referenced by the "bin" field in package.json. Because of that,
-// we need this file to exist on disk even before the project is built. This
-// allows package managers to properly link the CLI command when the monorepo is
-// being setup (during initial "pnpm install" from the root).
+// we need this file to exist on disk before the project is built. This allows
+// package managers to properly link the CLI command when the monorepo is being
+// setup (during initial "pnpm install" from the repo root).
 
 import { PDS } from '@atproto/pds'
 
