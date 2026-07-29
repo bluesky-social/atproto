9556dcb5855a1a1b9a2dc119e0fd4abbb8b27198
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2026-07-29 11:36:54 +0200

    Migrate oauth tests to vitest (#5280)
    * Migrate oauth tests to vitest
    
    * update skills
    
    * changeset

---
+++ .changeset/heavy-friends-cough.md
@@ -0,0 +1,5 @@
+---
+'@atproto/oauth-scopes': patch
+---
+
+Update test runner from `jest` to `vitest`
--- packages/oauth/oauth-scopes/src/lib/syntax.test.ts
+++ packages/oauth/oauth-scopes/src/lib/syntax.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest'
 import { isScopeStringFor } from './syntax.js'
 
 describe('isScopeStringFor', () => {
