659ac9b87b90489cd1b21ff4a49934436697a9e5
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-08-03 19:05:20 +0200

    remove use of deprecated option

--- packages/pds/tests/_puppeteer.ts
+++ packages/pds/tests/_puppeteer.ts
@@ -95,7 +95,7 @@ export class PageHelper implements AsyncDisposable {
   async typeIn(selector: string, text: string) {
     const elementHandle = await this.getVisibleElement(selector)
     elementHandle.focus()
-    await elementHandle.click({ clickCount: 3 }) // Select all existing text
+    await elementHandle.click({ count: 3 }) // Select all existing text
     await elementHandle.press('Backspace') // Clear the input
     await elementHandle.type(text, { delay: 20 }) // Give the web app time to react to each keystroke
     return elementHandle
