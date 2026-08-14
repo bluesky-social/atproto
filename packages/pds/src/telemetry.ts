3f104bab404ecb67e4aa5386bf9f8e20b699f619
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-08-14 10:24:07 +0200

    lint

--- packages/pds/src/telemetry.ts
+++ packages/pds/src/telemetry.ts
@@ -1,8 +1,8 @@
 import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk'
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis'
 import { BetterSqlite3Instrumentation } from 'opentelemetry-plugin-better-sqlite3'
-import { setup } from '@atproto-labs/opentelemetry-node'
 import pkg from '@atproto/pds/package.json' with { type: 'json' }
+import { setup } from '@atproto-labs/opentelemetry-node'
 
 setup(() => ({
   name: pkg.name,
