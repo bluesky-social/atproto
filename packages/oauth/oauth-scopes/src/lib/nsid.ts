f4b66750705536e87c33aaa84df1d76b9f48ab1e
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-08-31 15:25:39 +0200

    tidy

--- packages/oauth/oauth-scopes/src/lib/nsid.ts
+++ packages/oauth/oauth-scopes/src/lib/nsid.ts
@@ -1,5 +1,5 @@
-import { isValidNsid } from '@atproto/syntax'
+import { type NsidString, isValidNsid } from '@atproto/syntax'
 
-export type Nsid = `${string}.${string}.${string}`
+export type Nsid = NsidString
 export const isNsid = (v: unknown): v is Nsid =>
   typeof v === 'string' && isValidNsid(v)
