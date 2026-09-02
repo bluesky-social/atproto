ae0bdd85c242508c6fca92ecba33c480c64f5f98
Matthieu Sieben <matthieu.sieben@gmail.com>
2026-09-02 11:41:20 +0200

    dead code cleanup

--- packages/ozone/src/mod-service/util.ts
+++ packages/ozone/src/mod-service/util.ts
@@ -72,13 +72,6 @@ export const signLabel = async (
   }
 }
 
-export const isSafeUrl = (url: URL) => {
-  if (url.protocol !== 'https:') return false
-  if (!url.hostname || url.hostname === 'localhost') return false
-  if (net.isIP(url.hostname) !== 0) return false
-  return true
-}
-
 export const getPdsClientForRepo = async (
   idResolver: IdResolver,
   did: DidString,
